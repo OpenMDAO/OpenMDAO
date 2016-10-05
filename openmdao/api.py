ad60eab8e8836d8419ed344082f5813754b33c05
Justin Gray <justin.s.gray@gmail.com>
2016-10-05 09:25:00 -0400

    adding api

---
+++ openmdao/api.py
@@ -0,0 +1,6 @@
+from openmdao.core.problem import Problem
+from openmdao.core.group import Group
+from openmdao.core.component import ExplicitComponent
+from openmdao.core.component import ImplicitComponent
+from openmdao.core.component import IndepVarComponent
+from openmdao.vectors.vector import PETScVector
