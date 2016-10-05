8f3f7ccb3ed43246e5301b5f36bcb4f9e0fb62cd
Justin Gray <justin.s.gray@gmail.com>
2016-10-05 08:53:59 -0400

    making an actual package

--- API/__init__.py
+++
@@ -1,6 +0,0 @@
-from Blue.core.problem import Problem
-from Blue.core.group import Group
-from Blue.core.component import ExplicitComponent
-from Blue.core.component import ImplicitComponent
-from Blue.core.component import IndepVarComponent
-from Blue.vectors.vector import PETScVector
