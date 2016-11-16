657bbc3d2c21de55613aec733584b105284fce53
Bret Naylor <naylor.b@gmail.com>
2016-11-16 08:40:24 -0500

    cleanups

--- openmdao/jacobians/dense_jacobian.py
+++ openmdao/jacobians/dense_jacobian.py
@@ -32,35 +32,33 @@ class DenseJacobian(Jacobian):
             else:
                 re_size = 0
 
-            for op_ivar_set in range(op_nvar_set):
-                op_bool = set_indices['output'][:, 0] == op_ivar_set
-                op_inds = set_indices['output'][op_bool, 1]
-                if len(op_inds) > 0:
-                    sizes_array = sizes['output'][op_ivar_set]
-                    ind1 = numpy.sum(sizes_array[iproc, :op_inds[0]])
-                    ind2 = numpy.sum(sizes_array[iproc, :op_inds[-1] + 1])
-                    op_size = ind2 - ind1
-                else:
-                    op_size = 0
+            if re_size > 0:
 
-                if re_size > 0 and op_size > 0:
-                    array = numpy.zeros((re_size, op_size))
-                    self._int_mtx[re_ivar_set, op_ivar_set] = array
+                for op_ivar_set in range(op_nvar_set):
+                    op_bool = set_indices['output'][:, 0] == op_ivar_set
+                    op_inds = set_indices['output'][op_bool, 1]
+                    if len(op_inds) > 0:
+                        sizes_array = sizes['output'][op_ivar_set]
+                        ind1 = numpy.sum(sizes_array[iproc, :op_inds[0]])
+                        ind2 = numpy.sum(sizes_array[iproc, :op_inds[-1] + 1])
+                        op_size = ind2 - ind1
 
-            for ip_ivar_set in range(ip_nvar_set):
-                ip_bool = set_indices['input'][:, 0] == ip_ivar_set
-                ip_inds = set_indices['input'][ip_bool, 1]
-                if len(ip_inds) > 0:
-                    sizes_array = sizes['input'][ip_ivar_set]
-                    ind1 = numpy.sum(sizes_array[iproc, :ip_inds[0]])
-                    ind2 = numpy.sum(sizes_array[iproc, :ip_inds[-1] + 1])
-                    ip_size = ind2 - ind1
-                else:
-                    ip_size = 0
+                        if op_size > 0:
+                            array = numpy.zeros((re_size, op_size))
+                            self._int_mtx[re_ivar_set, op_ivar_set] = array
 
-                if ip_size > 0 and re_size > 0:
-                    array = numpy.zeros((re_size, ip_size))
-                    self._ext_mtx[re_ivar_set, ip_ivar_set] = array
+                for ip_ivar_set in range(ip_nvar_set):
+                    ip_bool = set_indices['input'][:, 0] == ip_ivar_set
+                    ip_inds = set_indices['input'][ip_bool, 1]
+                    if len(ip_inds) > 0:
+                        sizes_array = sizes['input'][ip_ivar_set]
+                        ind1 = numpy.sum(sizes_array[iproc, :ip_inds[0]])
+                        ind2 = numpy.sum(sizes_array[iproc, :ip_inds[-1] + 1])
+                        ip_size = ind2 - ind1
+
+                        if ip_size > 0:
+                            array = numpy.zeros((re_size, ip_size))
+                            self._ext_mtx[re_ivar_set, ip_ivar_set] = array
 
     def _update(self):
         """See openmdao.jacobians.jacobian.Jacobian."""
@@ -70,8 +68,7 @@ class DenseJacobian(Jacobian):
         set_indices = self._assembler._variable_set_indices
         iproc = self._system.comm.rank + self._system._mpi_proc_range[0]
 
-        for re_ind in range(len(names['output'])):
-            re_name = names['output'][re_ind]
+        for re_ind, re_name in enumerate(names['output']):
             re_ivar_all = indices['output'][re_ind]
             re_ivar_set, re_ivar = set_indices['output'][re_ivar_all, :]
             sizes_array = sizes['output'][re_ivar_set]
@@ -95,8 +92,7 @@ class DenseJacobian(Jacobian):
                     elif type(jac) is list and len(jac) == 3:
                         mtx[re_ind1 + jac[1], op_ind1 + jac[2]] = jac[0]
 
-            for ip_ind in range(len(names['input'])):
-                ip_name = names['input'][ip_ind]
+            for ip_ind, ip_name in enumerate(names['input']):
                 ip_ivar_all = indices['input'][ip_ind]
                 ip_ivar_set, ip_ivar = set_indices['input'][ip_ivar_all, :]
                 sizes_array = sizes['input'][ip_ivar_set]
@@ -106,11 +102,11 @@ class DenseJacobian(Jacobian):
                 if (re_name, ip_name) in self:
                     jac = self[re_name, ip_name]
                     mtx = self._ext_mtx[re_ivar_set, ip_ivar_set]
-                    if type(jac) is numpy.ndarray:
+                    if isinstance(jac, numpy.ndarray):
                         mtx[re_ind1:re_ind2, ip_ind1:ip_ind2] = jac
                     elif scipy.sparse.issparse(jac):
                         mtx[re_ind1:re_ind2, ip_ind1:ip_ind2] = jac.todense()
-                    elif type(jac) is list and len(jac) == 3:
+                    elif isinstance(jac, list) and len(jac) == 3:
                         mtx[re_ind1 + jac[1], ip_ind1 + jac[2]] = jac[0]
 
     def _apply(self, d_inputs, d_outputs, d_residuals, mode):
