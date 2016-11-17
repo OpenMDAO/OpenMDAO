b009cf8e7cc5ba2960b8a33b468d6ff86d5b48c6
John Hwang <hwangjt@Johns-MacBook.local>
2016-11-17 18:30:10 -0500

    Correction to option description

--- openmdao/solvers/nl_btlinesearch.py
+++ openmdao/solvers/nl_btlinesearch.py
@@ -22,7 +22,7 @@ class BacktrackingLineSearch(NonlinearSolver):
                     desc='Set to True to solve subsystems. You may need '
                          'this for solvers nested under Newton.')
         opt.declare('rho', value=0.5, desc="Backtracking multiplier.")
-        opt.declare('alpha', value=1.0, desc="Initial backtracking step.")
+        opt.declare('alpha', value=1.0, desc="Initial line search step.")
         opt.declare('rtol', value=0.9, desc="Relative error tolerance")
         # opt.declare('c', value=0.5, desc="Slope check trigger.")
 
