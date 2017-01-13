593e4255b7f549ba945c7ff1c984eb0e637c2e9e
dwmunster <drayton.w.munster@nasa.gov>
2017-01-13 09:33:13 -0500

    Safe conversions.

--- openmdao/jacobians/jacobian.py
+++ openmdao/jacobians/jacobian.py
@@ -193,7 +193,7 @@ class Jacobian(object):
         dct, out_ind, in_ind, out_size, in_size, typ = self._process_key(key)
 
         if numpy.isscalar(jac) or isinstance(jac, numpy.ndarray):
-            jac = numpy.atleast_2d(jac).astype(float, copy=False)
+            jac = numpy.atleast_2d(jac).astype(float, copy=False, casting='safe')
         elif isinstance(jac, (coo_matrix, csr_matrix)):
             pass
         elif isinstance(jac, (tuple, list)):
