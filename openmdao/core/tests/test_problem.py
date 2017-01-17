6beb940deefc234b7901ef3f7477e56b36002a6d
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2017-01-17 10:56:16 -0500

    a little more

--- openmdao/core/tests/test_problem.py
+++ openmdao/core/tests/test_problem.py
@@ -2,7 +2,8 @@
 
 import unittest
 
-from openmdao.api import Problem, Group, Component
+from openmdao.api import Problem, Group, IndepVarComp
+from openmdao.test_suite.components.paraboloid import Paraboloid
 
 
 class TestProblem(unittest.TestCase):
@@ -11,7 +12,13 @@ class TestProblem(unittest.TestCase):
         # Basic test for the method using default solvers on simple model.
 
 
+        top = Problem()
+        root = top.root = Group()
+        root.add('xx', IndepVarComp('p1', name='x'), promotes=['x'])
+        root.add('xx', IndepVarComp('p2', name='y'), promotes=['y'])
+        root.add('comp', promotes=['x', 'y', 'f_xy'])
 
+        #top.setup(mode='fwd')
 
 
 if __name__ == "__main__":
