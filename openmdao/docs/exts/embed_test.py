8b21babd2c718b56f432ac7f2e01650a6c951f90
Herb Schilling <hschilling@nasa.gov>
2017-02-07 13:43:27 -0500

    Added colors to embedded test docs output blocks for skipped and failed test

--- openmdao/docs/exts/embed_test.py
+++ openmdao/docs/exts/embed_test.py
@@ -13,34 +13,20 @@ else:
     import html as cgiesc
 
 
-class skipped_node(nodes.Element):
+class skipped_or_failed_node(nodes.Element):
     pass
 
 
-def visit_skipped_node(self, node):
-    """
-    what to do when visiting a node *skipped*
-    the function should have different behaviour,
-    depending on the format, or the setup should
-    specify a different function for each.
-    """
+def visit_skipped_or_failed_node(self, node):
     pass
 
 
-def depart_skipped_node(self, node):
-    """
-    what to do when leaving a node *skipped*
-    the function should have different behaviour,
-    depending on the format, or the setup should
-    specify a different function for each.
-
-    It does only html for the time being.
-    """
+def depart_skipped_or_failed_node(self, node):
     if not isinstance(self, HTMLTranslator):
-        self.body.append("%skipped: output only available for HTML\n")
+        self.body.append("output only available for HTML\n")
         return
 
-    html = '<div class="skipped"><pre>{0}</pre></div>'.format(cgiesc.escape(node["text"]))
+    html = '<div class="{}"><pre>{}</pre></div>'.format(node["kind"], cgiesc.escape(node["text"]))
     self.body.append(html)
 
 
@@ -69,7 +55,7 @@ class EmbedTestDirective(Directive):
 
         # grabbing source, and output of a test segment
         method_path = self.arguments[0]
-        (src, output, skipped) = get_unit_test_source_and_run_outputs(method_path)
+        (src, output, skipped, failed) = get_unit_test_source_and_run_outputs(method_path)
 
         # we want the body of test code to be formatted and code highlighted
         body = nodes.literal_block(src, src)
@@ -79,7 +65,9 @@ class EmbedTestDirective(Directive):
         # we want the output block to also be formatted similarly unless test was skipped
         if skipped:
             output = "Test skipped because " + output
-            output_node = skipped_node(text=output)
+            output_node = skipped_or_failed_node(text=output, kind="skipped")
+        elif failed:
+            output_node = skipped_or_failed_node(text=output, kind="failed")
         else:
             output_node = nodes.literal_block(output, output)
 
@@ -92,6 +80,6 @@ def setup(app):
     """add custom directive into Sphinx so that it is found during document parsing"""
 
     app.add_directive('embed-test', EmbedTestDirective)
-    app.add_node(skipped_node, html=(visit_skipped_node, depart_skipped_node))
+    app.add_node(skipped_or_failed_node, html=(visit_skipped_or_failed_node, depart_skipped_or_failed_node))
 
     return {'version': sphinx.__display_version__, 'parallel_read_safe': True}
