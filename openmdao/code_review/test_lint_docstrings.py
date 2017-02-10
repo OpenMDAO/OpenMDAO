c6c975d10724ef0c06cb0bea87365c45945771f7
Rob Falck <rfalck@nasa.gov>
2017-02-09 20:17:17 -0500

    test_lint_docstrings now does the job of test_lint_returns.  Replaced regular expressions with the ast module to traverse the syntax tree and find instances of return at the top level of the method.

--- openmdao/code_review/test_lint_docstrings.py
+++ openmdao/code_review/test_lint_docstrings.py
@@ -1,9 +1,12 @@
 from __future__ import print_function
 
+import ast
 import unittest
 import os.path
 import importlib
 import inspect
+import re
+import textwrap
 from six import PY3
 
 from numpydoc.docscrape import NumpyDocString
@@ -20,35 +23,87 @@ directories = [
 ]
 
 
+class ReturnFinder(ast.NodeVisitor):
+    """
+    An implementation of node visitor only intended to visit a single
+    function/method and not recurse through nested functions.  To use this
+    we have to use ast.parse on the source code of a method.  Since ast.parse
+    is expected full python files, we have to dedent the source first.
+
+    Attributes
+    ----------
+    has_return : bool
+        When visit is called, this NodeVisitor will recurse through the
+        syntax tree and find any instance of return that is not in a nested
+        function.  If it finds return, it is set to True, otherwise it is
+        False.
+    passes : bool
+        Set to True if the method does nothing but pass (i.e. is not yet
+        implemented or is a 'virtual' method in a base class)
+    num_returns : int
+        If the function returns a Tuple, the number of elements in the Tuple.
+        If the function returns a single value, this is 1.
+        Otherwise if the function returns None, this is 0.
+
+    """
+    def __init__(self):
+        self.has_return = False
+        self.passes = False
+        self._function_depth = 0
+        self._depth = 0
+        self.num_returns = 0
+
+    def visit(self, node):
+        """ Visit nodes in the syntax tree to find instances of return. """
+
+        self._depth += 1
+
+        is_function_def = isinstance(node, ast.FunctionDef)
+
+        # Increase function_depth if node is a FunctionDef
+        if is_function_def:
+            self._function_depth += 1
+
+        # If node is a Return, and we're at a function depth of 1,
+        # and the value attribute is not None, then it
+        # returns meaningful (non None) values.
+        if isinstance(node, ast.Return) and self._function_depth == 1:
+            if node.value is not None:
+                self.has_return = True
+                if isinstance(node.value, ast.Tuple):
+                    self.num_returns = len(node.value.elts)
+                else:
+                    self.num_returns = 1
+
+        if hasattr(node, 'body'):
+            # If the top level function does nothing but pass, note it.
+            if is_function_def and self._depth == 2 and len(node.body) <= 2 \
+                     and isinstance(node.body[-1], ast.Pass):
+                self.passes = True
+            # Recurse through subnodes
+            for subnode in node.body:
+                self.visit(subnode)
+
+        # Reduce function_depth on exit if this is a FunctionDef
+        if is_function_def:
+            self._function_depth -= 1
+
+        self._depth -= 1
+
+
 class LintTestCase(unittest.TestCase):
 
-    def check_method(self, dir_name, file_name,
-                     class_name, method_name, method):
-        if PY3:
-            argspec = inspect.getfullargspec(method)
-        else:
-            argspec = inspect.getargspec(method)
-        doc = inspect.getdoc(method)
+    def check_parameters(self, dir_name, file_name, class_name, method_name,
+                         argspec, numpy_doc_string):
 
         fail_msg = '{0}, {1} : {2} {3}'.format(dir_name, file_name, class_name,
                                                method_name)
-        # Check if docstring is missing
-        if doc is None:
-            self.fail(fail_msg + '... missing docstring')
-
-        # Check if docstring references another method
-        if doc[:3] == 'See':
-            return
-
-        nds = NumpyDocString(doc)
-
         if len(argspec.args) > 1:
-
-            if not nds['Parameters']:
+            if not numpy_doc_string['Parameters']:
                 self.fail(fail_msg + '... does not have a Parameters section')
 
             # Check formatting
-            for entry in nds['Parameters']:
+            for entry in numpy_doc_string['Parameters']:
                 name = entry[0]
                 type_ = entry[1]
                 desc = '\n'.join(entry[2])
@@ -59,12 +114,13 @@ class LintTestCase(unittest.TestCase):
                                          'spaces'.format(name.split(':')[0]))
                 if type_ == '':
                     self.fail(fail_msg + '...no type info given for '
-                                         '{0}'.format(name))
+                                         'Parameter {0}'.format(name))
                 if desc == '':
                     self.fail(fail_msg + '...no description given for '
-                                         '{0}'.format(name))
+                                         'Parameter {0}'.format(name))
 
-            documented_arg_set = set(item[0] for item in nds['Parameters'])
+            documented_arg_set = set(item[0] for item in
+                                     numpy_doc_string['Parameters'])
             arg_set = set(argspec.args)
 
             # Require documentation of *args and **kwargs
@@ -88,6 +144,65 @@ class LintTestCase(unittest.TestCase):
             if overdocumented:
                 self.fail(fail_msg + '... documents nonexisting parameters: '
                                      '{0}'.format(str(list(overdocumented))))
+
+    def check_returns(self, dir_name, file_name, class_name, method_name,
+                      method, numpy_doc_string):
+        fail_msg = '{0}, {1} : {2} {3}'.format(dir_name, file_name, class_name,
+                                               method_name)
+        method_src = inspect.getsource(method)
+        dedented_src = textwrap.dedent(method_src)
+        f = ReturnFinder()
+        f.visit(ast.parse(dedented_src))
+
+        # If the function does nothing but pass, return
+        if f.passes:
+            return
+
+        if numpy_doc_string['Returns']:
+            if not f.has_return:
+                self.fail(fail_msg + '... method returns no value but found '
+                                     'unnecessary \'Returns\' sections '
+                                     'in docstring')
+            # Check formatting
+            for entry in numpy_doc_string['Returns']:
+                name = entry[0]
+                type_ = entry[1]
+                desc = '\n'.join(entry[2])
+                if not name:
+                    self.fail(fail_msg + '...no detectable name for Return '
+                                         'value'.format(name))
+                if desc == '':
+                    self.fail(fail_msg + '...no description given for Return '
+                                         '{0}'.format(name))
+        elif f.has_return:
+            self.fail(fail_msg + '... method returns value(s) but found '
+                                 'no \'Returns\' sections in docstring')
+
+    def check_method(self, dir_name, file_name,
+                     class_name, method_name, method):
+        if PY3:
+            argspec = inspect.getfullargspec(method)
+        else:
+            argspec = inspect.getargspec(method)
+        doc = inspect.getdoc(method)
+
+        fail_msg = '{0}, {1} : {2} {3}'.format(dir_name, file_name, class_name,
+                                               method_name)
+        # Check if docstring is missing
+        if doc is None:
+            self.fail(fail_msg + '... missing docstring')
+
+        # Check if docstring references another method
+        if doc[:3] == 'See':
+            return
+
+        nds = NumpyDocString(doc)
+
+        self.check_parameters(dir_name, file_name, class_name, method_name,
+                              argspec, nds)
+
+        self.check_returns(dir_name, file_name, class_name, method_name,
+                           method, nds)
 
     def test_docstrings(self):
         topdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
