56ff8e2c685c4a6c64c84e150cde21f30aaadb01
dwmunster <drayton.w.munster@nasa.gov>
2017-02-13 07:50:07 -0500

    Converted args -> parameters.

--- openmdao/core/component.py
+++ openmdao/core/component.py
@@ -348,8 +348,8 @@ class Component(System):
     def _check_partials_meta(self, key, meta):
         """Check a given partial derivative and metadata for the correct shapes.
 
-        Args
-        ----
+        Parameters
+        ----------
         key : tuple(str,str)
             The of/wrt pair defining the partial derivative.
         meta : dict
