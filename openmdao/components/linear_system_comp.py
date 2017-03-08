340e1c37744aa54fc2db899eb6b94946c6aa6e9b
Justin Gray <justin.s.gray@gmail.com>
2017-03-08 13:21:49 -0500

    allowed user to define how the derivs are computed for linear system component

--- openmdao/components/linear_system_comp.py
+++ openmdao/components/linear_system_comp.py
@@ -4,11 +4,16 @@ import numpy as np
 from scipy import linalg
 
 from openmdao.core.implicitcomponent import ImplicitComponent
+from openmdao.jacobians.global_jacobian import GlobalJacobian
+from openmdao.matrices.dense_matrix import DenseMatrix
 
 
 class LinearSystemComp(ImplicitComponent):
     """
     Component that solves a linear system, Ax=b.
+
+    Designed to handle small and dense linear systems that can be
+    efficiently solved with lu-decomposition
 
     Attributes
     ----------
@@ -23,8 +28,14 @@ class LinearSystemComp(ImplicitComponent):
         super(LinearSystemComp, self).__init__(**kwargs)
         self.metadata.declare('size', value=1, type_=int,
                               desc='the size of the linear system')
+        self.metadata.declare('deriv_type', value='dense',
+                              values=['dense', 'sparse', 'matrix_free'],
+                              desc='the way the derivatives are defined')
 
         self._lup = None
+
+        if self.metadata['deriv_type'] == "matrix_free":
+            self.apply_linear = self._mat_vec_prod
 
     def initialize_variables(self):
         """
@@ -35,6 +46,29 @@ class LinearSystemComp(ImplicitComponent):
         self.add_input("A", val=np.eye(size))
         self.add_input("b", val=np.ones(size))
         self.add_output("x", shape=size, val=2.)
+
+        self.jacobian = GlobalJacobian(matrix_class=DenseMatrix)
+
+    def initialize_partials(self):
+
+        deriv_type = self.metadata['deriv_type']
+
+        size = self.metadata['size']
+        row_col = range(size)
+
+        if deriv_type == 'sparse':
+            self.declare_partials('x', 'b', val=-1, rows=row_col, cols=row_col)
+
+            rows = []
+            cols = []
+            for i in xrange(size):
+                for j in xrange(size):
+                    rows.append(i)
+                    cols.append(i*size+j)
+            self.declare_partials('x', 'A', rows=row_col, cols=row_col)
+
+        elif deriv_type == "dense":
+            self.declare_partials('x', 'b', val=-np.eye(size))
 
     def apply_nonlinear(self, inputs, outputs, residuals):
         """
@@ -66,10 +100,38 @@ class LinearSystemComp(ImplicitComponent):
         self._lup = linalg.lu_factor(inputs['A'])
         outputs['x'] = linalg.lu_solve(self._lup, inputs['b'])
 
-    def apply_linear(self, inputs, outputs, d_inputs, d_outputs,
-                     d_residuals, mode):
-        r"""
+    def linearize(self, inputs, outputs, J):
+        deriv_type = self.metadata['deriv_type']
+        if deriv_type == "matrix_free":
+            return
+
+        x = outputs['x']
+        size = self.metadata['size']
+        if deriv_type == "dense":
+            dx_dA = np.zeros((size, size**2))
+            for i in xrange(size):
+                dx_dA[i, i*size:(i+1)*size] = x
+            J['x', 'A'] = dx_dA
+
+            J['x', 'x'] = inputs['A']
+
+            # constant, defined int initialize_partials
+            # J['x', 'b'] = -np.eye()
+
+        elif deriv_type == "sparse":
+            J['x', 'A'] = np.tile(x, size)
+            J['x', 'x'] = inputs['A']
+
+            # constant, defined int initialize_partials
+            # J['x', 'b'] = -np.ones(size)
+
+    def _mat_vec_prod(self, inputs, outputs, d_inputs, d_outputs,
+                      d_residuals, mode):
+        """
         Compute jac-vector product.
+
+        linear operator for the partial derivative jacobian, only used if the 'deriv_type'
+        metadata is set to 'matrix_free'.
 
         Parameters
         ----------
