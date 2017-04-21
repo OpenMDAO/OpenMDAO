44df24fa30217c11f689674a69719ab546e28ce1
John Hwang <hwangjt@umich.edu>
2017-04-21 11:29:52 -0400

    Minor update to previous commit; all tests pass

--- openmdao/core/system.py
+++ openmdao/core/system.py
@@ -900,8 +900,9 @@ class System(object):
                 # for those sources are not available, so those components of the
                 # scaling vectors will just be 0. That will zero out input values
                 # during transfers, so the multiplier must be 1 by default.
-                if '1' in key[1]:
-                    vecs[key][vec_name].set_const(1.)
+                if resize:
+                    if '1' in key[1]:
+                        vecs[key][vec_name].set_const(1.)
 
             for abs_name, meta in iteritems(self._var_abs2meta['output']):
                 shape = meta['shape']
