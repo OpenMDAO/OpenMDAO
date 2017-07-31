6d5d76b5c018940a765e9e68501ad7b6e029e81d
Herb Schilling <hschilling@nasa.gov>
2017-07-31 15:56:24 -0400

    starting to add more code for parallel recording

--- openmdao/recorders/recording_manager.py
+++ openmdao/recorders/recording_manager.py
@@ -47,6 +47,23 @@ class RecordingManager(object):
         """
         return iter(self._recorders)
 
+    def _gather_vars(self, root, local_vars):
+        """Gathers and returns only variables listed in
+        `local_vars` from the `root` System.
+        """
+
+        # if trace:
+        #     debug("gathering vars for recording in %s" % root.pathname)
+        all_vars = root.comm.gather(local_vars, root=0)
+        # if trace:
+        #     debug("DONE gathering rec vars for %s" % root.pathname)
+
+        if root.comm.rank == 0:
+            dct = all_vars[-1]
+            for d in all_vars[:-1]:
+                dct.update(d)
+            return dct
+
     def append(self, recorder):
         """
         Add a recorder for recording.
@@ -62,8 +79,62 @@ class RecordingManager(object):
         """
         Run startup on each recorder in the manager.
         """
+        pathname = object_requesting_recording.pathname
+        if MPI and root.is_active():
+            rrank = root.comm.rank
+            rowned = root._owning_ranks
+
+        if isinstance(object_requesting_recording,Driver):
+            self._record_desvars = self._record_responses = False
+            self._record_objectives = self._record_constraints = False
+
+
         for recorder in self._recorders:
             recorder.startup(object_requesting_recording)
+
+            if not recorder._parallel:
+                self._has_serial_recorders = True
+
+            desvarnames = recorder._filtered[pathname]['desvars']
+            responsenames = recorder._filtered[pathname]['responses']
+            objectivenames = recorder._filtered[pathname]['objectives']
+            constraintnames = recorder._filtered[pathname]['constraints']
+
+            if desvarnames:
+                self._record_desvars = True
+            if responsenames:
+                self._record_responses = True
+            if objectivenames:
+                self._record_objectives = True
+            if constraintnames:
+                self._record_constraints = True
+
+            # now localize the lists to only
+            # include local vars.  We need to do this after determining
+            # if any mpi procs need to record each of params, unknowns,
+            # and resids.  If none of them do, we can skip the mpi gather
+            # for that group of vars.
+            if MPI:
+                desvarnames = [n for n in desvarnames if rrank==rowned[n]]
+                responsenames = [n for n in responsenames if rrank==rowned[n]]
+                objectivenames = [n for n in objectivenames if rrank==rowned[n]]
+                constraintnames = [n for n in constraintnames if rrank==rowned[n]]
+
+                # reduce the filter set for any parallel recorders to only
+                # those variables that are owned by that rank
+                if recorder._parallel:
+                    recorder._filtered[pathname]['desvars'] = desvarnames
+                    recorder._filtered[pathname]['responses'] = responsenames
+                    recorder._filtered[pathname]['objectives'] = objectivenames
+                    recorder._filtered[pathname]['constraints'] = constraintnames
+
+            self._vars_to_record['desvarnames'].update(desvarnames)
+            self._vars_to_record['responsenames'].update(responsenames)
+            self._vars_to_record['objectivenames'].update(objectivenames)
+            self._vars_to_record['constraintnames'].update(constraintnames)
+
+
+
 
     def close(self):
         """
