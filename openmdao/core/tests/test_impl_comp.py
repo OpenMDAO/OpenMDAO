ddd64b866d2449469b9073cea0b8344daf5f5247
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2017-08-17 10:33:46 -0400

    need to run transfers before each guess_nonlinear

--- openmdao/core/tests/test_impl_comp.py
+++ openmdao/core/tests/test_impl_comp.py
@@ -246,6 +246,48 @@ class ImplicitCompTestCase(unittest.TestCase):
 
     def test_guess_nonlinear(self):
 
+        class ImpWithInitial(QuadraticLinearize):
+
+            def solve_nonlinear(self, inputs, outputs):
+                """ Do nothing. """
+                pass
+
+            def guess_nonlinear(self, inputs, outputs, resids):
+                # Solution at x=1 and x=3. Default value takes us to the x=1 solution. Here
+                # we set it to a value that will take us to the x=3 solution.
+                outputs['x'] = 5.0
+
+
+        group = Group()
+
+        group.add_subsystem('pa', IndepVarComp('a', 1.0))
+        group.add_subsystem('pb', IndepVarComp('b', 1.0))
+        group.add_subsystem('pc', IndepVarComp('c', 1.0))
+        group.add_subsystem('comp2', ImpWithInitial())
+        group.connect('pa.a', 'comp2.a')
+        group.connect('pb.b', 'comp2.b')
+        group.connect('pc.c', 'comp2.c')
+
+        prob = Problem(model=group)
+        group.nonlinear_solver = NewtonSolver()
+        group.nonlinear_solver.options['solve_subsystems'] = True
+        group.nonlinear_solver.options['max_sub_solves'] = 1
+        group.linear_solver = ScipyIterativeSolver()
+
+        prob.setup(check=False)
+
+        prob['pa.a'] = 1.
+        prob['pb.b'] = -4.
+        prob['pc.c'] = 3.
+
+        # Making sure that guess_nonlinear is called early enough to eradicate this.
+        prob['comp2.x'] = np.NaN
+
+        prob.run_model()
+        assert_rel_error(self, prob['comp2.x'], 3.)
+
+    def test_guess_nonlinear_transfer(self):
+
         class ImpWithInitial(ImplicitComponent):
 
             def setup(self):
@@ -278,18 +320,6 @@ class ImplicitCompTestCase(unittest.TestCase):
 
         prob.run_model()
         assert_rel_error(self, prob['comp2.y'], 77.)
-
-    def test_guess_transfer(self):
-        # Make sure that we transfer data after each component's guess_nonlinear
-
-        class ImpWithInitial(QuadraticLinearize):
-
-            def solve_nonlinear(self, inputs, outputs):
-                """ Do nothing. """
-                pass
-
-            def guess_nonlinear(self, inputs, outputs, resids):
-                outputs['xx'] = inputs['x']
 
     def test_guess_nonlinear_feature(self):
 
