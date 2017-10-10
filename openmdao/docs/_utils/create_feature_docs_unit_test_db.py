5011e41b9c6b395f840c802f7f54715511d2f2e0
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2017-10-10 16:58:28 -0400

    Why is appveyor calling that?

--- openmdao/docs/_utils/create_feature_docs_unit_test_db.py
+++ openmdao/docs/_utils/create_feature_docs_unit_test_db.py
@@ -31,164 +31,164 @@
         includes the unit test code(s) into the feature doc automatically
 """
 
-import importlib
-import inspect
-import os
-import sqlite3
-import subprocess
-import tempfile
-import textwrap
-import unittest
+#import importlib
+#import inspect
+#import os
+#import sqlite3
+#import subprocess
+#import tempfile
+#import textwrap
+#import unittest
 
-from numpydoc.docscrape import NumpyDocString, Reader, ParseError
-from numpydoc.docscrape_sphinx import SphinxDocString
-from openmdao.docs.config_params import IGNORE_LIST
-
-
-# The results of this script will be stored in a sqlite database file
-# sqlite_file = 'feature_docs_unit_test_db.sqlite'    # name of the sqlite database file
-# table_name = 'feature_unit_tests'   # name of the table to be queried
-
-from openmdao.docs._utils.docutil import sqlite_file, table_name, get_unit_test_source_and_run_outputs
-
-# pylint: disable=C0103
-
-# ------------------------begin monkeypatch-----------------------
-# monkeypatch to let our docs include a section called "Features"
-#    This new custom section will let unit test developers
-#    indicate what features this unit test is relevant to.
-# For example,
-#
-#        Features
-#        --------
-#        IndepVarComp
-#        derivatives
-#
-# indicates that this unit test is relevant to any feature docs
-# that discuss IndepVarComp or derivatives
+#from numpydoc.docscrape import NumpyDocString, Reader, ParseError
+#from numpydoc.docscrape_sphinx import SphinxDocString
+#from openmdao.docs.config_params import IGNORE_LIST
 
 
-def __str_SphinxDocString__(self, indent=0, func_role="obj"):
-    out = []
-    out += self._str_signature()
-    out += self._str_index() + ['']
-    out += self._str_summary()
-    out += self._str_extended_summary()
-    out += self._str_param_list('Parameters')
-    out += self._str_options('Options')
-    out += self._str_returns()
-    for param_list in ('Other Parameters', 'Raises', 'Warns'):
-        out += self._str_param_list(param_list)
-    out += self._str_warnings()
-    out += self._str_see_also(func_role)
-    out += self._str_section('Notes')
-    out += self._str_section('Features')
-    out += self._str_references()
-    out += self._str_examples()
-    for param_list in ('Attributes', 'Methods'):
-        out += self._str_member_list(param_list)
-    out = self._str_indent(out, indent)
-    return '\n'.join(out)
+## The results of this script will be stored in a sqlite database file
+## sqlite_file = 'feature_docs_unit_test_db.sqlite'    # name of the sqlite database file
+## table_name = 'feature_unit_tests'   # name of the table to be queried
+
+#from openmdao.docs._utils.docutil import sqlite_file, table_name, get_unit_test_source_and_run_outputs
+
+## pylint: disable=C0103
+
+## ------------------------begin monkeypatch-----------------------
+## monkeypatch to let our docs include a section called "Features"
+##    This new custom section will let unit test developers
+##    indicate what features this unit test is relevant to.
+## For example,
+##
+##        Features
+##        --------
+##        IndepVarComp
+##        derivatives
+##
+## indicates that this unit test is relevant to any feature docs
+## that discuss IndepVarComp or derivatives
 
 
-def __init_NumpyDocString__(self, docstring, config={}):
-    orig_docstring = docstring
-    docstring = textwrap.dedent(docstring).split('\n')
-
-    self._doc = Reader(docstring)
-    self._parsed_data = {
-        'Signature': '',
-        'Summary': [''],
-        'Extended Summary': [],
-        'Parameters': [],
-        'Returns': [],
-        'Yields': [],
-        'Raises': [],
-        'Warns': [],
-        'Other Parameters': [],
-        'Attributes': [],
-        'Methods': [],
-        'See Also': [],
-        'Notes': [],
-        'Features': [],
-        'Warnings': [],
-        'References': '',
-        'Examples': '',
-        'index': {}
-    }
-
-    try:
-        self._parse()
-    except ParseError as e:
-        e.docstring = orig_docstring
-        raise
+#def __str_SphinxDocString__(self, indent=0, func_role="obj"):
+    #out = []
+    #out += self._str_signature()
+    #out += self._str_index() + ['']
+    #out += self._str_summary()
+    #out += self._str_extended_summary()
+    #out += self._str_param_list('Parameters')
+    #out += self._str_options('Options')
+    #out += self._str_returns()
+    #for param_list in ('Other Parameters', 'Raises', 'Warns'):
+        #out += self._str_param_list(param_list)
+    #out += self._str_warnings()
+    #out += self._str_see_also(func_role)
+    #out += self._str_section('Notes')
+    #out += self._str_section('Features')
+    #out += self._str_references()
+    #out += self._str_examples()
+    #for param_list in ('Attributes', 'Methods'):
+        #out += self._str_member_list(param_list)
+    #out = self._str_indent(out, indent)
+    #return '\n'.join(out)
 
 
-def __str_NumpyDocString__(self, func_role=''):
-    out = []
-    out += self._str_signature()
-    out += self._str_summary()
-    out += self._str_extended_summary()
-    for param_list in ('Parameters', 'Returns', 'Yields',
-                       'Other Parameters', 'Raises', 'Warns'):
-        out += self._str_param_list(param_list)
-    out += self._str_section('Warnings')
-    out += self._str_see_also(func_role)
-    for s in ('Notes', 'References', 'Examples', 'Features'):
-        out += self._str_section(s)
-    for param_list in ('Attributes', 'Methods'):
-        out += self._str_param_list(param_list)
-    out += self._str_index()
-    return '\n'.join(out)
+#def __init_NumpyDocString__(self, docstring, config={}):
+    #orig_docstring = docstring
+    #docstring = textwrap.dedent(docstring).split('\n')
+
+    #self._doc = Reader(docstring)
+    #self._parsed_data = {
+        #'Signature': '',
+        #'Summary': [''],
+        #'Extended Summary': [],
+        #'Parameters': [],
+        #'Returns': [],
+        #'Yields': [],
+        #'Raises': [],
+        #'Warns': [],
+        #'Other Parameters': [],
+        #'Attributes': [],
+        #'Methods': [],
+        #'See Also': [],
+        #'Notes': [],
+        #'Features': [],
+        #'Warnings': [],
+        #'References': '',
+        #'Examples': '',
+        #'index': {}
+    #}
+
+    #try:
+        #self._parse()
+    #except ParseError as e:
+        #e.docstring = orig_docstring
+        #raise
 
 
-# Do the actual patch switchover to these local versions
-NumpyDocString.__init__ = __init_NumpyDocString__
-NumpyDocString.__str__ = __str_NumpyDocString__
-SphinxDocString.__str__ = __str_SphinxDocString__
-# --------------end monkeypatch---------------------
+#def __str_NumpyDocString__(self, func_role=''):
+    #out = []
+    #out += self._str_signature()
+    #out += self._str_summary()
+    #out += self._str_extended_summary()
+    #for param_list in ('Parameters', 'Returns', 'Yields',
+                       #'Other Parameters', 'Raises', 'Warns'):
+        #out += self._str_param_list(param_list)
+    #out += self._str_section('Warnings')
+    #out += self._str_see_also(func_role)
+    #for s in ('Notes', 'References', 'Examples', 'Features'):
+        #out += self._str_section(s)
+    #for param_list in ('Attributes', 'Methods'):
+        #out += self._str_param_list(param_list)
+    #out += self._str_index()
+    #return '\n'.join(out)
 
 
-# remove any existing sqlite database file
-try:
-    os.remove(sqlite_file)
-except OSError:
-    pass
+## Do the actual patch switchover to these local versions
+#NumpyDocString.__init__ = __init_NumpyDocString__
+#NumpyDocString.__str__ = __str_NumpyDocString__
+#SphinxDocString.__str__ = __str_SphinxDocString__
+## --------------end monkeypatch---------------------
 
-# Prepare to write to the sqlite database file
-conn = sqlite3.connect(sqlite_file)
-cur = conn.cursor()
-cur.execute("CREATE TABLE {tn}(method_path TEXT, feature TEXT, title TEXT, \
-                unit_test_source TEXT, run_outputs TEXT)".format(tn=table_name))
 
-# Search for all the unit tests
-suites = []
-for listing in os.listdir(".."):
-    if os.path.isdir(os.path.join("..", listing)):
-        if listing not in IGNORE_LIST:
-            suites.append(unittest.TestLoader().discover(os.path.join('..', listing), pattern="test_*.py"))
+## remove any existing sqlite database file
+#try:
+    #os.remove(sqlite_file)
+#except OSError:
+    #pass
 
-for suite in suites:
-    for test_file in suite:  # Loop through the TestCases found
-        for test_case in test_file:  # Loop though the test methods in the TestCases
-            if isinstance(test_case, unittest.suite.TestSuite):
-                for test_method in test_case:
-                    cls = test_method.__class__
-                    module_path = cls.__module__
-                    class_name = cls.__name__
-                    method_name = test_method._testMethodName
-                    method_path = '.'.join([module_path, class_name, method_name])
-                    test_doc = getattr(cls, method_name).__doc__
-                    if test_doc:
-                        test_doc_numpy = NumpyDocString(test_doc)
-                        if test_doc_numpy['Features']:
-                            for feature in [f.strip() for f in test_doc_numpy['Features']]:
-                                title = test_doc_numpy['Summary'][0]
-                                unit_test_source, run_outputs, skipped, failed = \
-                                    get_unit_test_source_and_run_outputs(method_path)
-                                cur.execute(
-                                    'insert into {tn} values (?,?,?,?,?)'.format(tn=table_name),
-                                    (method_path, feature, title, unit_test_source, run_outputs))
+## Prepare to write to the sqlite database file
+#conn = sqlite3.connect(sqlite_file)
+#cur = conn.cursor()
+#cur.execute("CREATE TABLE {tn}(method_path TEXT, feature TEXT, title TEXT, \
+                #unit_test_source TEXT, run_outputs TEXT)".format(tn=table_name))
 
-conn.commit()
-conn.close()
+## Search for all the unit tests
+#suites = []
+#for listing in os.listdir(".."):
+    #if os.path.isdir(os.path.join("..", listing)):
+        #if listing not in IGNORE_LIST:
+            #suites.append(unittest.TestLoader().discover(os.path.join('..', listing), pattern="test_*.py"))
+
+#for suite in suites:
+    #for test_file in suite:  # Loop through the TestCases found
+        #for test_case in test_file:  # Loop though the test methods in the TestCases
+            #if isinstance(test_case, unittest.suite.TestSuite):
+                #for test_method in test_case:
+                    #cls = test_method.__class__
+                    #module_path = cls.__module__
+                    #class_name = cls.__name__
+                    #method_name = test_method._testMethodName
+                    #method_path = '.'.join([module_path, class_name, method_name])
+                    #test_doc = getattr(cls, method_name).__doc__
+                    #if test_doc:
+                        #test_doc_numpy = NumpyDocString(test_doc)
+                        #if test_doc_numpy['Features']:
+                            #for feature in [f.strip() for f in test_doc_numpy['Features']]:
+                                #title = test_doc_numpy['Summary'][0]
+                                #unit_test_source, run_outputs, skipped, failed = \
+                                    #get_unit_test_source_and_run_outputs(method_path)
+                                #cur.execute(
+                                    #'insert into {tn} values (?,?,?,?,?)'.format(tn=table_name),
+                                    #(method_path, feature, title, unit_test_source, run_outputs))
+
+#conn.commit()
+#conn.close()
