3a38163473f546cf999f7a9ca8daa22a1f97ab2f
Herb Schilling <hschilling@nasa.gov>
2017-11-09 17:44:14 -0500

    simpler print statement

--- openmdao/utils/tests/test_assert_utils.py
+++ openmdao/utils/tests/test_assert_utils.py
@@ -48,7 +48,7 @@ class TestAssertUtils(unittest.TestCase):
         rtol = 1.e-6
         assert_check_partials(data, atol, rtol)
 
-        print("prob['y'] = {}".format(prob['y']))
+        print("prob['y'] =", prob['y'])
 
 
     def test_assert_check_partials_exception_expected(self):
