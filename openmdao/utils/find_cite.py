14d57dbf5dca845f01ce2ce20fdef5b956628e20
Justin Gray <justin.s.gray@gmail.com>
2017-12-17 23:16:08 -0500

    doh

---
+++ openmdao/utils/find_cite.py
@@ -0,0 +1,47 @@
+import sys
+
+
+def _check_cite(instance, citations):
+    """helper function to grab the cite attribute, if it exists"""
+
+    if instance.cite:
+        klass = instance.__class__
+        # return klass, cite
+        citations[klass] = instance.cite
+
+
+def find_citations(prob, out_stream=sys.stdout):
+    """Compiles a list of citations from all classes in the problem
+
+    Parameters
+    ----------
+    prob : <Problem>
+        The Problem instance to be searched
+    out_stream : File like
+        defaults to sys.stdout. False will prevent printed output
+    """
+
+    #dict keyed by the class so we don't report multiple citations
+    #for the same class showing up in multiple instances
+
+    citations = {}
+    _check_cite(prob, citations)
+    _check_cite(prob.driver, citations)
+
+    # recurse down the model
+    for subsys in prob.model.system_iter(include_self=True, recurse=True):
+        _check_cite(subsys, citations)
+        if subsys.nonlinear_solver is not None:
+            _check_cite(subsys.nonlinear_solver, citations)
+        if subsys.linear_solver is not None:
+            _check_cite(subsys.linear_solver, citations)
+
+    if out_stream:
+
+        for klass, cite in citations.items():
+            out_stream.write("Class: {}\n".format(klass))
+            lines = cite.split('\n')
+            for line in lines:
+                out_stream.write("    {}\n".format(line))
+
+    return citations
