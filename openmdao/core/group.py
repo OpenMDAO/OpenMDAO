fix for direct solver with csc jacobian

openmdao/core/group.py
@@ -1635,33 +1635,29 @@ class Group(System):
 
             with self.jacobian_context() as J:
                 for key in product(of, wrt.union(of)):
-                    meta_changes = {
-                        'method': method,
-                    }
-                    if key[0] == key[1]:
-                        size = self._outputs._views_flat[key[0]].shape[0]
-                        meta_changes['rows'] = np.arange(size)
-                        meta_changes['cols'] = np.arange(size)
-                        meta_changes['value'] = np.ones(size)
-
-                    # This suppports desvar and constraint indices.
-                    if key[0] in self._owns_approx_of_idx:
-                        meta_changes['idx_of'] = self._owns_approx_of_idx[key[0]]
-
-                    if key[1] in self._owns_approx_wrt_idx:
-                        meta_changes['idx_wrt'] = self._owns_approx_wrt_idx[key[1]]
-
                     if key in self._subjacs_info:
                         meta = self._subjacs_info[key]
                     else:
                         meta = SUBJAC_META_DEFAULTS.copy()
+
+                    meta['method'] = method
+                    if key[0] == key[1]:
+                        size = self._outputs._views_flat[key[0]].shape[0]
+                        meta['rows'] = meta['cols'] = np.arange(size)
+                        meta['value'] = np.ones(size)
+
+                    # This suppports desvar and constraint indices.
+                    if key[0] in self._owns_approx_of_idx:
+                        meta['idx_of'] = self._owns_approx_of_idx[key[0]]
+
+                    if key[1] in self._owns_approx_wrt_idx:
+                        meta['idx_wrt'] = self._owns_approx_wrt_idx[key[1]]
 
                     # A group under approximation needs all keys from below, so set dependent to
                     # True.
                     # TODO: Maybe just need a subset of keys (those that go to the boundaries.)
                     meta['dependent'] = True
 
-                    meta.update(meta_changes)
                     meta.update(self._owns_approx_jac_meta)
                     self._subjacs_info[key] = meta
 

openmdao/matrices/matrix.py
@@ -63,7 +63,28 @@ class Matrix(object):
         factor : float or None
             Unit conversion factor.
         """
-        self._submats[key] = (info, irow, icol, src_indices, shape, factor)
+        if key in self._submats:
+            infoset = set(info)
+            submat = self._submats[key][0]
+            oldset = set(submat)
+            common = infoset & oldset
+            new = infoset - oldset
+            diffs = []
+            for name in common:
+                same = submat[name] == info[name]
+                if same is True:
+                    continue
+                if isinstance(same, np.ndarray) and np.all(same):
+                    continue
+                diffs.append(name)
+
+            if diffs:
+                raise RuntimeError("multiple submat definitions with differing metadata values "
+                                   "%s for key %s" % (sorted(diffs), key))
+            for name in new:
+                submat[name] = info[name]
+        else:
+            self._submats[key] = (info, irow, icol, src_indices, shape, factor)
 
     def _build(self, num_rows, num_cols):
         """