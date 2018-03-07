143d78d1849e119cce4405bba976ca945aa01f54
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2018-03-07 11:28:45 -0500

    benchs ready

--- benchmark/benchmark_beam.py
+++ benchmark/benchmark_beam.py
@@ -6,7 +6,7 @@ import unittest
 from openmdao.api import Problem
 from openmdao.parallel_api import PETScVector
 from openmdao.test_suite.test_examples.beam_optimization.multipoint_beam_group import MultipointBeamGroup
-from openmdao.test_suite.test_examples.beam_optimization.multipoint_beam_group_slow import MultipointBeamGroup
+from openmdao.test_suite.test_examples.beam_optimization.multipoint_beam_group_slow import MultipointBeamGroup as MBGSlow
 
 
 class BenchBeamSlowNP1(unittest.TestCase):
@@ -24,9 +24,9 @@ class BenchBeamSlowNP1(unittest.TestCase):
         num_cp = 4
         num_load_cases = 32
 
-        prob = Problem(model=MultipointBeamGroup(E=E, L=L, b=b, volume=volume,
-                                                 num_elements=num_elements, num_cp=num_cp,
-                                                 num_load_cases=num_load_cases))
+        prob = Problem(model=MBGSlow(E=E, L=L, b=b, volume=volume,
+                                     num_elements=num_elements, num_cp=num_cp,
+                                     num_load_cases=num_load_cases))
 
         prob.setup(vector_class=PETScVector)
 
@@ -48,9 +48,9 @@ class BenchBeamSlowNP2(unittest.TestCase):
         num_cp = 4
         num_load_cases = 32
 
-        prob = Problem(model=MultipointBeamGroup(E=E, L=L, b=b, volume=volume,
-                                                 num_elements=num_elements, num_cp=num_cp,
-                                                 num_load_cases=num_load_cases))
+        prob = Problem(model=MBGSlow(E=E, L=L, b=b, volume=volume,
+                                     num_elements=num_elements, num_cp=num_cp,
+                                     num_load_cases=num_load_cases))
 
         prob.setup(vector_class=PETScVector)
 
@@ -62,6 +62,78 @@ class BenchBeamSlowNP4(unittest.TestCase):
     N_PROCS = 4
 
     def benchmark_beam_slow_np4(self):
+
+        E = 1.
+        L = 1.
+        b = 0.1
+        volume = 0.01
+
+        num_elements = 50 * 32
+        num_cp = 4
+        num_load_cases = 32
+
+        prob = Problem(model=MBGSlow(E=E, L=L, b=b, volume=volume,
+                                     num_elements=num_elements, num_cp=num_cp,
+                                     num_load_cases=num_load_cases))
+
+        prob.setup(vector_class=PETScVector)
+
+        prob.run_model()
+
+
+class BenchBeamNP1(unittest.TestCase):
+
+    N_PROCS = 1
+
+    def benchmark_beam_np1(self):
+
+        E = 1.
+        L = 1.
+        b = 0.1
+        volume = 0.01
+
+        num_elements = 50 * 32
+        num_cp = 4
+        num_load_cases = 32
+
+        prob = Problem(model=MultipointBeamGroup(E=E, L=L, b=b, volume=volume,
+                                                 num_elements=num_elements, num_cp=num_cp,
+                                                 num_load_cases=num_load_cases))
+
+        prob.setup(vector_class=PETScVector)
+
+        prob.run_model()
+
+
+class BenchBeamNP2(unittest.TestCase):
+
+    N_PROCS = 2
+
+    def benchmark_beam_np2(self):
+
+        E = 1.
+        L = 1.
+        b = 0.1
+        volume = 0.01
+
+        num_elements = 50 * 32
+        num_cp = 4
+        num_load_cases = 32
+
+        prob = Problem(model=MultipointBeamGroup(E=E, L=L, b=b, volume=volume,
+                                                 num_elements=num_elements, num_cp=num_cp,
+                                                 num_load_cases=num_load_cases))
+
+        prob.setup(vector_class=PETScVector)
+
+        prob.run_model()
+
+
+class BenchBeamNP4(unittest.TestCase):
+
+    N_PROCS = 4
+
+    def benchmark_beam_np4(self):
 
         E = 1.
         L = 1.
