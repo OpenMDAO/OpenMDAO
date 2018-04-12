577e2a63e7939210fb9a01eb21dfe5e68e2a553a
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2018-04-12 11:09:40 -0400

    All Working

--- openmdao/solvers/linear/tests/test_direct_solver.py
+++ openmdao/solvers/linear/tests/test_direct_solver.py
@@ -128,13 +128,14 @@ class TestDirectSolver(LinearSolverTests.LinearSolverTestCase):
         teg.nonlinear_solver.options['max_sub_solves'] = 1
         teg.nonlinear_solver.options['atol'] = 1e-4
 
+
         prob.setup(check=False)
         prob.set_solver_print(level=0)
 
         with self.assertRaises(RuntimeError) as cm:
             prob.run_model()
 
-        expected_msg = "Singular entry found in 'thrust_equilibrium_group' for row associated with state/residual 'thrust'."
+        expected_msg = "Singular entry found in 'thrust_equilibrium_group' for column associated with state/residual 'thrust_equilibrium_group.dynamics.z'."
 
         self.assertEqual(expected_msg, str(cm.exception))
 
@@ -172,7 +173,7 @@ class TestDirectSolver(LinearSolverTests.LinearSolverTestCase):
         with self.assertRaises(RuntimeError) as cm:
             prob.run_model()
 
-        expected_msg = "Singular entry found in 'thrust_equilibrium_group' for row associated with state/residual 'thrust'."
+        expected_msg = "Singular entry found in 'thrust_equilibrium_group' for column associated with state/residual 'thrust_equilibrium_group.dynamics.z'."
 
         self.assertEqual(expected_msg, str(cm.exception))
 
--- openmdao/solvers/solver.py
+++ openmdao/solvers/solver.py
@@ -3,7 +3,7 @@
 from __future__ import division, print_function
 
 from six import iteritems
-
+from collections import OrderedDict
 import os
 import re
 
@@ -575,7 +575,7 @@ class NonlinearSolver(Solver):
             options dictionary.
         """
         super(NonlinearSolver, self).__init__(**kwargs)
-        self._err_cache = {}
+        self._err_cache = OrderedDict()
 
     def _declare_options(self):
         """
