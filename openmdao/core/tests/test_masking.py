7e36b61c1a2a9999059f8332551ef05e9b072f22
Bret Naylor <naylor.b@gmail.com>
2018-04-19 10:27:44 -0400

    removed range case that didn't actually test range

--- openmdao/core/tests/test_masking.py
+++ openmdao/core/tests/test_masking.py
@@ -64,55 +64,21 @@ def _masking_case(mode):
 
     return objective, jac
 
-def _range_case(mode):
-    p = Problem()
-
-    dv = p.model.add_subsystem('dv', IndepVarComp(), promotes=['*'])
-    dv.add_output('z', [1.,1.])
-
-    p.model.add_subsystem('double_sellar', DoubleSellar())
-    p.model.connect('z', ['double_sellar.g1.z', 'double_sellar.g2.z'])
-
-    p.model.add_design_var('z', lower=-10, upper=10)
-    p.model.add_objective('double_sellar.g1.y1')
-
-    p.setup(mode=mode)
-
-    p.model.jacobian = CSCJacobian()
-    p.model.linear_solver = DirectSolver()
-
-    p.model.double_sellar.g1.nonlinear_solver = NewtonSolver()
-    p.model.double_sellar.g2.nonlinear_solver = NewtonSolver()
-
-    p.run_model()
-
-    objective = p['double_sellar.g1.y1']
-    jac = p.compute_totals()
-
-    return objective, jac
-
-
 class CSCMaskingTestCase(unittest.TestCase):
     def test_csc_masking_fwd(self):
         base_objective, base_jac = _baseline('fwd')
-        range_obj, range_jac = _range_case('fwd')
         obj, jac = _masking_case('fwd')
 
         assert_almost_equal(base_objective, obj, decimal=7)
-        assert_almost_equal(base_objective, range_obj, decimal=7)
 
         for key in jac:
             assert_almost_equal(base_jac[key], jac[key], decimal=7)
-            assert_almost_equal(base_jac[key], range_jac[key], decimal=7)
 
     def test_csc_masking_rev(self):
         base_objective, base_jac = _baseline('rev')
-        range_obj, range_jac = _range_case('fwd')
         obj, jac = _masking_case('rev')
 
         assert_almost_equal(base_objective, obj, decimal=7)
-        assert_almost_equal(base_objective, range_obj, decimal=7)
 
         for key in jac:
             assert_almost_equal(base_jac[key], jac[key], decimal=7)
-            assert_almost_equal(base_jac[key], range_jac[key], decimal=7)
