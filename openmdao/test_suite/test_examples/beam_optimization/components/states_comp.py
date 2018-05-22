c245b8946ae8b421bf5b98d4a8d1dd5b2b041575
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2018-05-22 13:25:42 -0400

    shaved off a bunch more time

--- openmdao/test_suite/test_examples/beam_optimization/components/states_comp.py
+++ openmdao/test_suite/test_examples/beam_optimization/components/states_comp.py
@@ -59,17 +59,17 @@ class StatesComp(ImplicitComponent):
 
         partials['d', 'K_local'] = outputs['d'][i_d]
 
-        partials['d', 'd'] = self.K
+        partials['d', 'd'] = self.K.toarray()
 
     def solve_linear(self, d_outputs, d_residuals, mode):
         if mode == 'fwd':
-            d_outputs['d'] = lu_solve(self.lu, d_residuals['d'], trans=0)
+            d_outputs['d'] = self.lu.solve(d_residuals['d'])
         else:
-            d_residuals['d'] = lu_solve(self.lu, d_outputs['d'], trans=1)
+            d_residuals['d'] = self.lu.solve(d_outputs['d'])
 
     def assemble_CSC_K(self, inputs):
         """
-        Assemble the stiffness matrix.
+        Assemble the stiffness matrix in sparse CSC format.
 
         Returns
         -------
@@ -96,21 +96,20 @@ class StatesComp(ImplicitComponent):
             K = inputs['K_local'][ind, :, :]
 
             # NW quadrant gets summed.
-            data[j-4:j-2]  = K[0, :2]
-            data[j-2:j] = K[1, :2]
+            data[j-6:j-4] += K[0, :2]
+            data[j-2:j] += K[1, :2]
 
             # NE quadrant
             data[j:j+4] = K[:2, 2:].flat
-            rows[j:j+4] = np.array([ind1, ind1 + 1, ind1, ind1 + 1])
+            rows[j:j+4] = np.array([ind1, ind1, ind1 + 1, ind1 + 1])
             cols[j:j+4] = np.array([ind1 + 2, ind1 + 3, ind1 + 2, ind1 + 3])
 
             # SE and SW quadrants together
             data[j+4:j+12] = K[2:, :].flat
-            rows[j+4:j+12] = np.repeat(np.arange(ind1, ind1 + 2), 4)
+            rows[j+4:j+12] = np.repeat(np.arange(ind1 + 2, ind1 + 4), 4)
             cols[j+4:j+12] = np.tile(np.arange(ind1, ind1 + 4), 2)
 
             j += 12
-
 
         data[-4:] = 1.0
         rows[-4] = 2 * num_nodes
@@ -158,7 +157,7 @@ class MultiStatesComp(ImplicitComponent):
     def apply_nonlinear(self, inputs, outputs, residuals):
         num_rhs = self.options['num_rhs']
 
-        self.K = self.assemble_dense_K(inputs)
+        self.K = self.assemble_CSC_K(inputs)
         for j in range(num_rhs):
             force_vector = np.concatenate([self.options['force_vector'][:, j], np.zeros(2)])
             residuals['d_%d' % j] = np.dot(self.K, outputs['d_%d' % j]) - force_vector
@@ -166,13 +165,13 @@ class MultiStatesComp(ImplicitComponent):
     def solve_nonlinear(self, inputs, outputs):
         num_rhs = self.options['num_rhs']
 
-        self.K = self.assemble_dense_K(inputs)
-        self.lu = lu_factor(self.K)
+        self.K = self.assemble_CSC_K(inputs)
+        self.lu = splu(self.K)
 
         for j in range(num_rhs):
 
             force_vector = np.concatenate([self.options['force_vector'][:, j], np.zeros(2)])
-            outputs['d_%d' % j] = lu_solve(self.lu, force_vector)
+            outputs['d_%d' % j] = self.lu.solve(force_vector)
 
     def linearize(self, inputs, outputs, partials):
         num_rhs = self.options['num_rhs']
@@ -180,8 +179,8 @@ class MultiStatesComp(ImplicitComponent):
         num_nodes = num_elements + 1
         size = 2 * num_nodes + 2
 
-        self.K = self.assemble_dense_K(inputs)
-        self.lu = lu_factor(self.K)
+        self.K = self.assemble_CSC_K(inputs)
+        self.lu = splu(self.K)
 
         i_elem = np.tile(np.arange(4), 4)
         i_d = np.tile(i_elem, num_elements) + np.repeat(np.arange(num_elements), 16) * 2
@@ -189,7 +188,7 @@ class MultiStatesComp(ImplicitComponent):
         for j in range(num_rhs):
             disp = 'd_%d' % j
             partials[disp, 'K_local'] = outputs[disp][i_d]
-            partials[disp, disp] = self.K
+            partials[disp, disp] = self.K.toarray()
 
     def solve_linear(self, d_outputs, d_residuals, mode):
         num_rhs = self.options['num_rhs']
@@ -198,13 +197,13 @@ class MultiStatesComp(ImplicitComponent):
             disp = 'd_%d' % j
 
             if mode == 'fwd':
-                d_outputs[disp] = lu_solve(self.lu, d_residuals[disp], trans=0)
+                d_outputs[disp] = self.lu.solve(d_residuals[disp])
             else:
-                d_residuals[disp] = lu_solve(self.lu, d_outputs[disp], trans=1)
+                d_residuals[disp] = self.lu.solve(d_outputs[disp])
 
-    def assemble_dense_K(self, inputs):
+    def assemble_CSC_K(self, inputs):
         """
-        Assemble the stiffness matrix.
+        Assemble the stiffness matrix in sparse CSC format.
 
         Returns
         -------
@@ -213,18 +212,48 @@ class MultiStatesComp(ImplicitComponent):
         """
         num_elements = self.options['num_elements']
         num_nodes = num_elements + 1
-        K = self.K
-        K[:] = 0.0
+        num_entry = num_elements * 12 + 4
+        ndim = num_entry + 4
 
-        for ind in range(num_elements):
-            ind1_ = 2 * ind
-            ind2_ = 2 * ind + 4
+        data = np.zeros((ndim, ))
+        cols = np.empty((ndim, ))
+        rows = np.empty((ndim, ))
 
-            K[ind1_:ind2_, ind1_:ind2_] += inputs['K_local'][ind, :, :]
+        # First element.
+        data[:16] = inputs['K_local'][0, :, :].flat
+        cols[:16] = np.tile(np.arange(4), 4)
+        rows[:16] = np.repeat(np.arange(4), 4)
 
-        K[2 * num_nodes + 0, 0] = 1.0
-        K[2 * num_nodes + 1, 1] = 1.0
-        K[0, 2 * num_nodes + 0] = 1.0
-        K[1, 2 * num_nodes + 1] = 1.0
+        j = 16
+        for ind in range(1, num_elements):
+            ind1 = 2 * ind
+            K = inputs['K_local'][ind, :, :]
 
-        return K
+            # NW quadrant gets summed.
+            data[j-6:j-4] += K[0, :2]
+            data[j-2:j] += K[1, :2]
+
+            # NE quadrant
+            data[j:j+4] = K[:2, 2:].flat
+            rows[j:j+4] = np.array([ind1, ind1, ind1 + 1, ind1 + 1])
+            cols[j:j+4] = np.array([ind1 + 2, ind1 + 3, ind1 + 2, ind1 + 3])
+
+            # SE and SW quadrants together
+            data[j+4:j+12] = K[2:, :].flat
+            rows[j+4:j+12] = np.repeat(np.arange(ind1 + 2, ind1 + 4), 4)
+            cols[j+4:j+12] = np.tile(np.arange(ind1, ind1 + 4), 2)
+
+            j += 12
+
+        data[-4:] = 1.0
+        rows[-4] = 2 * num_nodes
+        rows[-3] = 2 * num_nodes + 1
+        rows[-2] = 0.0
+        rows[-1] = 1.0
+        cols[-4] = 0.0
+        cols[-3] = 1.0
+        cols[-2] = 2 * num_nodes
+        cols[-1] = 2 * num_nodes + 1
+
+        n_K = 2 * num_nodes + 2
+        return coo_matrix((data, (rows, cols)), shape=(n_K, n_K)).tocsc()
