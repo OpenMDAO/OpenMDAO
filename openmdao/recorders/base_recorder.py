b8c9019c8f607bd59dbe6d8ec93cb10d0817a7c4
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2018-07-19 12:43:47 -0400

    review

--- openmdao/recorders/base_recorder.py
+++ openmdao/recorders/base_recorder.py
@@ -279,8 +279,7 @@ class BaseRecorder(object):
 
         self._iteration_coordinate = recording_iteration.get_formatted_iteration_coordinate()
 
-        if isinstance(recording_requester, Driver):
-            self.record_derivatives_driver(recording_requester, data, metadata)
+        self.record_derivatives_driver(recording_requester, data, metadata)
 
     def record_derivatives_driver(self, recording_requester, data, metadata):
         """
