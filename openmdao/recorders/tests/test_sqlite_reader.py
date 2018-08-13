a4ceb0e08ba1645a6dea414681e36c85d587f99e
Herb Schilling <hschilling@nasa.gov>
2018-08-13 16:50:22 -0400

    removed unused code

--- openmdao/recorders/tests/test_sqlite_reader.py
+++ openmdao/recorders/tests/test_sqlite_reader.py
@@ -1062,13 +1062,7 @@ class TestSqliteCaseReader(unittest.TestCase):
         x_vals = np.array([0.55994437, -0.95923447, 0.21798656, -0.02158783, 0.62183717,
                            0.04007379, 0.46044942, -0.10129622, 0.27720413, -0.37107886]).reshape(
             (-1, 1))
-
-        # the following were randomly generated using np.random.random(10)*2-1 to randomly
-        # disperse them within a unit circle centered at the origin.
-        indeps.add_output('x', np.array([
-            0.55994437, -0.95923447, 0.21798656, -0.02158783, 0.62183717,
-            0.04007379, 0.46044942, -0.10129622, 0.27720413, -0.37107886
-        ]))
+        indeps.add_output('x', x_vals)
         indeps.add_output('y', np.array([
             0.52577864, 0.30894559, 0.8420792, 0.35039912, -0.67290778,
             -0.86236787, -0.97500023, 0.47739414, 0.51174103, 0.10052582
