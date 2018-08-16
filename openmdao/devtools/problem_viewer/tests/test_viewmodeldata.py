e4b4c907e46f28a355044ff1b8d2d2ebaf3f0c23
swryan <stephen.w.ryan@gmail.com>
2018-08-16 15:00:37 -0400

    turn off DEBUG flag

--- openmdao/devtools/problem_viewer/tests/test_viewmodeldata.py
+++ openmdao/devtools/problem_viewer/tests/test_viewmodeldata.py
@@ -16,7 +16,7 @@ from openmdao.recorders.sqlite_recorder import SqliteRecorder
 
 
 # set DEBUG to True if you want to view the generated HTML file(s)
-DEBUG = True
+DEBUG = False
 
 
 def clean(json_str):
