0017309e78e67ff1c7c6ae6c6a717ec3e64ba9f3
Bret Naylor <naylor.b@gmail.com>
2018-10-02 11:59:08 -0400

    added discrete object passing test

--- openmdao/core/tests/test_discrete.py
+++ openmdao/core/tests/test_discrete.py
@@ -49,7 +49,7 @@ class ModCompIm(ImplicitComponent):
 
 class _DiscreteVal(object):
     """Generic discrete value to test passing of objects."""
-    def __iniit__(self, val):
+    def __init__(self, val):
         self._val = val
 
     def getval(self):
@@ -64,11 +64,13 @@ class _DiscreteVal(object):
         if isinstance(val, _DiscreteVal):
             val = val.getval()
         self._val += val
+        return self
 
     def __imul__(self, val):
         if isinstance(val, _DiscreteVal):
             val = val.getval()
         self._val *= val
+        return self
 
 
 class PathCompEx(ExplicitComponent):
@@ -91,7 +93,7 @@ class ObjAdderCompEx(ExplicitComponent):
         self.add_discrete_output('y', val=copy.deepcopy(self.val))
 
     def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
-        discrete_outputs['y'] += discrete_inputs['x']
+        discrete_outputs['y'].setval(discrete_inputs['x'].getval() + self.val.getval())
 
 
 class DiscreteTestCase(unittest.TestCase):
@@ -159,6 +161,36 @@ class DiscreteStrTestCase(unittest.TestCase):
         self.assertEqual(prob['C3.y'], 'indep/G.G1.C1_1/G.G1.C1_2/C3/')
         self.assertEqual(prob['C4.y'], 'indep/G.G2.C2_1/G.G2.C2_2/C4/')
 
+    def test_obj_pass(self):
+        prob = Problem()
+        model = prob.model
+
+        indep = model.add_subsystem('indep', IndepVarComp(), promotes_outputs=['x'])
+        indep.add_discrete_output('x', _DiscreteVal(19))
+
+        G = model.add_subsystem('G', ParallelGroup(), promotes_inputs=['x'])
+
+        G1 = G.add_subsystem('G1', Group(), promotes_inputs=['x'], promotes_outputs=['y'])
+        G1.add_subsystem('C1_1', ObjAdderCompEx(_DiscreteVal(5)), promotes_inputs=['x'])
+        G1.add_subsystem('C1_2', ObjAdderCompEx(_DiscreteVal(7)), promotes_outputs=['y'])
+        G1.connect('C1_1.y', 'C1_2.x')
+
+        G2 = G.add_subsystem('G2', Group(), promotes_inputs=['x'])
+        G2.add_subsystem('C2_1', ObjAdderCompEx(_DiscreteVal(1)), promotes_inputs=['x'])
+        G2.add_subsystem('C2_2', ObjAdderCompEx(_DiscreteVal(11)), promotes_outputs=['y'])
+        G2.connect('C2_1.y', 'C2_2.x')
+
+        model.add_subsystem('C3', ObjAdderCompEx(_DiscreteVal(9)))
+        model.add_subsystem('C4', ObjAdderCompEx(_DiscreteVal(21)))
+
+        model.connect('G.y', 'C3.x')
+        model.connect('G.G2.y', 'C4.x')
+
+        prob.setup()
+        prob.run_model()
+
+        self.assertEqual(prob['C3.y'].getval(), 40)
+        self.assertEqual(prob['C4.y'].getval(), 52)
 
 if __name__ == "__main__":
     unittest.main()
