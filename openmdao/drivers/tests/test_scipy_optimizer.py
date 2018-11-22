added initialization for _dvlist and _lincongrad_cache trust-constr tests: corrected scaling of objective

openmdao/drivers/tests/test_scipy_optimizer.py
@@ -894,13 +894,13 @@ class TestScipyOptimizeDriver(unittest.TestCase):
             def setup(self):
                 self.add_input('x', np.array([1.5, 1.5, 1.5]))
                 self.add_output('f', 0.0)
-                self.declare_partials('f', 'x', method='fd', form='central', step=1e-4)
+                self.declare_partials('f', 'x', method='fd', form='central', step=1e-2)
 
             def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
                 x = inputs['x']
                 outputs['f'] = rosenbrock(x)
 
-        x0 = np.array([0.5, 0.8, 1.4])
+        x0 = np.array([1.2, 0.8, 1.3])
 
         prob = Problem()
         indeps = prob.model.add_subsystem('indeps', IndepVarComp(problem=prob), promotes=['*'])
@@ -910,18 +910,18 @@ class TestScipyOptimizeDriver(unittest.TestCase):
         prob.model.add_subsystem('con', ExecComp('c=sum(x)', x=np.ones(3)), promotes=['*'])
         prob.driver = ScipyOptimizeDriver()
         prob.driver.options['optimizer'] = 'trust-constr'
-        prob.driver.options['tol'] = 1e-5
+        prob.driver.options['tol'] = 1e-8
         prob.driver.options['maxiter'] = 2000
         prob.driver.options['disp'] = False
 
         prob.model.add_design_var('x')
-        prob.model.add_objective('f', scaler=rosenbrock(x0))
+        prob.model.add_objective('f', scaler=1/rosenbrock(x0))
         prob.model.add_constraint('c', lower=0, upper=10)  # Double sided
 
         prob.setup()
         prob.run_driver()
 
-        assert_rel_error(self, prob['x'][0], 1., 2e-2)
+        assert_rel_error(self, prob['x'], np.ones(3), 2e-2)
         assert_rel_error(self, prob['f'], 0., 1e-2)
         self.assertTrue(prob['c'] < 10)
         self.assertTrue(prob['c'] > 0)
@@ -966,7 +966,7 @@ class TestScipyOptimizeDriver(unittest.TestCase):
         prob.driver.options['disp'] = False
 
         prob.model.add_design_var('x')
-        prob.model.add_objective('f', scaler=rosenbrock(x0))
+        prob.model.add_objective('f', scaler=1/rosenbrock(x0))
         prob.model.add_constraint('c', equals=1.)
 
         prob.setup()
@@ -978,10 +978,10 @@ class TestScipyOptimizeDriver(unittest.TestCase):
                          "scipy >= 1.1 is required.")
     def test_trust_constr_inequality_con(self):
 
-        class Rosenbrock(ExplicitComponent):
+        class Sphere(ExplicitComponent):
 
             def __init__(self, problem):
-                super(Rosenbrock, self).__init__()
+                super(Sphere, self).__init__()
                 self.problem = problem
                 self.counter = 0
 
@@ -1000,7 +1000,7 @@ class TestScipyOptimizeDriver(unittest.TestCase):
         indeps = prob.model.add_subsystem('indeps', IndepVarComp(problem=prob), promotes=['*'])
         indeps.add_output('x', list(x0))
 
-        prob.model.add_subsystem('sphere', Rosenbrock(problem=prob), promotes=['*'])
+        prob.model.add_subsystem('sphere', Sphere(problem=prob), promotes=['*'])
         prob.model.add_subsystem('con', ExecComp('c=sum(x)', x=np.ones(2)), promotes=['*'])
         prob.driver = ScipyOptimizeDriver()
         prob.driver.options['optimizer'] = 'trust-constr'