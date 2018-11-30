689fc697323f3724a0ba4af12054dff62c5a3932
Bret Naylor <naylor.b@gmail.com>
2018-11-30 13:59:48 -0500

    added problem setitem test

--- openmdao/core/tests/test_prob_remote.py
+++ openmdao/core/tests/test_prob_remote.py
@@ -40,3 +40,29 @@ class ProbRemoteTestCase(unittest.TestCase):
         np.testing.assert_almost_equal(prob['group.comp1.f'], 42., decimal=5)
         np.testing.assert_almost_equal(prob['group.comp2.g'], 10., decimal=5)
 
+    def test_remote_var_access_prom(self):
+        # build the model
+        prob = Problem()
+
+        group = prob.model.add_subsystem('group', ParallelGroup(), promotes=['f', 'g'])
+
+        group.add_subsystem('comp1', ExecComp('f = 3.0*x**2 + 5', x=1.0), promotes=['x', 'f'])
+        group.add_subsystem('comp2', ExecComp('g = 7.0*x', x=1.0), promotes=['x', 'g'])
+
+        prob.model.add_subsystem('summ', ExecComp('z = f + g'), promotes=['f', 'g'])
+        prob.model.add_subsystem('prod', ExecComp('z = f * g'), promotes=['f', 'g'])
+
+        #import wingdbstub
+
+        prob.setup()
+
+        # both of these values will get overwritten, but the error only happens when you
+        # set an output, so...
+        prob['f'] = 4.
+        prob['g'] = 5.
+
+        prob.run_model()
+
+        np.testing.assert_almost_equal(prob['summ.z'], 15., decimal=5)
+        np.testing.assert_almost_equal(prob['prod.z'], 56., decimal=5)
+
