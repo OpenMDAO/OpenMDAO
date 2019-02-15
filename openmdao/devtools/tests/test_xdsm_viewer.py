dc101c8aecb40caf65d5d17fed0d6f2d325f98f7
Peter ONODI <p.onodi@gmail.com>
2019-02-15 23:24:58 +0100

    added test for catching error when model_path is given, and recurse is true

--- openmdao/devtools/tests/test_xdsm_viewer.py
+++ openmdao/devtools/tests/test_xdsm_viewer.py
@@ -10,6 +10,7 @@ from openmdao.api import Problem, ExplicitComponent, IndepVarComp, ExecComp, Sci
     Group, write_xdsm
 from openmdao.devtools.xdsm_viewer.html_writer import write_html
 from openmdao.test_suite.components.sellar import SellarNoDerivatives
+from openmdao.test_suite.scripts.circuit import Circuit
 
 try:
     from pyxdsm.XDSM import XDSM
@@ -193,35 +194,9 @@ class TestPyXDSMViewer(unittest.TestCase):
         self.assertTrue(os.path.isfile('.'.join(['xdsm5', 'tex'])))
         self.assertTrue(not pdflatex or os.path.isfile('.'.join(['xdsm5', 'pdf'])))
 
-    def test_circuit(self):
+    def test_circuit_no_recurse(self):
 
-        from openmdao.api import Group, NewtonSolver, DirectSolver, Problem, IndepVarComp
-
-        from openmdao.test_suite.test_examples.test_circuit_analysis import Resistor, Diode, Node
-
-        class Circuit(Group):
-
-            def setup(self):
-                self.add_subsystem('n1', Node(n_in=1, n_out=2),
-                                   promotes_inputs=[('I_in:0', 'I_in')])
-                self.add_subsystem('n2', Node())  # leaving defaults
-
-                self.add_subsystem('R1', Resistor(R=100.), promotes_inputs=[('V_out', 'Vg')])
-                self.add_subsystem('R2', Resistor(R=10000.))
-                self.add_subsystem('D1', Diode(), promotes_inputs=[('V_out', 'Vg')])
-
-                self.connect('n1.V', ['R1.V_in', 'R2.V_in'])
-                self.connect('R1.I', 'n1.I_out:0')
-                self.connect('R2.I', 'n1.I_out:1')
-
-                self.connect('n2.V', ['R2.V_out', 'D1.V_in'])
-                self.connect('R2.I', 'n2.I_in:0')
-                self.connect('D1.I', 'n2.I_out:0')
-
-                self.nonlinear_solver = NewtonSolver()
-                self.nonlinear_solver.options['iprint'] = 2
-                self.nonlinear_solver.options['maxiter'] = 20
-                self.linear_solver = DirectSolver()
+        from openmdao.api import Problem, IndepVarComp
 
         p = Problem()
         model = p.model
@@ -245,9 +220,39 @@ class TestPyXDSMViewer(unittest.TestCase):
 
         p.run_model()
 
-        write_xdsm(p, 'xdsm_circuit', out_format='pdf', quiet=True, show_browser=True,
+        write_xdsm(p, 'xdsm_circuit', out_format='pdf', quiet=True, show_browser=False,
                    recurse=False)
         self.assertTrue(os.path.isfile('.'.join(['xdsm_circuit', 'tex'])))
+
+    def test_circuit_recurse(self):
+
+        from openmdao.api import Problem, IndepVarComp
+
+        p = Problem()
+        model = p.model
+
+        model.add_subsystem('ground', IndepVarComp('V', 0., units='V'))
+        model.add_subsystem('source', IndepVarComp('I', 0.1, units='A'))
+        model.add_subsystem('circuit', Circuit())
+
+        model.connect('source.I', 'circuit.I_in')
+        model.connect('ground.V', 'circuit.Vg')
+
+        model.add_design_var('ground.V')
+        model.add_design_var('source.I')
+        model.add_objective('circuit.D1.I')
+
+        p.setup(check=False)
+
+        # set some initial guesses
+        p['circuit.n1.V'] = 10.
+        p['circuit.n2.V'] = 1.
+
+        p.run_model()
+
+        write_xdsm(p, 'xdsm_circuit2', out_format='pdf', quiet=True, show_browser=False,
+                   recurse=True, model_path='circuit')
+        self.assertTrue(os.path.isfile('.'.join(['xdsm_circuit2', 'tex'])))
 
 
 class TestXDSMjsViewer(unittest.TestCase):
