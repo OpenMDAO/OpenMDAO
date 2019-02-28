fix bool jacobian scaling issue

openmdao/utils/coloring.py
@@ -381,8 +381,8 @@ def _tol_sweep(arr, tol=1e-15, orders=5):
     """
     Find best tolerance 'around' tol to choose nonzero values of arr.
 
-    # Sweeps over tolerances +- 'orders' orders of magnitude around tol and picks the most
-    # stable one (one corresponding to the most repeated number of nonzero entries).
+    Sweeps over tolerances +- 'orders' orders of magnitude around tol and picks the most
+    stable one (one corresponding to the most repeated number of nonzero entries).
 
     Parameters
     ----------
@@ -516,8 +516,7 @@ def _get_bool_jac(prob, repeats=3, tol=1e-15, orders=5, setup=False, run_model=F
                 fullJ += np.abs(J)
         elapsed = time.time() - start_time
 
-    # normalize the full J by dividing by the max value
-    fullJ /= np.max(fullJ)
+    fullJ /= repeats
 
     good_tol, nz_matches, n_tested, zero_entries = _tol_sweep(fullJ, tol, orders)
 