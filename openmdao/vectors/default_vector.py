working on scaling overhaul to fix bug.

openmdao/vectors/default_vector.py
@@ -135,7 +135,12 @@ class DefaultVector(Vector):
                     self._scaling['norm'] = (np.zeros(data.size), np.ones(data.size))
                 elif self._name == 'linear':
                     # reuse the nonlinear scaling vecs since they're the same as ours
-                    nlvec = self._system._root_vecs[self._kind]['nonlinear']
+                    # However, the residual/output linear vecs both should used output scale
+                    # factors.
+                    kind = self._kind
+                    if kind == 'residual':
+                        kind = 'output'
+                    nlvec = self._system._root_vecs[kind]['nonlinear']
                     self._scaling['phys'] = (None, nlvec._scaling['phys'][1])
                     self._scaling['norm'] = (None, nlvec._scaling['norm'][1])
                 else: