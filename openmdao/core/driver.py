01cbd9b47b711112710859abbf762bb8a20eefcd
Kenneth Moore <kenneth.t.moore-1@nasa.gov>
2019-04-25 09:35:54 -0400

    review

--- openmdao/core/driver.py
+++ openmdao/core/driver.py
@@ -164,7 +164,7 @@ class Driver(object):
         self.supports.declare('linear_constraints', types=bool, default=False)
         self.supports.declare('two_sided_constraints', types=bool, default=False)
         self.supports.declare('multiple_objectives', types=bool, default=False)
-        self.supports.declare('integer_design_vars', types=bool, default=True)
+        self.supports.declare('integer_design_vars', types=bool, default=False)
         self.supports.declare('gradients', types=bool, default=False)
         self.supports.declare('active_set', types=bool, default=False)
         self.supports.declare('simultaneous_derivatives', types=bool, default=False)
