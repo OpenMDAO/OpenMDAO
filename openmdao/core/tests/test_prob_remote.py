aa6132e6511def2a615e52dedf1321984fe160be
Bret Naylor <naylor.b@gmail.com>
2019-05-24 16:33:48 -0400

    added test for problem with comm other than COMM_WORLD

--- openmdao/core/tests/test_prob_remote.py
+++ openmdao/core/tests/test_prob_remote.py
@@ -3,8 +3,11 @@ import unittest
 import numpy as np
 
 from openmdao.api import Problem, ExecComp, Group, ParallelGroup, IndepVarComp
-
 from openmdao.utils.mpi import MPI
+from openmdao.utils.general_utils import set_pyoptsparse_opt
+from openmdao.proc_allocators.default_allocator import DefaultAllocator
+from openmdao.test_suite.components.expl_comp_array import TestExplCompArrayDense
+from openmdao.utils.assert_utils import assert_rel_error
 
 
 if MPI:
@@ -12,6 +15,14 @@ if MPI:
         from openmdao.vectors.petsc_vector import PETScVector
     except ImportError:
         PETScVector = None
+
+
+# check that pyoptsparse is installed. if it is, try to use SLSQP.
+OPT, OPTIMIZER = set_pyoptsparse_opt('SLSQP')
+if OPTIMIZER:
+    from openmdao.drivers.pyoptsparse_driver import pyOptSparseDriver
+
+
 
 @unittest.skipUnless(MPI and PETScVector, "only run with MPI and PETSc.")
 class ProbRemoteTestCase(unittest.TestCase):
@@ -62,3 +73,50 @@ class ProbRemoteTestCase(unittest.TestCase):
         np.testing.assert_almost_equal(prob['prod.z'], 20., decimal=5)
 
 
+@unittest.skipUnless(MPI and PETScVector, "only run with MPI and PETSc.")
+class ProbRemote4TestCase(unittest.TestCase):
+
+    N_PROCS = 4
+
+    def test_prob_split_comm(self):
+        colors = [0, 0, 1, 1]
+        alloc = DefaultAllocator(parallel=True)
+
+        comm = MPI.COMM_WORLD.Split(colors[MPI.COMM_WORLD.rank])
+
+        # split the siz 4 comm into 2 size 2 comms
+        self.assertEqual(comm.size, 2)
+
+        prob = Problem(comm=comm)
+        model = prob.model = Group()
+
+        p1 = model.add_subsystem('p1', IndepVarComp('x', 99.0))
+        p1.add_design_var('x', lower=-50.0, upper=50.0)
+
+        par = model.add_subsystem('par', ParallelGroup())
+        c1 = par.add_subsystem('C1', ExecComp('y = x*x'))
+        c2 = par.add_subsystem('C2', ExecComp('y = x*x'))
+
+        model.add_subsystem('obj', ExecComp('o = a + b + 2.'))
+
+        model.connect('p1.x', ['par.C1.x', 'par.C2.x'])
+        model.connect('par.C1.y', 'obj.a')
+        model.connect('par.C2.y', 'obj.b')
+
+        model.add_objective('obj.o')
+
+        prob.set_solver_print(level=0)
+
+        prob.driver = pyOptSparseDriver()
+        prob.driver.options['optimizer'] = OPTIMIZER
+        prob.driver.options['print_results'] = False
+
+        prob.setup(check=False)
+        prob.run_model()
+
+        failed = prob.run_driver()
+
+        self.assertFalse(failed, "Optimization failed, info = " +
+                                 str(prob.driver.pyopt_solution.optInform))
+
+        assert_rel_error(self, prob['obj.o'], 2.0, 1e-6)
