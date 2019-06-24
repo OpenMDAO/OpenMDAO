9aaf352ec548a083204c49090338066615c479d6
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2019-06-24 13:58:46 -0400

    implemented, but some failures

--- openmdao/vectors/default_vector.py
+++ openmdao/vectors/default_vector.py
@@ -342,7 +342,10 @@ class DefaultVector(Vector):
         """
         slices = {}
         start = end = 0
+        print(self._system.pathname)
         for name in self._system._var_abs_names[self._typ]:
+            print(name)
+            print(self._views_flat)
             end += self._views_flat[name].size
             slices[name] = slice(start, end)
             start = end
--- openmdao/vectors/petsc_vector.py
+++ openmdao/vectors/petsc_vector.py
@@ -1,5 +1,6 @@
 """Define the PETSc Vector classe."""
 from __future__ import division
+from copy import deepcopy
 
 import numpy as np
 from petsc4py import PETSc
@@ -28,10 +29,46 @@ class PETScVector(DefaultVector):
     PETSc Vector implementation for running in parallel.
 
     Most methods use the DefaultVector's implementation.
+
+    Attributes
+    ----------
+    _dup_slice : list(int)
+        Keeps track of indices that aren't locally owned; used by norm calculation.
     """
 
     TRANSFER = PETScTransfer
     cite = CITATION
+
+    def __init__(self, name, kind, system, root_vector=None, resize=False, alloc_complex=False,
+                 ncol=1, relevant=None):
+        """
+        Initialize all attributes.
+
+        Parameters
+        ----------
+        name : str
+            The name of the vector: 'nonlinear', 'linear', or right-hand side name.
+        kind : str
+            The kind of vector, 'input', 'output', or 'residual'.
+        system : <System>
+            Pointer to the owning system.
+        root_vector : <Vector>
+            Pointer to the vector owned by the root system.
+        resize : bool
+            If true, resize the root vector.
+        alloc_complex : bool
+            Whether to allocate any imaginary storage to perform complex step. Default is False.
+        ncol : int
+            Number of columns for multi-vectors.
+        relevant : dict
+            Mapping of a VOI to a tuple containing dependent inputs, dependent outputs,
+            and dependent systems.
+        """
+        super(PETScVector, self).__init__(name, kind, system, root_vector=root_vector,
+                                          resize=resize, alloc_complex=alloc_complex, ncol=ncol,
+                                          relevant=relevant)
+
+        self._dup_slice = None
 
     def _initialize_data(self, root_vector):
         """
@@ -84,7 +121,40 @@ class PETScVector(DefaultVector):
         float
             norm of this vector.
         """
-        return self._system.comm.allreduce(np.linalg.norm(self._data))
+        system = self._system
+        comm = system.comm
+        if comm.size > 1:
+            dup_slice = self._dup_slice
+            if dup_slice is None:
+
+                # Here, we are find the indices that are not locally owned so that we can
+                # temporarilly zero them out for the norm calculation.
+                dup_slice = []
+                abs2meta = system._var_allprocs_abs2meta
+                for name, idx_slice in iteritems(self.get_slice_dict()):
+                    owning_rank = system._owning_rank[name]
+                    distributed = abs2meta[name]['distributed']
+                    if not distributed and owning_rank != system.comm.rank:
+                        dup_slice.extend(list(range(idx_slice.start, idx_slice.stop)))
+
+                self._dup_slice = dup_slice
+
+            data_cache = self._data.copy()
+            self._petsc.array = data_cache
+            self._petsc.array[dup_slice] = 0.0
+            distributed_norm = self._petsc.norm()
+
+            # Reset petsc array
+            self._petsc.array = data_cache
+
+            return distributed_norm
+
+        else:
+            # If we are below a parallel group, all variables only appear on the rank that
+            # owns them.
+            self._petsc.array = self._data
+            return self._petsc.norm()
+
 
     def dot(self, vec):
         """
