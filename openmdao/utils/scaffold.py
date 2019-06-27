18d29449cc20abbba7cae58a8f347447b9bbdbac
Bret Naylor <naylor.b@gmail.com>
2019-06-27 16:07:37 -0400

    added test for scaffold

--- openmdao/utils/scaffold.py
+++ openmdao/utils/scaffold.py
@@ -23,6 +23,31 @@ def _scaffold_setup_parser(parser):
                         help="Generate an ImplicitComponent.")
 
 
+def _camel_case_split(cname):
+    # split on camel case names
+    chars = []
+    for i in range(len(cname)):
+        if cname[i] == cname[i].upper():
+            if i > 0 and chars[-1] != chars[-1].upper():
+                chars.append('_')
+            chars.append(cname[i])
+        else:
+            chars.append(cname[i])
+    return ''.join(chars).lower()
+
+
+def _get_template(fname, **kwargs):
+    tfile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                         'scaffolding_templates', fname)
+    with open(tfile, 'r') as f:
+        template = f.read()
+
+    if kwargs:
+        return template.format(**kwargs)
+
+    return template
+
+
 def _scaffold_exec(options):
     """
     Execute the 'openmdao scaffold' command.
@@ -33,21 +58,10 @@ def _scaffold_exec(options):
         Command line options.
     """
     if options.file is None:
-        # construct file name
-
-        # split on camel case names
-        chars = []
-        cname = options.class_name
-        for i in range(len(cname)):
-            if cname[i] == cname[i].upper():
-                if i > 0 and chars[-1] != chars[-1].upper():
-                    chars.append('_')
-                chars.append(cname[i])
-            else:
-                chars.append(cname[i])
-        outfile = ''.join(chars).lower()
+        outfile = _camel_case_split(options.class_name)
     else:
-        outfile = os.path.splitext(options.file[0])[0]
+        outfile = os.path.splitext(options.file)[0]
+
     compfile = outfile + '.py'
     testfile = 'test_' + compfile
 
@@ -58,20 +72,16 @@ def _scaffold_exec(options):
         raise RuntimeError("Component cannot be both implicit and explicit.")
 
     if options.explicit:
-        tfile = os.path.join(templates_dir, 'explicit_comp_template')
+        template = _get_template('explicit_comp_template', class_name=options.class_name)
     elif options.implicit:
-        tfile = os.path.join(templates_dir, 'implicit_comp_template')
+        template = _get_template('implicit_comp_template', class_name=options.class_name)
     else:
         raise RuntimeError("Component must be either implicit or explicit.")
 
-    with open(tfile, 'r') as f:
-        template = f.read()
-
     with open(compfile, 'w') as f:
-        f.write(template.format(class_name=options.class_name))
+        f.write(template)
 
-    with open(os.path.join(templates_dir, 'test_comp_template'), 'r') as f:
-        test_template = f.read()
+    test_template = _get_template('test_comp_template', class_name=options.class_name)
 
     with open(testfile, 'w') as f:
-        f.write(test_template.format(class_name=options.class_name))
+        f.write(test_template)
