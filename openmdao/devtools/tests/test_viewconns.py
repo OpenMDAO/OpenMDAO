701c9cee629c12a6d4c8cb2f347c00f5fdeddae7
Herb Schilling <hschilling@nasa.gov>
2019-07-08 14:28:42 -0400

    Mods based on reviewer comments

--- openmdao/devtools/tests/test_viewconns.py
+++ openmdao/devtools/tests/test_viewconns.py
@@ -1,22 +1,22 @@
 import unittest
+from openmdao.utils.testing_utils import use_tempdirs
 
-
+@use_tempdirs
 class TestSellarFeature(unittest.TestCase):
 
     # no output checking, just make sure no exceptions raised
     # Just tests Newton on Sellar with FD derivs.
     def test_feature_sellar(self):
-        from openmdao.api import Problem
+        import openmdao.api as om
         from openmdao.test_suite.components.sellar import SellarNoDerivatives
-        from openmdao.api import view_connections
 
-        prob = Problem()
+        prob = om.Problem()
         prob.model = SellarNoDerivatives()
 
         prob.setup()
         prob.final_setup()
 
-        view_connections(prob, outfile= "sellar_connections.html", show_browser=False)
+        om.view_connections(prob, outfile= "sellar_connections.html", show_browser=False)
 
 if __name__ == "__main__":
     unittest.main()
