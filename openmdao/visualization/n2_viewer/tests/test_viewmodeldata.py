540c6cd006cd417cbea3f5c35ceb0542eef5cfb2
Herb Schilling <hschilling@nasa.gov>
2019-09-11 09:51:37 -0400

    Initial commit with added test

--- openmdao/visualization/n2_viewer/tests/test_viewmodeldata.py
+++ openmdao/visualization/n2_viewer/tests/test_viewmodeldata.py
@@ -8,10 +8,11 @@ import errno
 from shutil import rmtree
 from tempfile import mkdtemp
 
-from openmdao.core.problem import Problem
+from openmdao.api import Problem, IndepVarComp, ScipyOptimizeDriver
 from openmdao.test_suite.components.sellar import SellarStateConnection
 from openmdao.visualization.n2_viewer.n2_viewer import _get_viewer_data, n2
 from openmdao.recorders.sqlite_recorder import SqliteRecorder
+from openmdao.test_suite.test_examples.test_betz_limit import ActuatorDisc
 from openmdao.utils.shell_proc import check_call
 
 
@@ -79,6 +80,10 @@ class TestViewModelData(unittest.TestCase):
             }
         """)
 
+        self.expected_declare_partials = json.loads("""
+        ["px.x > px.x", "pz.z > pz.z", "sub.state_eq_group.state_eq.y2_command > sub.state_eq_group.state_eq.y2_command", "sub.state_eq_group.state_eq.y2_command > sub.state_eq_group.state_eq.y2_actual", "sub.d1.y1 > sub.d1.y1", "sub.d1.y1 > sub.d1.z", "sub.d1.y1 > sub.d1.x", "sub.d1.y1 > sub.d1.y2", "sub.d2.y2 > sub.d2.y2", "sub.d2.y2 > sub.d2.z", "sub.d2.y2 > sub.d2.y1", "obj_cmp.obj > obj_cmp.obj", "obj_cmp.obj > obj_cmp.x", "obj_cmp.obj > obj_cmp.y1", "obj_cmp.obj > obj_cmp.y2", "obj_cmp.obj > obj_cmp.z", "con_cmp1.con1 > con_cmp1.con1", "con_cmp1.con1 > con_cmp1.y1", "con_cmp2.con2 > con_cmp2.con2", "con_cmp2.con2 > con_cmp2.y2"]
+        """)
+
     def tearDown(self):
         if not DEBUG:
             try:
@@ -88,6 +93,43 @@ class TestViewModelData(unittest.TestCase):
                 if e.errno not in (errno.ENOENT, errno.EACCES, errno.EPERM):
                     raise e
 
+    def check_model_viewer_data(self, model_viewer_data,
+                                expected_tree,
+                                expected_pathnames,
+                                expected_conns,
+                                expected_abs2prom,
+                                expected_declare_partials
+                                ):
+        self.assertDictEqual(model_viewer_data['tree'], expected_tree)
+
+        # check expected system pathnames
+        pathnames = model_viewer_data['sys_pathnames_list']
+        self.assertListEqual(sorted(pathnames), expected_pathnames)
+
+        # check expected connections, after mapping cycle_arrows indices back to pathnames
+        connections = sorted(model_viewer_data['connections_list'],
+                             key=lambda x: (x['tgt'], x['src']))
+        expected_conns = sorted(expected_conns,
+                             key=lambda x: (x['tgt'], x['src']))
+        for conn in connections:
+            if 'cycle_arrows' in conn and conn['cycle_arrows']:
+                cycle_arrows = []
+                for src, tgt in conn['cycle_arrows']:
+                    cycle_arrows.append(' '.join([pathnames[src], pathnames[tgt]]))
+                conn['cycle_arrows'] = sorted(cycle_arrows)
+            self.assertEqual(len(connections), len(expected_conns))
+        for c, ex in zip(connections, expected_conns):
+            self.assertEqual(c['src'], ex['src'])
+            self.assertEqual(c['tgt'], ex['tgt'])
+            self.assertEqual(c.get('cycle_arrows', []), ex.get('cycle_arrows', []))
+
+        # check expected abs2prom map
+        self.assertListEqual(sorted(model_viewer_data['abs2prom']), sorted(expected_abs2prom))
+
+        # check expected declare_partials_list
+        self.assertListEqual(sorted(model_viewer_data['declare_partials_list']),
+                             sorted(expected_declare_partials))
+
     def test_model_viewer_has_correct_data_from_problem(self):
         """
         Verify that the correct model structure data exists when stored as compared
@@ -95,32 +137,109 @@ class TestViewModelData(unittest.TestCase):
         """
         p = Problem(model=SellarStateConnection())
         p.setup()
+        p.final_setup()
 
         model_viewer_data = _get_viewer_data(p)
 
         # check expected model tree
-        self.assertDictEqual(model_viewer_data['tree'], self.expected_tree)
+        self.check_model_viewer_data(
+            model_viewer_data,
+            self.expected_tree,
+            self.expected_pathnames,
+            self.expected_conns,
+            self.expected_abs2prom,
+            self.expected_declare_partials
+        )
 
-        # check expected system pathnames
-        pathnames = model_viewer_data['sys_pathnames_list']
-        self.assertListEqual(sorted(pathnames), self.expected_pathnames)
+    def test_model_viewer_has_correct_data_from_optimization_problem(self):
+        """
+        Verify that the correct model structure data exists when stored as compared
+        to the expected structure, using the SellarStateConnection model.
+        """
 
-        # check expected connections, after mapping cycle_arrows indices back to pathnames
-        connections = sorted(model_viewer_data['connections_list'], key=lambda x: (x['tgt'], x['src']))
-        for conn in connections:
-            if 'cycle_arrows' in conn and conn['cycle_arrows']:
-                cycle_arrows = []
-                for src, tgt in conn['cycle_arrows']:
-                    cycle_arrows.append(' '.join([pathnames[src], pathnames[tgt]]))
-                conn['cycle_arrows'] = sorted(cycle_arrows)
-        self.assertEqual(len(connections), len(self.expected_conns))
-        for c, ex in zip(connections, self.expected_conns):
-            self.assertEqual(c['src'], ex['src'])
-            self.assertEqual(c['tgt'], ex['tgt'])
-            self.assertEqual(c.get('cycle_arrows', []), ex.get('cycle_arrows', []))
+        # build the model
+        prob = Problem()
+        indeps = prob.model.add_subsystem('indeps', IndepVarComp(), promotes=['*'])
+        indeps.add_output('a', .5)
+        indeps.add_output('Area', 10.0, units='m**2')
+        indeps.add_output('rho', 1.225, units='kg/m**3')
+        indeps.add_output('Vu', 10.0, units='m/s')
 
-        # check expected abs2prom map
-        self.assertDictEqual(model_viewer_data['abs2prom'], self.expected_abs2prom)
+        prob.model.add_subsystem('a_disk', ActuatorDisc(),
+                                 promotes_inputs=['a', 'Area', 'rho', 'Vu'])
+
+        # setup the optimization
+        prob.driver = ScipyOptimizeDriver()
+        prob.driver.options['optimizer'] = 'SLSQP'
+
+        prob.model.add_design_var('a', lower=0., upper=1.)
+        prob.model.add_design_var('Area', lower=0., upper=1.)
+
+        # negative one so we maximize the objective
+        prob.model.add_objective('a_disk.Cp', scaler=-1)
+
+        prob.setup()
+        prob.final_setup()
+
+        model_viewer_data = _get_viewer_data(prob)
+
+        expected_tree_betz = json.loads("""
+                    {"name": "root", "type": "root", "class": "Group", "component_type": null,
+                     "subsystem_type": "group", "is_parallel": false, "linear_solver": "LN: RUNONCE",
+                     "nonlinear_solver": "NL: RUNONCE", "children": [
+                        {"name": "indeps", "type": "subsystem", "class": "IndepVarComp",
+                         "subsystem_type": "component", "is_parallel": false, "component_type": "indep",
+                         "linear_solver": "", "nonlinear_solver": "", "children": [
+                            {"name": "a", "type": "unknown", "implicit": false, "dtype": "ndarray"},
+                            {"name": "Area", "type": "unknown", "implicit": false, "dtype": "ndarray"},
+                            {"name": "rho", "type": "unknown", "implicit": false, "dtype": "ndarray"},
+                            {"name": "Vu", "type": "unknown", "implicit": false, "dtype": "ndarray"}]},
+                        {"name": "a_disk", "type": "subsystem", "class": "ActuatorDisc",
+                         "subsystem_type": "component", "is_parallel": false, "component_type": "explicit",
+                         "linear_solver": "", "nonlinear_solver": "",
+                         "children": [{"name": "a", "type": "param", "dtype": "ndarray"},
+                                      {"name": "Area", "type": "param", "dtype": "ndarray"},
+                                      {"name": "rho", "type": "param", "dtype": "ndarray"},
+                                      {"name": "Vu", "type": "param", "dtype": "ndarray"},
+                                      {"name": "Vr", "type": "unknown", "implicit": false,
+                                       "dtype": "ndarray"},
+                                      {"name": "Vd", "type": "unknown", "implicit": false,
+                                       "dtype": "ndarray"},
+                                      {"name": "Ct", "type": "unknown", "implicit": false,
+                                       "dtype": "ndarray"},
+                                      {"name": "thrust", "type": "unknown", "implicit": false,
+                                       "dtype": "ndarray"},
+                                      {"name": "Cp", "type": "unknown", "implicit": false,
+                                       "dtype": "ndarray"},
+                                      {"name": "power", "type": "unknown", "implicit": false,
+                                       "dtype": "ndarray"}]}]}
+                    """
+                                             )
+        expected_pathnames_betz = json.loads('[]')
+        expected_conns_betz = json.loads("""
+                [{"src": "indeps.a", "tgt": "a_disk.a"}, {"src": "indeps.Area", "tgt": "a_disk.Area"},
+                 {"src": "indeps.rho", "tgt": "a_disk.rho"}, {"src": "indeps.Vu", "tgt": "a_disk.Vu"}]
+                """)
+        expected_abs2prom_betz = json.loads("""
+                    {"input": {"a_disk.a": "a", "a_disk.Area": "Area", "a_disk.rho": "rho", "a_disk.Vu": "Vu"},
+                    "output": {"indeps.a": "a", "indeps.Area": "Area", "indeps.rho": "rho", "indeps.Vu": "Vu",
+                    "a_disk.Vr": "a_disk.Vr", "a_disk.Vd": "a_disk.Vd", "a_disk.Ct": "a_disk.Ct",
+                    "a_disk.thrust": "a_disk.thrust", "a_disk.Cp": "a_disk.Cp",
+                    "a_disk.power": "a_disk.power"}}
+                    """)
+        expected_declare_partials_betz = json.loads("""
+                ["indeps.a > indeps.a", "indeps.Area > indeps.Area", "indeps.rho > indeps.rho", "indeps.Vu > indeps.Vu", "a_disk.Vr > a_disk.a", "a_disk.Vr > a_disk.Vu", "a_disk.Vd > a_disk.a", "a_disk.Ct > a_disk.a", "a_disk.thrust > a_disk.a", "a_disk.thrust > a_disk.Area", "a_disk.thrust > a_disk.rho", "a_disk.thrust > a_disk.Vu", "a_disk.Cp > a_disk.a", "a_disk.power > a_disk.a", "a_disk.power > a_disk.Area", "a_disk.power > a_disk.rho", "a_disk.power > a_disk.Vu", "a_disk.Vr > a_disk.Vr", "a_disk.Vd > a_disk.Vd", "a_disk.Ct > a_disk.Ct", "a_disk.thrust > a_disk.thrust", "a_disk.Cp > a_disk.Cp", "a_disk.power > a_disk.power"]
+        """)
+
+        # check expected model tree
+        self.check_model_viewer_data(
+            model_viewer_data,
+            expected_tree_betz,
+            expected_pathnames_betz,
+            expected_conns_betz,
+            expected_abs2prom_betz,
+            expected_declare_partials_betz
+        )
 
     def test_model_viewer_has_correct_data_from_sqlite(self):
         """
@@ -138,31 +257,16 @@ class TestViewModelData(unittest.TestCase):
         r.shutdown()
 
         model_viewer_data = _get_viewer_data(self.sqlite_db_filename)
-        print(model_viewer_data['tree'])
 
         # check expected model tree
-        self.assertDictEqual(model_viewer_data['tree'], self.expected_tree)
-
-        # check expected system pathnames
-        pathnames = model_viewer_data['sys_pathnames_list']
-        self.assertListEqual(sorted(pathnames), self.expected_pathnames)
-
-        # check expected connections, after mapping cycle_arrows indices back to pathnames
-        connections = sorted(model_viewer_data['connections_list'], key=lambda x: (x['tgt'], x['src']))
-        for conn in connections:
-            if 'cycle_arrows' in conn:
-                cycle_arrows = []
-                for src, tgt in conn['cycle_arrows']:
-                    cycle_arrows.append(' '.join([pathnames[src], pathnames[tgt]]))
-                conn['cycle_arrows'] = sorted(cycle_arrows)
-        self.assertEqual(len(connections), len(self.expected_conns))
-        for c, ex in zip(connections, self.expected_conns):
-            self.assertEqual(c['src'], ex['src'])
-            self.assertEqual(c['tgt'], ex['tgt'])
-            self.assertEqual(c.get('cycle_arrows', []), ex.get('cycle_arrows', []))
-
-        # check expected abs2prom map
-        self.assertDictEqual(model_viewer_data['abs2prom'], self.expected_abs2prom)
+        self.check_model_viewer_data(
+            model_viewer_data,
+            self.expected_tree,
+            self.expected_pathnames,
+            self.expected_conns,
+            self.expected_abs2prom,
+            self.expected_declare_partials
+        )
 
     def test_n2_from_problem(self):
         """
