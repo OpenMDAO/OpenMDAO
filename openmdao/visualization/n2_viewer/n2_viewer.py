4ea50798144c745fee298e3360a425271c6f5a14
Tad Kollar <tad.kollar@nasa.gov>
2019-09-18 09:31:50 -0400

    Added N2Matrix inclusion

--- openmdao/visualization/n2_viewer/n2_viewer.py
+++ openmdao/visualization/n2_viewer/n2_viewer.py
@@ -380,7 +380,7 @@ def n2(data_source, outfile='n2.html', show_browser=True, embeddable=False,
     # grab the libraries, src and style
     lib_dct = {'d3': 'd3.v4.min', 'awesomplete': 'awesomplete', 'vk_beautify': 'vkBeautify'}
     libs = read_files(itervalues(lib_dct), libs_dir, 'js')
-    src_names = 'constants', 'draw', 'legend', 'modal', 'N2Arrow', 'ptN2', 'search', 'svg'
+    src_names = 'constants', 'draw', 'legend', 'modal', 'N2Matrix', 'N2Arrow', 'ptN2', 'search', 'svg'
     srcs = read_files(src_names, src_dir, 'js')
     styles = read_files(('awesomplete', 'partition_tree'), style_dir, 'css')
 
