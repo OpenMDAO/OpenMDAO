442b29dbdeb9a28fee210c43735df2dc96b43d3a
Bret Naylor <naylor.b@gmail.com>
2019-10-24 13:47:07 -0400

    fixed a couple of scripts to include if __name__ == '__main__' block

--- openmdao/test_suite/scripts/circle_coloring_dynpartials.py
+++ openmdao/test_suite/scripts/circle_coloring_dynpartials.py
@@ -6,6 +6,7 @@ COLOR_TYPE = os.environ.get('COLOR_TYPE', 'auto')
 from openmdao.api import ScipyOptimizeDriver
 from openmdao.core.tests.test_coloring import run_opt
 
-
-p_color = run_opt(ScipyOptimizeDriver, COLOR_TYPE, optimizer='SLSQP', disp=False, dynamic_total_coloring=True, partial_coloring=True)
+if __name__ == '__main__':
+    p_color = run_opt(ScipyOptimizeDriver, COLOR_TYPE, optimizer='SLSQP', disp=False,
+                      dynamic_total_coloring=True, partial_coloring=True)
 
