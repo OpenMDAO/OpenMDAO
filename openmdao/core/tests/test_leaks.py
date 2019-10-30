bd2f2ec22d3a6c136f50c9f9f02851a7b7272782
Bret Naylor <naylor.b@gmail.com>
2019-10-30 15:00:16 -0400

    interim

--- openmdao/core/tests/test_leaks.py
+++ openmdao/core/tests/test_leaks.py
@@ -1,26 +1,41 @@
 
+import gc
 from contextlib import contextmanager
 
 import openmdao.api as om
+from openmdao.core.tests.test_coloring import run_opt
+from openmdao.core.system import System
+from openmdao.vectors.vector import Vector
+from openmdao.solvers.solver import Solver
+from openmdao.core.driver import Driver
 
 
 @contextmanager
-def report_leaks(flags=gc.DEBUG_COLLECTABLE | gc.DEBUG_UNCOLLECTABLE | gc.DEBUG_SAVEALL):
+def report_leaks(classes=(object,), flags=gc.DEBUG_COLLECTABLE | gc.DEBUG_UNCOLLECTABLE | gc.DEBUG_SAVEALL):
     gc.set_debug(flags)
 
     yield
 
     # Force a sweep
-    print('Collecting')
+    print('Collecting garbage')
     gc.collect()
     print('Done')
 
     # Report on what was left
     for o in gc.garbage:
-        if isinstance(o, Graph):
+        if isinstance(o, classes):
             print('Retained: {} 0x{:x}'.format(o, id(o)))
 
     # Reset the debug flags before exiting to avoid dumping a lot
     # of extra information and making the example output more
     # confusing.
     gc.set_debug(0)
+
+
+
+with report_leaks((System, om.Problem, Vector, Driver, Solver)):
+    for i in range(3):
+        p_color = run_opt(om.pyOptSparseDriver, 'auto', optimizer='SNOPT', print_results=False,
+                          dynamic_total_coloring=True, partial_coloring=True)
+        p_color = None
+
