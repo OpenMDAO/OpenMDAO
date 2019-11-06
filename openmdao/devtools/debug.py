a12fe3871f3699e8b35c22f641ac3391f7207a9d
Bret Naylor <naylor.b@gmail.com>
2019-11-06 16:48:09 -0500

    cleanup

--- openmdao/devtools/debug.py
+++ openmdao/devtools/debug.py
@@ -195,7 +195,6 @@ def tree(top, show_solvers=True, show_jacs=True, show_colors=True, show_approx=T
         if isinstance(s, Group):
             cprint("%s " % type(s).__name__, color=Fore.GREEN + Style.BRIGHT)
             cprint("%s" % s.name)
-            dist = False
         else:
             if isinstance(s, ImplicitComponent):
                 colr = Back.CYAN + Fore.BLACK + Style.BRIGHT
@@ -203,10 +202,8 @@ def tree(top, show_solvers=True, show_jacs=True, show_colors=True, show_approx=T
                 colr = Fore.CYAN + Style.BRIGHT
             cprint("%s " % type(s).__name__, color=colr)
             cprint("%s" % s.name)
-            dist = s.options['distributed']
-
-        if dist:
-            cprint(" (distributed)", color=Fore.MAGENTA)
+            if s.options['distributed']:
+                cprint(" (distributed)", color=Fore.MAGENTA)
 
         # FIXME: these sizes could be wrong under MPI
         if show_sizes:
