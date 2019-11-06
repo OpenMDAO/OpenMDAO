ab4ad2b1e36432a14e163447009c9aef60ea7fe3
Bret Naylor <naylor.b@gmail.com>
2019-11-06 12:48:13 -0500

    refactor test

--- openmdao/core/tests/test_leaks.py
+++ openmdao/core/tests/test_leaks.py
@@ -12,27 +12,27 @@ from openmdao.vectors.vector import Vector
 from openmdao.solvers.solver import Solver
 from openmdao.core.driver import Driver
 from openmdao.jacobians.jacobian import Jacobian
-
-try:
-    from pyoptsparse import Optimization
-except ImportError:
-    Optimization = Driver
-
-try:
-    from parameterized import parameterized
-except ImportError:
-    from openmdao.utils.assert_utils import SkipParameterized as parameterized
+from openmdao.utils.general_utils import set_pyoptsparse_opt
 
 
-# much of the following code was taken from https://pymotw.com/3/gc/
+OPT, OPTIMIZER = set_pyoptsparse_opt('SNOPT', fallback=True)
+
+
+# much of the following code for recording leaks was taken from https://pymotw.com/3/gc/
 
 
 # Ignore references from local variables in this module, global
 # variables, and from the garbage collector's bookkeeping.
 REFERRERS_TO_IGNORE = [locals(), globals(), gc.garbage]
 
-_om_classes = (System, om.Problem, Vector, Driver, Solver, MethodType, FunctionType,
-               GeneratorType, CoroutineType, Optimization)
+_om_classes = [System, om.Problem, Vector, Driver, Solver, MethodType, FunctionType,
+               GeneratorType, CoroutineType]
+
+if OPTIMIZER:
+    import pyoptsparse
+    _om_classes.append(pyoptsparse.Optimization)
+
+_om_classes = tuple(_om_classes)
 
 
 def find_referring_objects(obj, classes=(object,)):
@@ -80,15 +80,14 @@ def record_leaks(classes=(object,),
     gc.set_debug(0)
 
 
-class LeakTestPyoptSparseCase(unittest.TestCase):
+class LeakTestCase(unittest.TestCase):
 
     ISOLATED = True
 
-    def test_leaks_pyoptsparse(self):
-
+    def _check_leaks(self, driver_class, optimizer):
         with record_leaks(_om_classes) as rec:
             for i in range(3):
-                p_color = run_opt(om.pyOptSparseDriver, 'auto', optimizer='SLSQP',
+                p_color = run_opt(driver_class, 'auto', optimizer=optimizer,
                                   dynamic_total_coloring=True, partial_coloring=True)
                 p_color = None
 
@@ -110,39 +109,17 @@ class LeakTestPyoptSparseCase(unittest.TestCase):
 
             self.fail('\n'.join(msgs))
 
-        # Force a sweep
-        gc.collect()
 
+    @unittest.skipIf(OPTIMIZER is None, 'pyoptsparse SLSQP is not installed.')
+    def test_leaks_pyoptsparse_slsqp(self):
+        self._check_leaks(om.pyOptSparseDriver, 'SLSQP')
 
-class LeakTestScipyCase(unittest.TestCase):
+    @unittest.skipUnless(OPTIMIZER == 'SNOPT', 'pyoptsparse SNOPT is not installed.')
+    def test_leaks_pyoptsparse_snopt(self):
+        self._check_leaks(om.pyOptSparseDriver, 'SNOPT')
 
-    ISOLATED = True
-
-    def test_leaks_scipyopt(self):
-
-        with record_leaks(_om_classes) as rec:
-            for i in range(3):
-                p_color = run_opt(om.ScipyOptimizeDriver, 'auto', optimizer='SLSQP',
-                                  dynamic_total_coloring=True, partial_coloring=True)
-                p_color = None
-
-        if len(rec) > 0:
-            msgs = []
-            for o, refs in rec:
-                try:
-                    n = o.__name__
-                except AttributeError:
-                    n = ''
-                msgs.append('Retained: {} {}'.format(n, type(o)))
-
-                for r in refs:
-                    try:
-                        nr = r.__name__
-                    except AttributeError:
-                        nr = ''
-                    msgs.append("   referred to by: {} {}".format(nr, type(r)))
-
-            self.fail('\n'.join(msgs))
+    def test_leaks_scipy_slsqp(self):
+        self._check_leaks(om.ScipyOptimizeDriver, 'SLSQP')
 
 
 if __name__ == '__main__':
