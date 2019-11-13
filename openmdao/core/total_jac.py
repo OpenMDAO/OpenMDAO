f10b60e0828fadd571304fbf1bbfe214253db1be
Danny Kilkenny <danny.kilkenny18@gmail.com>
2019-11-13 10:15:11 -0500

    implemented fwd and reverse modes

--- openmdao/core/total_jac.py
+++ openmdao/core/total_jac.py
@@ -1198,6 +1198,39 @@ class _TotalJacInfo(object):
         for matmat_idxs in inds:
             self.matmat_jac_setter(matmat_idxs, mode)
 
+    def local_indices_rev(self, inds):
+        # _col_var_sizes is the size of width of the columns in each of the _col_vars
+        # _fwd[0] contains the list of sub_indices that need to be converted to local
+        # Ex. 3 will turn into [0, 2, 4, 6, 8]
+        # Don't forget to do a version with reverse. Which the sizes will correspond to the rows
+        col_names = self.idx_iter_dict[self.mode]['@simul_coloring'][0]['coloring']._col_vars
+        col_sizes = self.idx_iter_dict[self.mode]['@simul_coloring'][0]['coloring']._col_var_sizes
+
+        row_names = self.idx_iter_dict[self.mode]['@simul_coloring'][0]['coloring']._row_vars
+        row_sizes = self.idx_iter_dict[self.mode]['@simul_coloring'][0]['coloring']._row_var_sizes
+
+        names_array = np.zeros((sum(row_sizes), 1), dtype=object)
+        local_positions_array = np.zeros((sum(row_sizes), len(row_names)), dtype=int)
+
+        names = []
+        local_indices = []
+        for i, j in zip(row_names, row_sizes):
+            names.append(np.repeat(i, j))
+            local_indices.append(np.arange(j))
+
+        names_list = np.concatenate(names).ravel().tolist()
+        local_list = np.concatenate(local_indices).ravel().tolist()
+        names_array[:, 0] = names_list
+        local_positions_array[:, 0] = local_list
+
+        if isinstance(inds, (list)):
+            idx_name = names_array[inds[0], 0]
+        else:
+            idx_name = names_array[inds, 0]
+        idx = local_positions_array[inds, 0]
+
+        return idx_name, idx
+
     def local_indices(self, inds):
         # _col_var_sizes is the size of width of the columns in each of the _col_vars
         # _fwd[0] contains the list of sub_indices that need to be converted to local
@@ -1209,8 +1242,8 @@ class _TotalJacInfo(object):
         row_names = self.idx_iter_dict[self.mode]['@simul_coloring'][0]['coloring']._row_vars
         row_sizes = self.idx_iter_dict[self.mode]['@simul_coloring'][0]['coloring']._row_var_sizes
 
-        names_array = np.zeros((len(row_vars), sum(col_sizes)), dtype=object)
-        local_positions_array = np.zeros((len(row_vars), sum(col_sizes)), dtype=int)
+        names_array = np.zeros((len(row_names), sum(col_sizes)), dtype=object)
+        local_positions_array = np.zeros((len(row_names), sum(col_sizes)), dtype=int)
 
         names = []
         local_indices = []
@@ -1277,7 +1310,11 @@ class _TotalJacInfo(object):
                             varlist = '(' + ', '.join([name for name in par_deriv[key]]) + ')'
                             print('Solving color:', key, varlist)
                         else:
-                            if key == '@simul_coloring':
+                            if key == '@simul_coloring' and mode=='rev':
+                                local_inds = self.local_indices_rev(inds)
+                                print('In mode: {0}, Solving variable: {1}\n'
+                                      'Sub Indices: {1} {2}'.format(mode, local_inds[0], local_inds[1]))
+                            elif key == '@simul_coloring' and mode=='fwd':
                                 local_inds = self.local_indices(inds)
                                 print('In mode: {0}, Solving variable: {1}\n'
                                       'Sub Indices: {1} {2}'.format(mode, local_inds[0], local_inds[1]))
