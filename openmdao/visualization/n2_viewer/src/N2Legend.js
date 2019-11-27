7c3cd821268520dfcbd331ff905eac41a7247ecc
Tad Kollar <tad.kollar@nasa.gov>
2019-11-27 12:19:38 -0500

    Finished refactoring the N2 legend code

--- openmdao/visualization/n2_viewer/src/N2Legend.js
+++ openmdao/visualization/n2_viewer/src/N2Legend.js
@@ -1,23 +1,43 @@
+/**
+ * Draw a box under the diagram describing each of the element types.
+ * @typedef N2Legend
+ * @property {String} title The label to put at the top of the legend.
+ * @property {Boolean} shown Whether the legend is currently drawn or not.
+ */
 class N2Legend {
+    /**
+     * Initializes the legend object, but doesn't draw it yet.
+     * @param {String} [title = "LEGEND"] The label at the top of the legend box.
+     */
     constructor(title = "LEGEND") {
-        this.numColumns = 3;
-        this.elementSize = 30;
-        this.xOffset = 10;
-        this.columnWidth = 250;
         this.title = title;
+        this.shown = false; // Not shown until show() is called
 
-        this.dims = {
-            'width': this.columnWidth * this.numColumns,
-            'height': 500,
-            'x': this.elementSize * .5,
-            'y': this.elementSize * .5
+        this._elementSize = 30; // The base width & height of each legend item
+        this._xOffset = 10; // Left-margin width
+        this._columnWidth = 250; // The width of each column in pixels
+
+        // Overall legend sizes
+        let numColumns = 3;
+        this._width = this._columnWidth * numColumns;
+        this._height = 500;
+
+        // Individual element sizes
+        this._elemDims = {
+            'x': this._elementSize * .5,
+            'y': this._elementSize * .5,
+            'topLeft': { 'x': this._elementSize * -.5, 'y': this._elementSize * -.5 },
+            'bottomRight': { 'x': this._elementSize * .5, 'y': this._elementSize * .5 },
+            'size': { 'width': this._elementSize, 'height': this._elementSize }
         }
 
-        this.div = d3.select("div#legend")
-            .style("width", this.dims.width + "px")
-            .style("height", this.dims.height + "px");
+        // The HTML parent element to draw in.
+        this._div = d3.select("div#legend")
+            .style("width", this._width + "px")
+            .style("height", this._height + "px");
 
-        this.colors = [
+        // Colors of each variable/system type
+        this._colors = [
             { 'name': "Group", 'color': N2Style.color.group },
             { 'name': "Component", 'color': N2Style.color.component },
             { 'name': "Input", 'color': N2Style.color.param },
@@ -27,46 +47,70 @@ class N2Legend {
             { 'name': "Collapsed", 'color': N2Style.color.collapsed },
             { 'name': "Connection", 'color': N2Style.color.connection }
         ];
+
+        // Name and renderer object for each symbol type
+        let symColor = N2Style.color.unknownExplicit;
+        this._symbols = [
+            { 'name': 'Scalar', 'cell': new N2ScalarCell(symColor) },
+            { 'name': 'Vector', 'cell': new N2VectorCell(symColor) },
+            { 'name': 'Collapsed variables', 'cell': new N2GroupCell(symColor) }
+        ];
     }
 
-    _createElementBorder(g) {
-        g.append("rect")
-            .attr("x", -this.dims.x)
-            .attr("y", -this.dims.y)
-            .attr("width", this.elementSize)
-            .attr("height", this.elementSize)
+    /**
+     * Create a square, white border for a symbol.
+     * @param {Array} svgGrp D3 selection of a previously-created SVG <g> element.
+     */
+    _createElementBorder(svgGrp) {
+        svgGrp.append("rect")
+            .attr("x", -this._elemDims.x)
+            .attr("y", -this._elemDims.y)
+            .attr("width", this._elemDims.size.width)
+            .attr("height", this._elemDims.size.height)
             .style("stroke-width", 2)
             .style("stroke", "white")
             .style("fill", "none");
     }
 
-    _createText(g, text) {
-        g.append("svg:text")
-            .attr("x", u + 5)
+    /**
+     * Add SVG text label to the legend.
+     * @param {Array} svgGrp D3 selection of a previously-created SVG <g> element.
+     * @param {String} label The label to insert.
+     */
+    _createText(svgGrp, label) {
+        svgGrp.append("svg:text")
+            .attr("x", this._elemDims.x + 5)
             .attr("y", 0)
             .attr("dy", ".35em")
             .attr("font-size", 20)
-            .text(text)
+            .text(label)
             .style("fill", "black");
     }
 
-    _drawLegendColor(g, color, justUpdate) {
-        let shape = justUpdate ? g.select(".colorMid") :
-            g.append("rect").attr("class", "colorMid").style("fill", color);
+    /**
+     * Add a colored rectangle to the legend.
+     * @param {Array} svgGrp D3 selection of a previously-created SVG <g> element.
+     * @param {String} color The value of the fill color.
+     */
+    _drawLegendColor(svgGrp, color) {
+        let shape = svgGrp.append("rect")
+            .attr("class", "colorMid")
+            .style("fill", color);
 
-        return shape.attr("x", -this.dims.x)
-            .attr("y", -this.dims.y)
-            .attr("width", this.dims.x * 2)
-            .attr("height", this.dims.y * 2)
+        return shape.attr("x", -this._elemDims.x)
+            .attr("y", -this._elemDims.y)
+            .attr("width", this._elemDims.x * 2)
+            .attr("height", this._elemDims.y * 2)
             .style("stroke-width", 0)
             .style("fill-opacity", 1);
     }
 
+    /** Create an SVG group and text element containing the main title. */
     _addMainTitle() {
-        let el = this.svg.append("g")
-            .attr("transform", "translate(" + (legendWidth * .5) + ",15)");
+        let titleGrp = this.svg.append("g")
+            .attr("transform", "translate(" + (this._width * .5) + ",15)");
 
-        el.append("svg:text")
+        titleGrp.append("svg:text")
             .attr("text-anchor", "middle")
             .attr("dy", ".35em")
             .attr("font-size", 30)
@@ -75,6 +119,10 @@ class N2Legend {
             .style("fill", "black");
     }
 
+    /**
+     * For each column, create an SVG group and text element with its title.
+     * @param {Boolean} showLinearSolverNames Whether linear or non-linear solvers are viewed.
+     */
     _addColumnTitles(showLinearSolverNames) {
         let titles = [
             "Systems & Variables",
@@ -83,11 +131,11 @@ class N2Legend {
         ];
 
         for (let i = 0; i < titles.length; ++i) {
-            let el = this.svg.append("g")
+            let columnTitleGrp = this.svg.append("g")
                 .attr("transform", "translate(" +
-                    (this.columnWidth * i + this.xOffset) + ",60)");
+                    (this._columnWidth * i + this._xOffset) + ",60)");
 
-            el.append("svg:text")
+            columnTitleGrp.append("svg:text")
                 .attr("dy", ".35em")
                 .attr("font-size", 24)
                 .attr("text-decoration", "underline")
@@ -96,49 +144,106 @@ class N2Legend {
         }
     }
 
+    /** Draw a colored rectangle for each system/variable type in the first column. */
     _drawColors() {
         let i = 0;
 
-        for (let color of this.colors) {
+        for (let color of this._colors) {
             let el = this.svg.append("g")
                 .attr("transform", "translate(" +
-                    (this.xOffset + this.dims.x) + "," +
-                    (80 + i + this.dims.y) + ")");
-            this.drawLegendColor(el, color.color, false);
-            this.createText(el, color.name);
+                    (this._xOffset + this._elemDims.x) + "," +
+                    (80 + i + this._elemDims.y) + ")");
+            this._drawLegendColor(el, color.color, false);
+            this._createText(el, color.name);
             i += 40;
         }
     }
 
+    /** Render a shape for each symbol type in the second column. */
     _drawSymbols() {
-        var text = ["Scalar", "Vector", "Collapsed variables"];
-        var colors = [N2Style.color.unknownExplicit, N2Style.color.unknownExplicit, N2Style.color.unknownExplicit];
-        var shapeFunctions = [DrawScalar, DrawVector, DrawGroup];
-        for (var i = 0; i < text.length; ++i) {
-            var el = svg_legend.append("g").attr("transform", "translate(" + (columnWidth * 1 + xOffset + u) + "," + (80 + 40 * i + v) + ")");
-            shapeFunctions[i](el, u, v, colors[i], false);
-            CreateElementBorder(el);
-            CreateText(el, text[i]);
+        let yOffset = 0;
+
+        for (let sym of this._symbols) {
+            let svgGrp = this.svg.append("g")
+                .attr("transform", "translate(" + (this._columnWidth + this._xOffset
+                    + this._elemDims.x) + "," + (80 + yOffset + this._elemDims.y) + ")");
+
+            sym.cell.render(svgGrp.node(), this._elemDims, true);
+
+            this._createElementBorder(svgGrp);
+            this._createText(svgGrp, sym.name);
+            yOffset += 40;
         }
     }
 
-    show(showLinearSolverNames) {
-        // Clear anything that might already be in there
-        this.div.node().innerHTML = "";
+    /**
+     * Render a colored rectangle for each solver in the third column, depending
+     * on whether linear or non-linear ones are selected.
+     * @param {Boolean} showLinearSolverNames Determines solver name type displayed.
+     * @param {Object} solverStyles Solver names, types, and styles including color.
+     */
+    _drawSolverColors(showLinearSolverNames, solverStyles) {
+        let yOffset = 0;
+        for (let solverName in solverStyles) {
+            let solver = solverStyles[solverName];
+            if ((solver.type == 'linear' && showLinearSolverNames) ||
+                (solver.type == 'nonLinear' && !showLinearSolverNames)) {
+                let el = this.svg.append("g")
+                    .attr("transform", "translate(" + (this._columnWidth * 2 + this._xOffset +
+                        this._elemDims.x) + "," + (80 + yOffset + this._elemDims.y) + ")");
+                this._drawLegendColor(el, solver.style.fill, false);
+                this._createText(el, solverName);
+                yOffset += 40;
+            }
+        }
+    }
 
-        this.svg = this.div.append("svg:svg")
+    /** Remove all legend SVG elements. */
+    _clear() {
+        this._div.node().innerHTML = "";
+    }
+
+    /** Remove all legend SVG elements and mark as not shown. */
+    hide() {
+        this._clear();
+        this.shown = false;
+    }
+
+    /**
+     * Remove any existing SVG elements, create a new SVG element with
+     * a background rect, and add all of the other pieces.
+     * @param {Boolean} showLinearSolverNames Determines solver name type displayed.
+     * @param {Object} solverStyles Solver names, types, and styles including color.
+     */
+    show(showLinearSolverNames, solverStyles) {
+        // Clear anything that might already be in there
+        this._clear();
+        this.shown = true;
+
+        this.svg = this._div.append("svg:svg")
             .attr("id", "legendSVG")
-            .attr("width", this.dims.width)
-            .attr("height", this.dims.height);
+            .attr("width", this._width)
+            .attr("height", this._height);
 
         this.svg.append("rect")
             .attr("class", "background")
-            .attr("width", this.dims.width)
-            .attr("height", this.dims.height);
+            .attr("width", this._width)
+            .attr("height", this._height);
 
         this._addMainTitle();
         this._addColumnTitles(showLinearSolverNames);
         this._drawColors();
+        this._drawSymbols();
+        this._drawSolverColors(showLinearSolverNames, solverStyles);
+    }
 
+    /**
+     * If legend is shown, hide it; if it's hidden, show it.
+     * @param {Boolean} showLinearSolverNames Determines solver name type displayed.
+     * @param {Object} solverStyles Solver names, types, and styles including color.
+     */
+    toggle(showLinearSolverNames, solverStyles) {
+        if (this.shown) this.hide();
+        else this.show(showLinearSolverNames, solverStyles);
     }
 }
