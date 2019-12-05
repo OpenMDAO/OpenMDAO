40c0e5e79d1126b93db510349b6682d55b584e7f
relf <remi.lafage@onera.fr>
2019-12-05 11:37:47 +0100

    update XDSMjs for XDSM v2 support

--- openmdao/visualization/xdsm_viewer/html_writer.py
+++ openmdao/visualization/xdsm_viewer/html_writer.py
@@ -7,8 +7,13 @@ import os
 
 from six import itervalues
 
-from openmdao.visualization.html_utils import read_files, write_div, head_and_body, write_script, \
-    write_style
+from openmdao.visualization.html_utils import (
+    read_files,
+    write_div,
+    head_and_body,
+    write_script,
+    write_style,
+)
 
 _DEFAULT_JSON_FILE = "xdsm.json"  # Used as default name if data is not embedded
 _CHAR_SET = "utf-8"  # HTML character set
@@ -38,63 +43,65 @@ def write_html(outfile, source_data=None, data_file=None, embeddable=False):
     """
     # directories
     main_dir = os.path.dirname(os.path.abspath(__file__))
-    code_dir = os.path.join(main_dir, 'XDSMjs')
+    code_dir = os.path.join(main_dir, "XDSMjs")
     build_dir = os.path.join(code_dir, "build")
     style_dir = code_dir  # CSS
 
     with open(os.path.join(build_dir, "xdsm.bundle.js"), "r") as f:
         code = f.read()
-        xdsm_bundle = write_script(code, {'type': 'text/javascript'})
+        xdsm_bundle = write_script(code, {"type": "text/javascript"})
 
-    xdsm_attrs = {'class': 'xdsm'}
+    xdsm_attrs = {"class": "xdsm2"}
     # grab the data
     if data_file is not None:
         # Add name of the data file
-        xdsm_attrs['data-mdo-file'] = data_file
+        xdsm_attrs["data-mdo-file"] = data_file
     elif source_data is not None:
         if isinstance(source_data, (dict, str)):
             data_str = str(source_data)  # dictionary converted to string
         else:
-            msg = ('Invalid data type for source data: {} \n'
-                   'The source data should be a JSON file name or a dictionary.')
+            msg = (
+                "Invalid data type for source data: {} \n"
+                "The source data should be a JSON file name or a dictionary."
+            )
             raise ValueError(msg.format(type(source_data)))
 
         # Replace quote marks for the HTML syntax
         for i in ('u"', "u'", '"', "'"):  # quote marks and unicode prefixes
-            data_str = data_str.replace(i, r'&quot;')
-        xdsm_attrs['data-mdo'] = data_str
+            data_str = data_str.replace(i, r"&quot;")
+        xdsm_attrs["data-mdo"] = data_str
     else:  # both source data and data file name are None
         msg = 'Specify either "source_data" or "data_file".'
         raise ValueError(msg.format(type(source_data)))
 
     # grab the style
-    styles = read_files(('fontello', 'xdsm'), style_dir, 'css')
-    styles_elem = write_style(content='\n\n'.join(itervalues(styles)))
+    styles = read_files(("fontello", "xdsm"), style_dir, "css")
+    styles_elem = write_style(content="\n\n".join(itervalues(styles)))
 
     # put all style and JS into index
-    toolbar_div = write_div(attrs={'class': 'xdsm-toolbar'})
+    toolbar_div = write_div(attrs={"class": "xdsm-toolbar"})
     xdsm_div = write_div(attrs=xdsm_attrs)
-    body = '\n\n'.join([toolbar_div, xdsm_div])
+    body = "\n\n".join([toolbar_div, xdsm_div])
 
     if embeddable:
-        index = '\n\n'.join([styles_elem, xdsm_bundle, body])
+        index = "\n\n".join([styles_elem, xdsm_bundle, body])
     else:
         meta = '<meta charset="{}">'.format(_CHAR_SET)
 
-        head = '\n\n'.join([meta, styles_elem, xdsm_bundle])
+        head = "\n\n".join([meta, styles_elem, xdsm_bundle])
 
-        index = head_and_body(head, body, attrs={'class': "js", 'lang': ""})
+        index = head_and_body(head, body, attrs={"class": "js", "lang": ""})
 
     # Embed style, scripts and data to HTML
-    with open(outfile, 'w') as f:
+    with open(outfile, "w") as f:
         f.write(index)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # with JSON file name as input
-    write_html(outfile='xdsmjs/xdsm_diagram.html', source_data="examples/idf.json")
+    write_html(outfile="xdsmjs/xdsm_diagram.html", source_data="examples/idf.json")
 
     # with JSON data as input
     with open("XDSMjs/examples/idf.json") as f:
         data = json.load(f)
-    write_html(outfile='xdsm_diagram_data_embedded.html', source_data=data)
+    write_html(outfile="xdsm_diagram_data_embedded.html", source_data=data)
