f78341b19a4008e3dcfee34443c459672a06e241
Herb Schilling <hschilling@nasa.gov>
2019-12-10 10:10:44 -0500

    Backing out google option for pydocstyle because:the 'google' option is not supported in older versions of pydocstyle (<3.x) but we can't update our install_requires yet because newer versions aren't supported in python 2.7

--- openmdao/code_review/test_lint_peps.py
+++ openmdao/code_review/test_lint_peps.py
@@ -117,11 +117,7 @@ class LintTestCase(unittest.TestCase):
 
     @unittest.skipUnless(pydocstyle, "requires 'pydocstyle', install openmdao[test]")
     def test_pep257(self):
-        failures = [str(fail) for fail in pydocstyle.check(_get_files(),
-                                                           ignore=ignores['pep257'] + \
-                                                            list(pydocstyle.conventions['google'])
-                                                           )
-                    ]
+        failures = [str(fail) for fail in pydocstyle.check(_get_files(), ignore=ignores['pep257']) ]
         if failures:
             self.fail('{} PEP 257 Failure(s):\n'.format(len(failures)) + '\n'.join(failures))
 
