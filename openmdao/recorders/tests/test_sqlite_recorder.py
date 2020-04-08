3a604c5148d385bc0e231adfe7c76c2bc0ae8c05
Herb Schilling <hschilling@nasa.gov>
2020-04-08 08:19:48 -0400

    Removed commented out code that we don't need

--- openmdao/recorders/tests/test_sqlite_recorder.py
+++ openmdao/recorders/tests/test_sqlite_recorder.py
@@ -36,25 +36,6 @@ if OPTIMIZER:
     from openmdao.drivers.pyoptsparse_driver import pyOptSparseDriver
 
 
-# class ParaboloidProblem(om.Problem):
-#     """
-#     Paraboloid problem with Constraint.
-#     """
-#
-#     def __init__(self):
-#         super(ParaboloidProblem, self).__init__()
-#
-#         model = self.model
-#         model.add_subsystem('p1', om.IndepVarComp('x', 50.0), promotes=['*'])
-#         model.add_subsystem('p2', om.IndepVarComp('y', 50.0), promotes=['*'])
-#         model.add_subsystem('comp', Paraboloid(), promotes=['*'])
-#         model.add_subsystem('con', om.ExecComp('c = - x + y'), promotes=['*'])
-#
-#         model.add_design_var('x', lower=-50.0, upper=50.0)
-#         model.add_design_var('y', lower=-50.0, upper=50.0)
-#         model.add_objective('f_xy')
-#         model.add_constraint('c', upper=-15.0)
-
 class Cycle(om.Group):
 
     def setup(self):
