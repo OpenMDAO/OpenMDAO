0a8b4f148ae42f1908b663a1c6ba68a65d65340f
Herb Schilling <hschilling@nasa.gov>
2020-06-08 14:33:42 -0400

    When OPENMDAO_REQUIRE_MPI is set to true, force using MPI or fail

--- openmdao/solvers/linear/petsc_ksp.py
+++ openmdao/solvers/linear/petsc_ksp.py
@@ -6,13 +6,12 @@ import sys
 
 from openmdao.solvers.solver import LinearSolver
 
+# If OPENMDAO_REQUIRE_MPI is set to a recognized positive value, attempt import
+# and raise exception on failure. If set to anything else, no import is attempted.
 if 'OPENMDAO_REQUIRE_MPI' in os.environ:
     if os.environ['OPENMDAO_REQUIRE_MPI'].lower() in ['always', '1', 'true', 'yes']:
-        try:
-            import petsc4py
-            from petsc4py import PETSc
-        except ImportError:
-            PETSc = None
+        import petsc4py
+        from petsc4py import PETSc
     else:
         PETSc = None
 # If OPENMDAO_REQUIRE_MPI is unset, attempt to import petsc4py, but continue on failure
