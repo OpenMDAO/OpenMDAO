2186adb1ba66e0babaad8d2e6c7da071e1c6e973
Neil Wu <neilwu@umich.edu>
2020-10-26 13:37:01 -0400

    split up lines so max char < 100

--- openmdao/drivers/pyoptsparse_driver.py
+++ openmdao/drivers/pyoptsparse_driver.py
@@ -50,8 +50,10 @@ DEFAULT_OPT_SETTINGS['IPOPT'] = {
 }
 
 CITATIONS = """@article{Wu_pyoptsparse_2020,
-    author = {Neil Wu and Gaetan Kenway and Charles A. Mader and John Jasa and Joaquim R. R. A. Martins},
-    title = {{pyOptSparse:} A {Python} framework for large-scale constrained nonlinear optimization of sparse systems},
+    author = {Neil Wu and Gaetan Kenway and Charles A. Mader and John Jasa and
+     Joaquim R. R. A. Martins},
+    title = {{pyOptSparse:} A {Python} framework for large-scale constrained
+     nonlinear optimization of sparse systems},
     journal = {Journal of Open Source Software},
     volume = {5},
     number = {54},
