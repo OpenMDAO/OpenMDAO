69f6ef0388ea74da4a1537fd1a4bb2c49ffeffee
Bret Naylor <naylor.b@gmail.com>
2020-11-09 13:01:03 -0500

    fixed test

--- openmdao/core/tests/test_prob_remote.py
+++ openmdao/core/tests/test_prob_remote.py
@@ -212,10 +212,12 @@ class ProbRemoteTestCase(unittest.TestCase):
             # get_remote=None
             #
 
-            assert_near_equal(prob.get_val('dst.x', get_remote=False), [7.])  # from src ('ivc.x')
+            with self.assertRaises(RuntimeError) as cm:
+                prob['dst.x']
+            self.assertEqual(str(cm.exception), distrib_msg.format(name='dst.x'))
 
             with self.assertRaises(RuntimeError) as cm:
-                assert_near_equal(prob.model.get_val('dst.x', get_remote=None, from_src=False), [7.])
+                prob.model.get_val('dst.x', get_remote=None, from_src=False)
             self.assertEqual(str(cm.exception), distrib_msg.format(name='dst.x'))
 
             with self.assertRaises(RuntimeError) as cm:
@@ -268,11 +270,12 @@ class ProbRemoteTestCase(unittest.TestCase):
             # get_remote=None
             #
 
-            # distributed should raise exception, except for input with from_src=True (the default)
-            assert_near_equal(prob.get_val('dst.x', get_remote=False), [7.])  # from src ('ivc.x')
+            with self.assertRaises(RuntimeError) as cm:
+                prob['dst.x']
+            self.assertEqual(str(cm.exception), distrib_msg.format(name='dst.x'))
 
             with self.assertRaises(RuntimeError) as cm:
-                assert_near_equal(prob.model.get_val('dst.x', get_remote=None, from_src=False), [7.])
+                prob.model.get_val('dst.x', get_remote=None, from_src=False)
             self.assertEqual(str(cm.exception), distrib_msg.format(name='dst.x'))
 
             with self.assertRaises(RuntimeError) as cm:
