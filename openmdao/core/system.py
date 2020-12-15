692f2fd0370cb36641b430d85a8f1284388ac68a
Danny Kilkenny <danny.kilkenny18@gmail.com>
2020-12-15 10:27:06 -0500

    Fixed issue where resid_tol failed with vectorized resids

--- openmdao/core/system.py
+++ openmdao/core/system.py
@@ -3632,8 +3632,14 @@ class System(object):
             impl_outputs = {}
             if residuals_tol:
                 for n, m in outputs.items():
-                    if "resids" in m and n in states and m['resids'] > residuals_tol:
-                        impl_outputs[n] = m
+                    if "resids" in m and n in states:
+                        if isinstance(m['resids'], np.ndarray):
+                            for i in m['resids']:
+                                if i > residuals_tol:
+                                    impl_outputs[n] = m
+                                    break
+                        else:
+                            impl_outputs[n] = m
             else:
                 impl_outputs = {n: m for n, m in outputs.items() if n in states}
             if out_stream:
