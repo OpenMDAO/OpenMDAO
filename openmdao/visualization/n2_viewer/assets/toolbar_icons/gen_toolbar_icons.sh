5590519b564d02bc4aff746dab99ee9084023964
Tad Kollar <tad.kollar@nasa.gov>
2021-02-19 15:20:07 -0500

    Added docs to the script and generated css file

--- openmdao/visualization/n2_viewer/assets/toolbar_icons/gen_toolbar_icons.sh
+++ openmdao/visualization/n2_viewer/assets/toolbar_icons/gen_toolbar_icons.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+#############################################################################
+# The script finds all of the .svg files in the toolbar_icons directory and
+# generates font and css files for them. These are copied to the
+# n2_viewer/styles directory, overwriting the existing files. It's left to
+# the developer to "git add" those two files and commit the changes.
+#
+# This is a replacement for the icomoon website method used previously. That
+# method required a large number of manual steps and produced inconsistent
+# results.
+#
+# Usage:
+#   ./gen_toolbar_icons.sh  [while in the toolbar_icons directory]
+#
+#############################################################################
 GLYPHS2FONT_REPO=https://github.com/rse/glyphs2font.git
 TEMPLATE=n2toolbar-font-template.yml
 CONFIG=n2toolbar-font.yml
 WORKDIR=work
 STYLE_RELDIR=../../style
 PREFIX=n2toolbar-icons
+TARGET_CSS=../${STYLE_RELDIR}/${PREFIX}.css
 
 # Check for Node.js
 if which npm > /dev/null 2>&1; then
@@ -55,11 +70,31 @@ done
 
 glyphs2font $CONFIG
 
-# Chop out the part of the file we don't need, and change one value.
+# Initialize the css file with some docs.
+cat>$TARGET_CSS<<EODOC
+/** n2toolbar-icons.css ****************************************************
+ *
+ * AUTOMATICALLY GENERATED FILE - DO NOT EDIT DIRECTLY
+ *
+ * This file links unicode characters to styles for use as N2 icons.
+ *
+ * To generate it, cd to the N2 assets/toolbar_icons directory, and run
+ *   ./gen_toolbar_icons.sh
+ *
+ * This file and the n2toolbar-icons-font.woff file will be overwritten.
+ ***************************************************************************
+ */
+EODOC
+
+# Remove parts of the file we don't need, and change the value of font-size.
 # Output the css and woff files to the style dir.
-sed -n 's#font-size:.*inherit;#font-size:       14px/1#;20,1000p' ${PREFIX}.css > ../${STYLE_RELDIR}/${PREFIX}.css
+sed -n 's#font-size:.*inherit;#font-size:       14px/1#;20,1000p' ${PREFIX}.css >> $TARGET_CSS
 cp -v ${PREFIX}-font.woff ../${STYLE_RELDIR}/
 
 echo "Finished with working directory."
 popd > /dev/null
+
+rm -fr $WORKDIR
+
+echo "Be sure to git add the new .css and .woff files before committing!"
 
