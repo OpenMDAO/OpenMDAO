259b902f80f2b55480f652c66a13cb81a9bc25bf
Kenneth T. Moore <kenneth.t.moore-1@nasa.gov>
2021-05-17 14:27:44 -0400

    Review:Steve

--- openmdao/docs/build_jupyter_book.py
+++ openmdao/docs/build_jupyter_book.py
@@ -2,39 +2,13 @@
 import argparse
 import os
 import pathlib
-import fnmatch
-import shutil
+
+from copy_build_artifacts import copy_build_artifacts
 
 
 _this_file = pathlib.Path(__file__).resolve()
 REPO_ROOT = _this_file.parent
 BOOK_DIR = pathlib.Path(REPO_ROOT, 'openmdao_book')
-
-
-def copy_build_artifacts(book_dir=BOOK_DIR):
-    """
-    Copy build artifacts (html files, images, etc) to the output _build directory.
-    Parameters
-    ----------
-    book_dir : str
-        The directory containing the Jupyter-Book to be created.
-    """
-    PATTERNS_TO_COPY = ('*.html', '*.png')
-    TARGET_DIR = '_build'
-    EXCLUDE_DIRS = ('_build', '.ipynb_checkpoints')
-
-    for dirpath, dirs, files in os.walk(book_dir, topdown=True):
-        dirs[:] = [d for d in dirs if d not in EXCLUDE_DIRS]
-        rel_path = pathlib.PurePath(dirpath).parts[1:]
-        target_path = pathlib.PurePath(book_dir, TARGET_DIR, 'html', *rel_path)
-        files_to_copy = set()
-        for pattern in PATTERNS_TO_COPY:
-            files_to_copy |= set(fnmatch.filter(files, pattern))
-        for f in files_to_copy:
-            src = pathlib.PurePath(dirpath, f)
-            dst = pathlib.PurePath(target_path, f)
-            os.makedirs(os.path.dirname(dst), exist_ok=True)
-            shutil.copyfile(src, dst)
 
 
 def build_book(book_dir=BOOK_DIR, clean=True):
