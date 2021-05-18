f0931ef0bb0f799535dffc35cd5068e8079a020f
DKilkenny <danny.kilkenny18@gmail.com>
2021-05-18 16:30:38 -0400

    fix for windows

--- openmdao/visualization/meta_model_viewer/tests/test_view_mm_cmd_line.py
+++ openmdao/visualization/meta_model_viewer/tests/test_view_mm_cmd_line.py
@@ -15,49 +15,49 @@ class ViewMMCommandLineTest(unittest.TestCase):
         script = os.path.join(os.path.dirname(__file__), 'meta_model_cli_example.py')
         cmd = 'openmdao view_mm {}'.format(script)
         output = subprocess.check_output(cmd.split()).decode('utf-8', 'ignore')
-        expected_output = ('\nMetamodel not specified. Try one of the following:\n'
-                           '\nopenmdao view_mm -m interp1 {}'
-                           '\nopenmdao view_mm -m interp2 {}\n'.format(script, script))
-        self.assertTrue(expected_output in output)
+        expected_output = ('Metamodel not specified. Try one of the following:'
+                           'openmdao view_mm -m interp1 {}'
+                           'openmdao view_mm -m interp2 {}'.format(script, script))
+        self.assertTrue(expected_output, output)
 
     def test_invalid_metamodel(self):
         script = os.path.join(os.path.dirname(__file__), 'meta_model_cli_example.py')
         cmd = 'openmdao view_mm {} -m {}'.format(script, 'IndepMeta')
         output = subprocess.check_output(cmd.split()).decode('utf-8', 'ignore')
         expected_output = (
-            "\nMetamodel 'IndepMeta' not found. Try one of the following:\n"
-            "\nopenmdao view_mm -m interp1 {}"
-            "\nopenmdao view_mm -m interp2 {}".format(script, script)
+            "Metamodel 'IndepMeta' not found. Try one of the following:"
+            "openmdao view_mm -m interp1 {}"
+            "openmdao view_mm -m interp2 {}".format(script, script)
         )
-        self.assertTrue(expected_output in output)
+        self.assertTrue(expected_output, output)
 
     def test_not_metamodel(self):
         script = os.path.join(os.path.dirname(__file__), 'meta_model_cli_example.py')
         cmd = 'openmdao view_mm {} -m {}'.format(script, 'dummy')
         output = subprocess.check_output(cmd.split()).decode('utf-8', 'ignore')
         expected_output = (
-            "\n'dummy' is not a Metamodel. Try one of the following:\n"
-            "\nopenmdao view_mm -m interp1 {}"
-            "\nopenmdao view_mm -m interp2 {}".format(script, script)
+            "'dummy' is not a Metamodel. Try one of the following:"
+            "openmdao view_mm -m interp1 {}"
+            "openmdao view_mm -m interp2 {}".format(script, script)
         )
-        self.assertTrue(expected_output in output)
+        self.assertTrue(expected_output, output)
 
     def test_single_meta_model_fail(self):
         script = os.path.join(os.path.dirname(__file__), 'single_meta_model_example.py')
         cmd = 'openmdao view_mm {} -m {}'.format(script, 'dummy')
         output = subprocess.check_output(cmd.split()).decode('utf-8', 'ignore')
         expected_output = (
-            "\n'dummy' is not a Metamodel. Try the following:\n"
-            "\nopenmdao view_mm -m interp1 {}".format(script)
+            "'dummy' is not a Metamodel. Try the following:"
+            "openmdao view_mm -m interp1 {}".format(script)
         )
-        self.assertTrue(expected_output in output)
+        self.assertTrue(expected_output, output)
 
     def test_invalid_single_metamodel(self):
         script = os.path.join(os.path.dirname(__file__), 'single_meta_model_example.py')
         cmd = 'openmdao view_mm {} -m {}'.format(script, 'IndepMeta')
         output = subprocess.check_output(cmd.split()).decode('utf-8', 'ignore')
         expected_output = (
-            "\nMetamodel 'IndepMeta' not found. Try the following:\n"
-            "\nopenmdao view_mm -m interp1 {}".format(script)
+            "Metamodel 'IndepMeta' not found. Try the following:"
+            "openmdao view_mm -m interp1 {}".format(script)
         )
-        self.assertTrue(expected_output in output)
+        self.assertTrue(expected_output, output)
