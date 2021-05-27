efe0ce1b68639df7cbb18eea9ba923e59124ca11
DKilkenny <danny.kilkenny18@gmail.com>
2021-05-27 11:08:53 -0400

    Added test of a direct options dict obj

--- openmdao/utils/tests/test_notebook_utils.py
+++ openmdao/utils/tests/test_notebook_utils.py
@@ -14,8 +14,31 @@ except ImportError:
 import openmdao.api as om
 from openmdao.utils.assert_utils import assert_warning
 
+class StateOptionsDictionary(om.OptionsDictionary):
+    """
+    An OptionsDictionary specific to states.
+
+    Parameters
+    ----------
+    read_only : bool
+        If True, setting (via __setitem__ or update) is not permitted.
+    """
+    def __init__(self, read_only=False):
+        super(StateOptionsDictionary, self).__init__(read_only)
+
+        self.declare(name='name', types=str,
+                     desc='name of ODE state variable')
+
 @unittest.skipUnless(tabulate and IPython, "Tabulate and IPython are required")
 class TestNotebookUtils(unittest.TestCase):
+
+    def test_show_obj_options(self):
+        from openmdao.utils import notebook_utils
+        notebook_utils.ipy = True
+
+        options = om.show_options_table("openmdao.utils.tests.test_notebook_utils.StateOptionsDictionary")
+
+        self.assertEqual(options, None)
 
     def test_show_options_w_attr(self):
         from openmdao.utils import notebook_utils
