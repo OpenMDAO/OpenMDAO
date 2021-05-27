ffa2230d992503d0803e12f5e1a360c76674cd66
swryan <stephen.w.ryan@gmail.com>
2021-05-27 11:05:02 -0400

    add tests for return values from add_input and add_output

--- openmdao/core/tests/test_impl_comp.py
+++ openmdao/core/tests/test_impl_comp.py
@@ -114,6 +114,48 @@ class QuadraticJacVec(QuadraticComp):
             d_residuals['x'] = self.inv_jac * d_outputs['x']
 
 
+class ImplCompTestCase(unittest.TestCase):
+
+    def test_add_input_output_retval(self):
+        # check basic metadata expected in return value
+        expected = {
+            'value': 3,
+            'shape': (1,),
+            'size': 1,
+            'units': 'ft',
+            'desc': '',
+            'tags': set(),
+        }
+        expected_discrete = {
+            'value': 3,
+            'type': int,
+            'desc': '',
+            'tags': set(),
+        }
+
+        class ImplComp(om.ImplicitComponent):
+            def setup(self):
+                meta = self.add_input('x', val=3.0, units='ft')
+                for key, val in expected.items():
+                    assert meta[key] == val, f'Expected {key}: {val} but got {key}: {meta[key]}'
+
+                meta = self.add_discrete_input('x_disc', val=3)
+                for key, val in expected_discrete.items():
+                    assert meta[key] == val, f'Expected {key}: {val} but got {key}: {meta[key]}'
+
+                meta = self.add_output('y', val=3.0, units='ft')
+                for key, val in expected.items():
+                    assert meta[key] == val, f'Expected {key}: {val} but got {key}: {meta[key]}'
+
+                meta = self.add_discrete_output('y_disc', val=3)
+                for key, val in expected_discrete.items():
+                    assert meta[key] == val, f'Expected {key}: {val} but got {key}: {meta[key]}'
+
+        prob = om.Problem()
+        prob.model.add_subsystem('comp', ImplComp())
+        prob.setup()
+
+
 class ImplicitCompTestCase(unittest.TestCase):
 
     def setUp(self):
