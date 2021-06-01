3ccfa0c089f7101fc1354be7bedee91f264a2ca4
Kenneth T. Moore <kenneth.t.moore-1@nasa.gov>
2021-06-01 18:55:43 -0400

    adding a few back in 5.5

--- openmdao/drivers/tests/test_pyoptsparse_driver.py
+++ openmdao/drivers/tests/test_pyoptsparse_driver.py
@@ -161,7 +161,6 @@ class TestNotInstalled(unittest.TestCase):
 
 
 @unittest.skipUnless(MPI, "MPI is required.")
-@use_tempdirs
 class TestMPIScatter(unittest.TestCase):
     N_PROCS = 2
 
@@ -2375,7 +2374,6 @@ class TestPyoptSparseFeature(unittest.TestCase):
         assert_near_equal(prob.get_val('z', indices=0), 1.98337708, 1e-3)
 
 
-@use_tempdirs
 class TestPyoptSparseSnoptFeature(unittest.TestCase):
     # All of these tests require SNOPT
 
--- openmdao/recorders/tests/test_sqlite_recorder.py
+++ openmdao/recorders/tests/test_sqlite_recorder.py
@@ -3218,10 +3218,10 @@ class TestFeatureSqliteRecorder(unittest.TestCase):
         assert_near_equal(y_recorded, y1)
 
 
+@use_tempdirs
 class TestFeatureAdvancedExample(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
+    def setUp(self):
 
         # build the model
         prob = om.Problem(model=SellarMDAWithUnits())
