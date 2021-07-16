3634201e007c8a86c1ffefb52ed5ae605f83f850
Tad Kollar <tad.kollar@nasa.gov>
2021-07-16 10:24:10 -0400

    Removed remaining pyppeteer references

--- openmdao/visualization/n2_viewer/tests/test_gui.py
+++ openmdao/visualization/n2_viewer/tests/test_gui.py
@@ -1,16 +1,7 @@
 """Test N2 GUI with multiple models using Playwright."""
-import sys
 import unittest
-import os
-import urllib.request
 
-# Make sure Python version is at least 3.6, otherwise do nothing.
-if sys.version_info > (3, 5):
-    from pyppeteer.chromium_downloader import check_chromium, download_chromium
-    from appdirs import AppDirs
-    import shutil
+from n2_gui_test import n2_gui_test_case
 
-    from n2_gui_test import n2_gui_test_case
-
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
