c147df56f477feeb79ae99b894587476e50afb4d
Tad Kollar <tad.kollar@nasa.gov>
2021-07-19 12:50:40 -0400

    Still trying to get playwright auto-install working

--- openmdao/visualization/n2_viewer/tests/test_gui.py
+++ openmdao/visualization/n2_viewer/tests/test_gui.py
@@ -1,10 +1,12 @@
 """Test N2 GUI with multiple models using Playwright."""
 import unittest
+import os
 import sys
-
-from n2_gui_test import n2_gui_test_case
 
 # Playwright requires Python 3.7 or higher
 if sys.version_info >= (3, 7):
+    os.system("playwright install")
+    from n2_gui_test import n2_gui_test_case
+
     if __name__ == "__main__":
         unittest.main()
