1233227c5d20bd3c39804db5af4e6b0aa84ba0e7
Tad Kollar <tad.kollar@nasa.gov>
2021-07-29 12:02:02 -0400

    Moved multi_proc_exception_check before if so all procs would hit it

--- openmdao/core/system.py
+++ openmdao/core/system.py
@@ -1523,24 +1523,24 @@ class System(object):
                     # assume that all but the first dimension of the shape of a
                     # distributed variable is the same on all procs
                     high_dims = local_shape[1:]
-                    if high_dims:
-                        high_size = np.prod(high_dims)
-                        dim_size_match = bool(global_size % high_size == 0)
+                    with multi_proc_exception_check(self.comm):
+                        if high_dims:
+                            high_size = np.prod(high_dims)
+                            dim_size_match = bool(global_size % high_size == 0)
 
-                        with multi_proc_exception_check(self.comm):
                             if dim_size_match is False:
                                 msg = (f"{self.msginfo}: All but the first dimension of the "
-                                       "shape's local parts in a distributed variable must match "
-                                       f"across processes. For output '{abs_name}', local shape "
-                                       f"{local_shape} in MPI rank {self.comm.rank} has a "
-                                       "higher dimension that differs in another rank.")
+                                        "shape's local parts in a distributed variable must match "
+                                        f"across processes. For output '{abs_name}', local shape "
+                                        f"{local_shape} in MPI rank {self.comm.rank} has a "
+                                        "higher dimension that differs in another rank.")
 
                                 raise RuntimeError(msg)
 
-                        dim1 = global_size // high_size
-                        mymeta['global_shape'] = tuple([dim1] + list(high_dims))
-                    else:
-                        mymeta['global_shape'] = (global_size,)
+                            dim1 = global_size // high_size
+                            mymeta['global_shape'] = tuple([dim1] + list(high_dims))
+                        else:
+                            mymeta['global_shape'] = (global_size,)
 
                 else:
                     # not distributed, just use local shape and size
