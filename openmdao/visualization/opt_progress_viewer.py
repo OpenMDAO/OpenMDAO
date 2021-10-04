b26d456aa6d1657ad9fbbdb07f2269cb5159f19c
DKilkenny <danny.kilkenny18@gmail.com>
2021-10-04 16:41:25 -0400

    Updated the dropdowns from ipy to Bokeh

--- openmdao/visualization/opt_progress_viewer.py
+++ openmdao/visualization/opt_progress_viewer.py
@@ -1,6 +1,12 @@
 import json
 from openmdao.utils.notebook_utils import notebook
 from ipywidgets import interact
+from bokeh.io import show, curdoc
+from bokeh.models import Select, CustomJS
+from bokeh.layouts import row, column
+from bokeh.server.server import Server
+from bokeh.application import Application
+from bokeh.application.handlers.function import FunctionHandler
 
 import openmdao.api as om
 
@@ -9,7 +15,7 @@ from bokeh.plotting import figure, ColumnDataSource
 
 class OptViewer(object):
 
-    def __init__(self, data):
+    def __init__(self, data, port=8888):
         """
         Initialize threading.
 
@@ -30,7 +36,7 @@ class OptViewer(object):
 
         output_notebook()
 
-        self._make_plot()
+        show(self._make_plot, notebook_handle=True, notebook_url=("http://localhost:" + str(port)))
 
     def _parse_cases(self):
         if isinstance(self.data, str):
@@ -59,28 +65,58 @@ class OptViewer(object):
 
         return opt_data
 
-    def _make_plot(self):
+    def _make_plot(self, doc):
         self._parse()
 
-        cases = list(self.cr.list_cases(out_stream=None))
-        variables = self.cr.list_source_vars('driver', out_stream=None)['outputs']
+        self.doc = doc
+        source_options = self.cr.list_sources(out_stream=None)
+        case_options = list(self.cr.list_cases(out_stream=None))
+        io_options = self.cr.list_source_vars(source_options[0], out_stream=None)
+        for key, val in io_options.items():
+            if val:
+                io_starting_option = val[0]
+                break
 
-        self.variables_plot = figure(title="Problem Variables", x_axis_label="Variable Length", y_axis_label="Variable X")
-        v_line = self.variables_plot.line(x="x_vals", y="y_vals", line_width=2, source=self.var_data)
 
-        show(self.variables_plot, notebook_handle=True)
+        self.variables_plot = figure(title="Problem Variables", x_axis_label="Variable Length",
+                                     y_axis_label="Variable X")
+        self.variables_plot.line(x="x_vals", y="y_vals", line_width=2,
+                                          source=self.var_data)
 
-        interact(self.update, case=cases, variable=variables)
+        self.source_select = Select(title="Source:", value=source_options[0],
+                                    options=self.cr.list_sources(out_stream=None))
+        # self.source_select.on_change('value', self._variable_select_update)
 
-    def update(self, case, variable):
-        case_val = self.cr.get_case(case)
-        variable_val = case_val.outputs[variable]
-        self.variables_plot.yaxis.axis_label = variable
+        self.case_select = Select(title="Case:", value=case_options[0],
+                                  options=list(self.cr.list_cases(out_stream=None)))
+        # self.case_select.on_change('value', self._variable_select_update)
+
+        self.io_select = Select(title="IO Data:", value=io_starting_option,
+                                options=self.cr.list_source_vars(self.source_select.options[0],
+                                                                 out_stream=None))
+        self.io_select.on_change('value', self.io_var_select)
+
+        layout = row(self.variables_plot, column(self.source_select,
+                                                 self.case_select,
+                                                 self.io_select))
+
+        self.doc.add_root(layout)
+        self.update()
+
+
+    def io_var_select(self, attr, old, new):
+        self.variables_plot.yaxis.axis_label = new
+        self.update()
+
+    def update(self,):
+        case = self.cr.get_case(self.case_select.value)
+        variable = case.outputs[self.io_select.value]
 
         new_data = dict(
-            x_vals=list(range(len(variable_val))),
-            y_vals=variable_val,
+            x_vals=list(range(len(variable))),
+            y_vals=variable,
         )
         self.var_data.data = new_data
 
-        push_notebook()
+        # push_notebook()
+
