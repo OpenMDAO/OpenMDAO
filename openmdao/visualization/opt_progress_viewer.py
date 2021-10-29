547ebc7d70403ba6e48f8eadf71f745966bc01c0
DKilkenny <danny.kilkenny18@gmail.com>
2021-10-29 12:17:36 -0400

    Refactor with new note section for warnings

--- openmdao/visualization/opt_progress_viewer.py
+++ openmdao/visualization/opt_progress_viewer.py
@@ -4,7 +4,7 @@ from functools import reduce
 import warnings
 
 from bokeh.io import show, output_notebook
-from bokeh.models import Select, HoverTool, MultiSelect
+from bokeh.models import Select, HoverTool, MultiSelect, Paragraph
 from bokeh.layouts import row, column
 from bokeh.plotting import figure, ColumnDataSource
 from bokeh.palettes import Category20, Turbo256
@@ -12,7 +12,6 @@ from bokeh.util.warnings import BokehUserWarning
 
 from openmdao.utils.notebook_utils import notebook
 import openmdao.api as om
-from openmdao.utils.om_warnings import issue_warning
 
 import numpy as np
 
@@ -23,6 +22,8 @@ class VarOptViewer(object):
         """
         Initialize threading.
 
+        Parameters
+        ----------
         port : int
             What port to host Bokeh server on.
         data : CaseRecorder or str
@@ -48,13 +49,15 @@ class VarOptViewer(object):
         if isinstance(self.data, om.SqliteRecorder):
             self.cr = self.data
 
-    def var_compatability_check(self, variables, var_to_comp):
-
+    def _var_compatability_check(self, variables, var_to_compare):
+        """
+        Check and filter variables with same vector length as var_to_compare.
+        """
         variables = list(set(variables['inputs'] + variables['outputs'] + variables['residuals']))
         var_list = []
 
         for variable in variables:
-            if variable in self.case.outputs and len(self.case[variable].flatten()) == len(self.case[var_to_comp].flatten()):
+            if variable in self.case.outputs and len(self.case[variable].flatten()) == len(self.case[var_to_compare].flatten()):
                 var_list.append(variable)
 
         if var_list:
@@ -84,6 +87,14 @@ class VarOptViewer(object):
         return opt_data
 
     def _make_plot(self, doc):
+        """
+        Create the plot and elements.
+
+        Parameters
+        ----------
+        doc : Document
+            The bokeh document to build.
+        """
         self._parse()
 
         self.doc = doc
@@ -97,13 +108,19 @@ class VarOptViewer(object):
                 io_starting_option = self.io_options_x[key][0]
             self.io_options_x[key] = sorted(self.io_options_x[key])
 
-
         self.variables_plot = figure(title="Problem Variables", x_axis_label="Variable Length",
                                      y_axis_label="Variable X")
-
         self.variables_plot.circle(x="x_vals", y="y_vals", source=self.circle_data)
+
         line_plot = self.variables_plot.multi_line(xs="x_vals", ys="y_vals", line_width=2, line_color='color',
                                                    source=self.multi_line_data)
+        ht = HoverTool(renderers=[line_plot],
+            tooltips=[
+                ( 'Case',  '@cases')
+            ],
+            mode = 'mouse'
+        )
+        self.variables_plot.add_tools(ht)
 
         self.source_select = Select(title="Source:", value=source_options[0],
                                     options=source_options)
@@ -122,66 +139,77 @@ class VarOptViewer(object):
 
         self.variables_plot.yaxis.axis_label = self.variables_plot.xaxis.axis_label = io_starting_option
 
+        self.warning_box = Paragraph(text="""""", width=350, height=100)
+
         self.layout = row(self.variables_plot, column(self.source_select,
                                                       self.case_select,
                                                       self.io_select_x,
                                                       self.io_select_y,
+                                                      self.warning_box
                                                       ))
 
-        ht = HoverTool(renderers=[line_plot],
-            tooltips=[
-                ( 'Case',  '@cases')
-            ],
-            mode = 'mouse'
-        )
-        self.variables_plot.add_tools(ht)
-        self.update()
+        self._update()
 
-        self.io_select_y.options = self.var_compatability_check(self.io_options_x, self.io_select_x.value)
+        self.io_select_y.options = self._var_compatability_check(self.io_options_x, self.io_select_x.value)
         for key, val in self.io_select_x.options.items():
             self.io_select_x.options[key] = val + ["Number of Points"] + ["Case Iterations"]
 
         self.doc.add_root(self.layout)
 
     def _source_update(self, attr, old, new):
+        """
+        Update function for when the source dropdown is updated.
+        """
         self.case_select.options = [(str(i), case) for i, case in \
                                     enumerate(self.cr.list_cases(new, out_stream=None))]
         self.source_select.value = new
         self.case_select.value = ['0']
-        self.update()
+        self._update()
 
     def _case_select_update(self, attr, old, new):
-        self.update()
+        """
+        Update function for when one or more cases are added by the user.
+        """
+        self._update()
 
     def _io_var_select_y_update(self, attr, old, new):
+        """
+        Update function for when the source Y Value dropdown is updated.
+        """
         self.variables_plot.yaxis.axis_label = new
-        self.update()
+        self._update()
 
     def _io_var_select_x_update(self, attr, old, new):
+        """
+        Update function for when the X Value dropdown is updated.
+        """
         if self.io_select_x.value == "Number of Points" or \
             self.io_select_x.value == "Case Iterations":
             self.io_select_y.options = self.io_select_x.options
             self.variables_plot.xaxis.axis_label = new
             self.variables_plot.yaxis.axis_label = self.io_select_y.value
         else:
-            self.io_select_y.options = self.var_compatability_check(self.io_select_x.options, new)
+            self.io_select_y.options = self._var_compatability_check(self.io_select_x.options, new)
             self.io_select_y.value = self.io_select_y.options[0]
 
             self.variables_plot.xaxis.axis_label = new
             self.variables_plot.yaxis.axis_label = self.io_select_y.value
 
-        self.update()
+        self._update()
 
     def flatten_list(self, list_to_flatten):
         return reduce(operator.concat, list_to_flatten)
 
-    def update(self):
+    def _update(self):
+        """
+        """
         new_data = dict(
             x_vals=[],
             y_vals=[],
             color=[],
             cases=[]
         )
+        self.warning_box.text = ""
         num_points_x = num_points_y = False
         case_iter_x = case_iter_y = False
 
@@ -211,18 +239,18 @@ class VarOptViewer(object):
                     x_io = getattr(self.case, key)
 
             if (num_points_y and num_points_x) or (case_iter_x and case_iter_y):
-                x_variable = list(range(1))
-                y_variable = list(range(1))
+                x_variable = np.zeros(1)
+                y_variable = np.zeros(1)
             elif (num_points_x and case_iter_y) or (num_points_y and case_iter_x):
-                x_variable = list(range(1))
-                y_variable = list(range(1))
+                x_variable = np.zeros(1)
+                y_variable = np.zeros(1)
                 print("Cannot compare Number of Points to Case Iterations")
             elif num_points_y or case_iter_y:
                 x_variable = x_io[self.io_select_x.value].flatten()
-                y_variable = list(range(len(x_variable)))
+                y_variable = np.arange(len(x_variable))
             elif num_points_x or case_iter_x:
                 y_variable = y_io[self.io_select_y.value].flatten()
-                x_variable = list(range(len(y_variable)))
+                x_variable = np.arange(len(y_variable))
             else:
                 x_variable = x_io[self.io_select_x.value].flatten()
                 y_variable = y_io[self.io_select_y.value].flatten()
@@ -235,41 +263,41 @@ class VarOptViewer(object):
             new_data['y_vals'] = np.vstack((new_data['y_vals'], y_variable))
 
         if new_data['x_vals'].shape[1] > 1:
+            if (new_data['x_vals'].shape[0], new_data['y_vals'].shape[0]) == (1,1) and \
+                (set(new_data['x_vals'][0]), set(new_data['y_vals'][0])) == ({0.}, {0.}):
+                self.warning_box.text = """NOTE: Both X and Y values contain zeros for values,
+                                        unable to plot"""
+
             x_len = new_data['x_vals'].shape[1]
             y_len = new_data['y_vals'].shape[1]
             new_data['color'] = self._line_color_list(new_data['x_vals'])
             new_data['cases'] = [self.case_options[int(case)][1] for case in self.case_select.value]
             case_len = len(new_data['cases'])
 
-            # Move this check outside of the if loop and then make it work if a user picks number of
-            # points for both x and y
             if case_iter_x:
                 if len(new_data['cases']) == 1:
-                    issue_warning("Select two or more cases")
-                new_data['x_vals'] = np.full((x_len, case_len), [list(range(0, case_len))]).T.tolist()
-                new_data['y_vals'] = new_data['y_vals'].tolist()
+                    self.warning_box.text = "NOTE: Select two or more cases"
+                new_data['x_vals'] = np.full((x_len, case_len), [list(range(0, case_len))]).T
+                new_data['y_vals'] = new_data['y_vals']
 
             elif case_iter_y:
                 if len(new_data['cases']) == 1:
-                    issue_warning("Select two or more cases")
-                new_data['y_vals'] = np.full((y_len, case_len), [list(range(0, case_len))]).T.tolist()
-                new_data['x_vals'] = new_data['x_vals'].tolist()
-            else:
-                new_data['x_vals'] = new_data['x_vals'].tolist()
-                new_data['y_vals'] = new_data['y_vals'].tolist()
+                    self.warning_box.text = "NOTE: Select two or more cases"
+                new_data['y_vals'] = np.full((y_len, case_len), [list(range(0, case_len))]).T
+                new_data['x_vals'] = new_data['x_vals']
 
-            # For debugging purposes only. Delete for final release.
-            self.x_vals = new_data['x_vals']
-            self.y_vals = new_data['y_vals']
+            if isinstance(new_data['x_vals'], np.ndarray):
+                new_data['x_vals'] = new_data['x_vals'].tolist()
+            if isinstance(new_data['y_vals'], np.ndarray):
+                new_data['y_vals'] = new_data['y_vals'].tolist()
 
             self.multi_line_data.data = new_data
             self.circle_data.data = {"x_vals": [], "y_vals": [], "color": [], "cases": []}
         else:
-            for key, val in new_data.items():
-                if len(val) > 1:
-                    new_data[key] = self.flatten_list(val)
-                else:
-                    new_data[key] = val
+            if isinstance(new_data['x_vals'], np.ndarray):
+                new_data['x_vals'] = new_data['x_vals'].tolist()
+            if isinstance(new_data['y_vals'], np.ndarray):
+                new_data['y_vals'] = new_data['y_vals'].tolist()
             new_data['color'] = self._line_color_list(new_data['x_vals'])
             new_data['cases'] = [self.case_options[int(case)][1] for case in self.case_select.value]
 
@@ -290,10 +318,6 @@ class VarOptViewer(object):
             elif length < 20:
                 colors = list(Category20[length])
             else:
-                issue_warning("Cannot compare more than 256 cases")
+                self.warning_box.text = "NOTE: Cannot compare more than 256 cases"
 
         return colors
-
-    def test_func(self):
-        # For debugging purposes only. Delete for final release.
-        return self.x_vals, self.y_vals
