c5313660309bb4945f54ae78738b0e25a6c7bd4a
DKilkenny <danny.kilkenny18@gmail.com>
2021-11-17 16:46:06 -0500

    Added warning when user tries to connect des_var to output

--- openmdao/core/problem.py
+++ openmdao/core/problem.py
@@ -937,6 +937,14 @@ class Problem(object):
         }
         model._setup(model_comm, mode, self._metadata)
 
+        des_vars = self.model.get_design_vars()
+        prom2abs = self._metadata['prom2abs']
+        if des_vars:
+            for key in des_vars.keys():
+                if key in prom2abs['output'] and key in prom2abs['input']:
+                    raise RuntimeError(f"Cannot connect the design variable '{key}' to output "
+                                       f"'{prom2abs['output'][key][0]}'.")
+
         # set static mode back to True in all systems in this Problem
         self._metadata['static_mode'] = True
 
