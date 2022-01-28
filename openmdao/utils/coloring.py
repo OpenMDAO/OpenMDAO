cc3844ad34bec00d8ee2edd8ae4d2e0171f8232c
Herb Schilling <hschilling@nasa.gov>
2022-01-28 09:33:56 -0500

    Added arguments to display method so display can optionally just save results to a file

--- openmdao/utils/coloring.py
+++ openmdao/utils/coloring.py
@@ -686,9 +686,16 @@ class Coloring(object):
             raise RuntimeError("Internal coloring bug: jacobian has entries where fwd and rev "
                                "colorings overlap!")
 
-    def display(self):
+    def display(self,show=True,fname='jacobian_to_compute_coloring.png'):
         """
         Display a plot of the sparsity pattern, showing grouping by color.
+
+        Parameters
+        ----------
+        show : bool
+            If True, show the plot. Otherwise, just save the plot in a file. Default is True.
+        fname : str
+            Path to the location where the plot file should be saved.
         """
         try:
             from matplotlib import pyplot, axes, cm
@@ -843,7 +850,11 @@ class Coloring(object):
         pyplot.imshow(J, interpolation="none")
         fig.tight_layout()
 
-        pyplot.show()
+        if show:
+            pyplot.show()
+        else:
+            pyplot.savefig(fname)
+
 
     def get_dense_sparsity(self, dtype=bool):
         """
