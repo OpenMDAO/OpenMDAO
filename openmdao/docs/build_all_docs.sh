862d4d5d0fe8a3f332760c10ffe487fd9bc7d5e0
Herb Schilling <hschilling@nasa.gov>
2022-02-15 12:49:35 -0500

    turn off reporting system when building docs

--- openmdao/docs/build_all_docs.sh
+++ openmdao/docs/build_all_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -rf openmdao_book/_srcdocs openmdao_book/_build
 python build_source_docs.py;
-jupyter-book build -W --keep-going openmdao_book
+export OLD_OPENMDAO_REPORTS=${OPENMDAO_REPORTS}
+export OPENMDAO_REPORTS=0
+jupyter-book build -W --keep-going openmdao_book || export OPENMDAO_REPORTS=${OLD_OPENMDAO_REPORTS}
 python copy_build_artifacts.py;
