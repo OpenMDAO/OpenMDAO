20dc6e791668876da418b1dc26200e8a13860af6
Bret Naylor <naylor.b@gmail.com>
2022-08-02 10:55:45 -0400

    working on test

--- openmdao/solvers/tests/test_output_cache.py
+++ openmdao/solvers/tests/test_output_cache.py
@@ -5,9 +5,9 @@ import numpy as np
 import openmdao.api as om
 
 from openmdao.utils.assert_utils import assert_near_equal, assert_warning, assert_no_warning
+from openmdao.core.tests.test_impl_comp import QuadraticComp
 
-
-SIZE = 5
+SIZE = 1
 
 class MyTestComp(om.ExplicitComponent):
     def __init__(self, neg_iter=4):
@@ -16,7 +16,7 @@ class MyTestComp(om.ExplicitComponent):
         self.count = 0
 
     def setup(self):
-        self.add_input('x', np.ones(SIZE))
+        self.add_input('x', np.zeros(SIZE))
         self.add_output('y', np.ones(SIZE))
 
         self.declare_partials(of='*', wrt='*')
@@ -24,9 +24,9 @@ class MyTestComp(om.ExplicitComponent):
     def compute(self, inputs, outputs):
         self.count += 1
         if self.count == self.neg_iter:
-            outputs['y'] = -99.
+            outputs['y'] = 5.
         else:
-            outputs['y'] = 3.0*inputs['x']
+            outputs['y'] = inputs['x']
 
 
 class MyLogComp(om.ExplicitComponent):
@@ -42,17 +42,22 @@ class MyLogComp(om.ExplicitComponent):
 
 class TestSolverFeatures(unittest.TestCase):
 
-    def test_simple_caching(self):
+    def setUp(self):
         p = om.Problem()
+        p.model.add_subsystem('C1', MyTestComp(neg_iter=2))
+        p.model.add_subsystem('C2', QuadraticComp())
+        p.model.connect('C1.y', 'C2.c')
         p.model.nonlinear_solver = om.NonlinearBlockGS()
         p.model.nonlinear_solver.options['err_on_non_converge'] = True
 
-        p.model.add_subsystem('C1', MyTestComp(neg_iter=9999))
-        p.model.add_subsystem('C2', MyLogComp())
-        p.model.connect('C1.y', 'C2.x')
-        p.model.connect('C2.y', 'C1.x')
-
         p.setup()
+
+        p['C2.b'] = 2.
+
+        self.p = p
+
+    def test_simple_caching(self):
+        p = self.p
         p.run_model()
 
         print("iters:", p.model.nonlinear_solver._iter_count)
