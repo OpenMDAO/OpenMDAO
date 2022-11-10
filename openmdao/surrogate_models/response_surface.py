6c48af27ec587fbb5bf568ca2bc4a8c4e1d44d03
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2022-11-10 11:16:19 -0500

    Adding conditional import for numpy.dual

--- openmdao/surrogate_models/response_surface.py
+++ openmdao/surrogate_models/response_surface.py
@@ -4,7 +4,11 @@ Surrogate Model based on second order response surface equations.
 """
 
 from numpy import zeros, einsum
-from numpy.linalg import lstsq
+import numpy as np
+if Version(np.__version__) >= Version("1.20"):
+    from numpy.linalg import lstsq
+else:
+    from numpy.dual import lstsq
 from openmdao.surrogate_models.surrogate_model import SurrogateModel
 
 
