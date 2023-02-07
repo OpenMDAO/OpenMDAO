e8560f55ac857e6211f09751239616e2487d3027
nsteffen <nathan.r.steffen@nasa.gov>
2023-02-07 15:05:58 -0500

    Fixed bug with displaying derivative type in

--- openmdao/core/problem.py
+++ openmdao/core/problem.py
@@ -2686,9 +2686,11 @@ def _assemble_derivative_data(derivative_data, rel_error_tol, abs_error_tol, out
                         fd = 0.
 
                     if directional:
-                        out_buffer.write(f"    Directional {fd_opts['method'].upper()} Derivative (J{fd_opts['method']})\n{fd}\n")
+                        out_buffer.write(f"    Directional {fd_opts['method'].upper()}"
+                                         " Derivative (J{fd_opts['method']})\n{fd}\n")
                     else:
-                        out_buffer.write(f"    Raw {fd_opts['method'].upper()} Derivative (J{fd_opts['method']})\n{fd}\n")
+                        out_buffer.write(f"    Raw {fd_opts['method'].upper()}"
+                                         " Derivative (J{fd_opts['method']})\n{fd}\n")
 
                     out_buffer.write(' -' * 30 + '\n')
 
