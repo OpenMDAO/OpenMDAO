ad16eae5789519d420f0184456b553693dafb06f
nsteffen <nathan.r.steffen@nasa.gov>
2023-02-16 11:38:50 -0500

    moved SubproblemComp and changed init args

--- openmdao/core/Subproblem/SubproblemComp_Test.py
+++ openmdao/core/Subproblem/SubproblemComp_Test.py
@@ -1,6 +1,6 @@
 from numpy import pi
 import openmdao.api as om
-from SubproblemComp import SubproblemComp
+from openmdao.core.SubproblemComp import SubproblemComp
 
 
 prob = om.Problem()
@@ -9,17 +9,17 @@ model = om.ExecComp('z = x**2 + y')
 sub_model1 = om.ExecComp('x = r*cos(theta)')
 sub_model2 = om.ExecComp('y = r*sin(theta)')
 
-subprob1 = SubproblemComp(problem=om.Problem(), model=sub_model1,
-                          inputs=['r', 'theta'], outputs=['x'])
-subprob2 = SubproblemComp(problem=om.Problem(), model=sub_model2,
-                          inputs=['r', 'theta'], outputs=['y'])
+subprob1 = SubproblemComp(model=sub_model1, inputs=['r', 'theta'],
+                          outputs=['x'])
+subprob2 = SubproblemComp(model=sub_model2, inputs=['r', 'theta'],
+                          outputs=['y'])
 
-prob.model.add_subsystem('supModel', model, promotes_inputs=['x','y'],
-                            promotes_outputs=['z'])
 prob.model.add_subsystem('sub1', subprob1, promotes_inputs=['r','theta'],
                             promotes_outputs=['x'])
 prob.model.add_subsystem('sub2', subprob2, promotes_inputs=['r','theta'],
                             promotes_outputs=['y'])
+prob.model.add_subsystem('supModel', model, promotes_inputs=['x','y'],
+                            promotes_outputs=['z'])
 
 prob.setup(force_alloc_complex=True)
 
@@ -27,7 +27,9 @@ prob.set_val('r', 1)
 prob.set_val('theta', pi)
 
 prob.run_model()
-cpd = prob.check_partials(method='cs')     
-print(prob.get_val('z'))
+cpd = prob.check_partials(method='cs')    
+print(f"x = {prob.get_val('x')}")
+print(f"y = {prob.get_val('y')}") 
+print(f"z = {prob.get_val('z')}")
 
 # om.n2(prob)
