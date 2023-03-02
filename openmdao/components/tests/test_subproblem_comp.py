58fb1315a781d2b12af7428962eb4980810b4a04
nsteffen <nathan.r.steffen@nasa.gov>
2023-03-02 14:58:02 -0500

    added option to list io as None and use unconnected io from list_inputs/outputs

--- openmdao/components/tests/test_subproblem_comp.py
+++ openmdao/components/tests/test_subproblem_comp.py
@@ -51,6 +51,52 @@ class TestSubproblemComp(unittest.TestCase):
 
         p.run_model()
         cpd = p.check_partials(method='cs', out_stream=None)
+
+        assert_near_equal(p.get_val('z'), 1.0)
+
+    def test_no_io(self):
+        p = om.Problem()
+
+        model = om.Group()
+        model.add_subsystem('supComp', om.ExecComp('z = x**2 + y'),
+                            promotes_inputs=['x', 'y'],
+                            promotes_outputs=['z'])
+
+        submodel1 = om.Group()
+        submodel1.add_subsystem('sub1_ivc_r', om.IndepVarComp('r', 1.),
+                                promotes_outputs=['r'])
+        submodel1.add_subsystem('sub1_ivc_theta', om.IndepVarComp('theta', pi),
+                                promotes_outputs=['theta'])
+        submodel1.add_subsystem('subComp1', om.ExecComp('x = r*cos(theta)'),
+                                promotes_inputs=['r', 'theta'],
+                                promotes_outputs=['x'])
+
+        submodel2 = om.Group()
+        submodel2.add_subsystem('sub2_ivc_r', om.IndepVarComp('r', 2),
+                                promotes_outputs=['r'])
+        submodel2.add_subsystem('sub2_ivc_theta', om.IndepVarComp('theta', pi/2),
+                                promotes_outputs=['theta'])
+        submodel2.add_subsystem('subComp2', om.ExecComp('y = r*sin(theta)'),
+                                promotes_inputs=['r', 'theta'],
+                                promotes_outputs=['y'])
+
+        subprob1 = om.SubproblemComp(model=submodel1)
+        subprob2 = om.SubproblemComp(model=submodel2)
+
+        p.model.add_subsystem('sub1', subprob1, promotes_inputs=['r','theta'],
+                                    promotes_outputs=['x'])
+        p.model.add_subsystem('sub2', subprob2, promotes_inputs=['r','theta'],
+                                    promotes_outputs=['y'])
+        p.model.add_subsystem('supModel', model, promotes_inputs=['x','y'],
+                                    promotes_outputs=['z'])
+
+        p.setup(force_alloc_complex=True)
+
+        p.set_val('r', 1)
+        p.set_val('theta', pi)
+
+        p.run_model()
+        cpd = p.check_partials(method='cs', out_stream=None)
         
         assert_near_equal(p.get_val('z'), 1.0) 
 
