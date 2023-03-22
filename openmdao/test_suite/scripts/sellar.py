aec4bbfbb8c0226be1cb6e4a11449f1b7a042f5b
Bret Naylor <naylor.b@gmail.com>
2023-03-22 13:41:59 -0400

    tests passing

--- openmdao/test_suite/scripts/sellar.py
+++ openmdao/test_suite/scripts/sellar.py
@@ -34,31 +34,32 @@ class SellarMDAConnect(om.Group):
         self.connect('cycle.d2.y2', ['obj_cmp.y2', 'con_cmp2.y2'])
 
 
-prob = om.Problem()
+if __name__ == '__main__':
+    prob = om.Problem()
 
-prob.model = SellarMDAConnect()
+    prob.model = SellarMDAConnect()
 
-prob.driver = om.ScipyOptimizeDriver()
-prob.driver.options['optimizer'] = 'SLSQP'
-# prob.driver.options['maxiter'] = 100
-prob.driver.options['tol'] = 1e-8
+    prob.driver = om.ScipyOptimizeDriver()
+    prob.driver.options['optimizer'] = 'SLSQP'
+    # prob.driver.options['maxiter'] = 100
+    prob.driver.options['tol'] = 1e-8
 
-prob.set_solver_print(level=0)
+    prob.set_solver_print(level=0)
 
-prob.model.add_design_var('x', lower=0, upper=10)
-prob.model.add_design_var('z', lower=0, upper=10)
-prob.model.add_objective('obj_cmp.obj')
-prob.model.add_constraint('con_cmp1.con1', upper=0)
-prob.model.add_constraint('con_cmp2.con2', upper=0)
+    prob.model.add_design_var('x', lower=0, upper=10)
+    prob.model.add_design_var('z', lower=0, upper=10)
+    prob.model.add_objective('obj_cmp.obj')
+    prob.model.add_constraint('con_cmp1.con1', upper=0)
+    prob.model.add_constraint('con_cmp2.con2', upper=0)
 
-prob.setup()
+    prob.setup()
 
-prob.set_val('x', 2.0)
-prob.set_val('z', [0., 0.])
+    prob.set_val('x', 2.0)
+    prob.set_val('z', [0., 0.])
 
-prob.run_driver()
-print('minimum found at')
-print(prob.get_val('x')[0])
-print(prob.get_val('z'))
-print('minumum objective')
-print(prob.get_val('obj_cmp.obj')[0])
+    prob.run_driver()
+    print('minimum found at')
+    print(prob.get_val('x')[0])
+    print(prob.get_val('z'))
+    print('minumum objective')
+    print(prob.get_val('obj_cmp.obj')[0])
