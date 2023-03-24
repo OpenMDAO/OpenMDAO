e827ab6fe8529176e5ebf2350dd8b3a5543f830c
arshsaja <arshsaja@umich.edu>
2023-03-24 02:52:25 -0400

    merge

--- openmdao/solvers/linear/linear_schur.py
+++ openmdao/solvers/linear/linear_schur.py
@@ -48,7 +48,6 @@ class LinearSchur(BlockLinearSolver):
         """
         super()._declare_options()
         # this solver does not iterate
-<<<<<<< HEAD
         # self.options.undeclare("maxiter")
         # self.options.undeclare("err_on_non_converge")
 
@@ -79,34 +78,6 @@ class LinearSchur(BlockLinearSolver):
             self._theta_n_1 = 1.0
 
         return super()._iter_initialize()
-=======
-        self.options.undeclare("maxiter")
-        self.options.undeclare("err_on_non_converge")
-
-        self.options.undeclare("atol")
-        self.options.undeclare("rtol")
-        # self.options["maxiter"] = 1
-
-    # def _iter_initialize(self):
-    #     """
-    #     Perform any necessary pre-processing operations.
-
-    #     Returns
-    #     -------
-    #     float
-    #         initial error.
-    #     float
-    #         error at the first iteration.
-    #     """
-    #     if self.options["use_aitken"]:
-    #         if self._mode_linear == "fwd":
-    #             self._delta_d_n_1 = self._system()._doutputs.asarray(copy=True)
-    #         else:
-    #             self._delta_d_n_1 = self._system()._dresiduals.asarray(copy=True)
-    #         self._theta_n_1 = 1.0
-
-    #     return super()._iter_initialize()
->>>>>>> 27fb8603301a85f483ebabd7cef4816fda004209
 
     def _single_iteration(self):
         """
@@ -117,7 +88,6 @@ class LinearSchur(BlockLinearSolver):
         #     raise ValueError(
         #         f"The solve function is called with {mode} mode. But the user defined the linear Schur solve to work in {self._mode_linear} mode"
         #     )
-<<<<<<< HEAD
         system = self._system()
         mode = self._mode_linear
         # self._update_rhs_vec()
@@ -140,13 +110,6 @@ class LinearSchur(BlockLinearSolver):
 
             d_n = d_out_vec.asarray(copy=True)
             delta_d_n = d_out_vec.asarray(copy=True)
-=======
-        self._mode = mode
-        system = self._system()
-        mode = self._mode_linear
-        self._update_rhs_vec()
-        self._rel_systems = rel_systems
->>>>>>> 27fb8603301a85f483ebabd7cef4816fda004209
 
         # take the subsystems
         subsys1, _ = system._subsystems_allprocs[self._groupNames[0]]
@@ -410,7 +373,6 @@ class LinearSchur(BlockLinearSolver):
             system._dinputs.set_val(dinputs_cahce)
             system._doutputs.set_val(outp_cache)
             system._dresiduals.set_val(resd_cache)
-<<<<<<< HEAD
             b_vec2.set_val(0.0)
 
             system._transfer("linear", mode, subsys2.name)
@@ -419,16 +381,6 @@ class LinearSchur(BlockLinearSolver):
             # b_vec += subsys1_rhs
 
             b_vec2 += subsys2_rhs
-=======
-            # b_vec2.set_val(0.0)
-
-            # system._transfer("linear", mode, subsys2.name)
-
-            # b_vec2 *= -1.0
-            # # b_vec += subsys1_rhs
-
-            # b_vec2 += subsys2_rhs
->>>>>>> 27fb8603301a85f483ebabd7cef4816fda004209
 
             # b_vec.set_val(b_vec_cache1)
             # if self._rel_systems is None and subsys2.pathname in self._rel_systems:
@@ -501,19 +453,66 @@ class LinearSchur(BlockLinearSolver):
             #### End solve for subsys 1 ####
             ################################
 
-            scope_out, scope_in = system._get_matvec_scope(subsys1)
-            scope_out = self._vars_union(self._scope_out, scope_out)
-            scope_in = self._vars_union(self._scope_in, scope_in)
-            subsys1._solve_linear(mode, self._rel_systems, scope_out, scope_in)
-
-            if subsys1._iter_call_apply_linear():
-                subsys1._apply_linear(None, self._rel_systems, mode, scope_out, scope_in)
+        if use_aitken:
+            if self._mode_linear == "fwd":
+                d_resid_vec = system._dresiduals
+                d_out_vec = system._doutputs
             else:
-                b_vec.set_val(0.0)
-                # else:
-                #     system._transfer('linear', mode, subsys1.name)
+                d_resid_vec = system._doutputs
+                d_out_vec = system._dresiduals
 
-            ################################
-            #### End solve for subsys 1 ####
-            ################################
-        self._scope_in = self._scope_out = _UNDEFINED
+            theta_n = self.options["aitken_initial_factor"]
+
+            # compute the change in the outputs after the NLBGS iteration
+            delta_d_n -= d_out_vec.asarray()
+            delta_d_n *= -1
+
+            if self._iter_count >= 2:
+                # Compute relaxation factor. This method is used by Kenway et al. in
+                # "Scalable Parallel Approach for High-Fidelity Steady-State Aero-
+                # elastic Analysis and Adjoint Derivative Computations" (ln 22 of Algo 1)
+
+                temp = delta_d_n.copy()
+                temp -= delta_d_n_1
+
+                # If MPI, piggyback on the residual vector to perform a distributed norm.
+                if system.comm.size > 1:
+                    backup_r = d_resid_vec.asarray(copy=True)
+                    d_resid_vec.set_val(temp)
+                    temp_norm = d_resid_vec.get_norm()
+                else:
+                    temp_norm = np.linalg.norm(temp)
+
+                if temp_norm == 0.0:
+                    temp_norm = 1e-12  # prevent division by 0 below
+
+                # If MPI, piggyback on the output and residual vectors to perform a distributed
+                # dot product.
+                if system.comm.size > 1:
+                    backup_o = d_out_vec.asarray(copy=True)
+                    d_out_vec.set_val(delta_d_n)
+                    tddo = d_resid_vec.dot(d_out_vec)
+                    d_resid_vec.set_val(backup_r)
+                    d_out_vec.set_val(backup_o)
+                else:
+                    tddo = temp.dot(delta_d_n)
+
+                theta_n = theta_n_1 * (1 - tddo / temp_norm**2)
+
+            else:
+                # keep the initial the relaxation factor
+                pass
+
+            # limit relaxation factor to the specified range
+            theta_n = max(aitken_min_factor, min(aitken_max_factor, theta_n))
+
+            # save relaxation factor for the next iteration
+            self._theta_n_1 = theta_n
+
+            d_out_vec.set_val(d_n)
+
+            # compute relaxed outputs
+            d_out_vec += theta_n * delta_d_n
+
+            # save update to use in next iteration
+            delta_d_n_1[:] = delta_d_n
