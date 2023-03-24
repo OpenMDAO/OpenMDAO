a102ca6f78dca46e58e2f5f5e31b74a6eb42ff2e
arshsaja <arshsaja@umich.edu>
2023-03-24 16:21:14 -0400

    full derv check for rev mode worked

--- openmdao/solvers/linear/linear_schur.py
+++ openmdao/solvers/linear/linear_schur.py
@@ -48,38 +48,13 @@ class LinearSchur(BlockLinearSolver):
         """
         super()._declare_options()
         # this solver does not iterate
-        # self.options.undeclare("maxiter")
-        # self.options.undeclare("err_on_non_converge")
+        self.options.undeclare("maxiter")
+        self.options.undeclare("err_on_non_converge")
 
-        # self.options.undeclare("atol")
-        # self.options.undeclare("rtol")
-        self.options["maxiter"] = 1
-        self.options.declare("use_aitken", types=bool, default=False, desc="set to True to use Aitken relaxation")
-        self.options.declare("aitken_min_factor", default=0.1, desc="lower limit for Aitken relaxation factor")
-        self.options.declare("aitken_max_factor", default=1.5, desc="upper limit for Aitken relaxation factor")
-        self.options.declare("aitken_initial_factor", default=1.0, desc="initial value for Aitken relaxation factor")
+        self.options.undeclare("atol")
+        self.options.undeclare("rtol")
 
-    def _iter_initialize(self):
-        """
-        Perform any necessary pre-processing operations.
-
-        Returns
-        -------
-        float
-            initial error.
-        float
-            error at the first iteration.
-        """
-        if self.options["use_aitken"]:
-            if self._mode_linear == "fwd":
-                self._delta_d_n_1 = self._system()._doutputs.asarray(copy=True)
-            else:
-                self._delta_d_n_1 = self._system()._dresiduals.asarray(copy=True)
-            self._theta_n_1 = 1.0
-
-        return super()._iter_initialize()
-
-    def _single_iteration(self):
+    def solve(self, mode, rel_systems=None):
         """
         Perform the operations in the iteration loop.
         """
@@ -88,28 +63,11 @@ class LinearSchur(BlockLinearSolver):
         #     raise ValueError(
         #         f"The solve function is called with {mode} mode. But the user defined the linear Schur solve to work in {self._mode_linear} mode"
         #     )
+        self._mode = mode
+        self._rel_systems = rel_systems
         system = self._system()
         mode = self._mode_linear
-        # self._update_rhs_vec()
-
-        use_aitken = self.options["use_aitken"]
-
-        if use_aitken:
-            aitken_min_factor = self.options["aitken_min_factor"]
-            aitken_max_factor = self.options["aitken_max_factor"]
-
-            # some variables that are used for Aitken's relaxation
-            delta_d_n_1 = self._delta_d_n_1
-            theta_n_1 = self._theta_n_1
-
-            # store a copy of the outputs, used to compute the change in outputs later
-            if self._mode_linear == "fwd":
-                d_out_vec = system._doutputs
-            else:
-                d_out_vec = system._dresiduals
-
-            d_n = d_out_vec.asarray(copy=True)
-            delta_d_n = d_out_vec.asarray(copy=True)
+        self._update_rhs_vec()
 
         # take the subsystems
         subsys1, _ = system._subsystems_allprocs[self._groupNames[0]]
@@ -139,23 +97,16 @@ class LinearSchur(BlockLinearSolver):
         ovec = system._vectors["output"]["linear"]
         ivec = system._vectors["input"]["linear"]
 
+        inpu_cache = system._dinputs.asarray(copy=True)
+        outp_cache = system._doutputs.asarray(copy=True)
+        resd_cache = system._dresiduals.asarray(copy=True)
+
         r_data = rvec.asarray(copy=True)
         o_data = ovec.asarray(copy=True)
         i_data = ivec.asarray(copy=True)
 
         if mode == "fwd":
             parent_offset = system._dresiduals._root_offset
-
-            # must always do the transfer on all procs even if subsys not local
-            # for subsys2 in subsystem_list:
-            if self._rel_systems is not None and subsys2.pathname not in self._rel_systems:
-                return
-            # must always do the transfer on all procs even if subsys not local
-            # system._transfer("linear", mode, subsys2.name)
-            system._transfer("linear", mode, subsys2.name)
-
-            if not subsys2._is_local:
-                return
 
             # take the d_resdiuals for both of the subsys
             b_vec = subsys1._dresiduals
@@ -166,6 +117,51 @@ class LinearSchur(BlockLinearSolver):
             # cache the rhs vector since we ll need this later
             subsys1_rhs = self._rhs_vec[off : off + len(b_vec)].copy()
             subsys2_rhs = self._rhs_vec[off2 : off2 + len(b_vec2)].copy()
+
+            ################################
+            #### Beg solve for subsys 1 ####
+            ################################
+
+            # subsys1._doutputs.set_val(0.0)
+            # if self._rel_systems is not None and subsys1.pathname not in self._rel_systems:
+            #     return
+            # must always do the transfer on all procs even if subsys not local
+            system._transfer("linear", mode, subsys1.name)
+
+            # if not subsys1._is_local:
+            #     return
+
+            scope_out, scope_in = system._get_matvec_scope(subsys1)
+            scope_out = self._vars_union(self._scope_out, scope_out)
+            scope_in = self._vars_union(self._scope_in, scope_in)
+
+            if subsys1._iter_call_apply_linear():
+                subsys1._apply_linear(None, None, mode, scope_out, scope_in)
+                b_vec *= -1.0
+                b_vec += self._rhs_vec[off : off + len(b_vec)]
+            else:
+                b_vec.set_val(self._rhs_vec[off : off + len(b_vec)])
+
+            subsys1._solve_linear(mode, ContainsAll(), scope_out, scope_in)
+
+            ################################
+            #### End solve for subsys 1 ####
+            ################################
+
+            ################################
+            #### Beg solve for subsys 2 ####
+            ################################
+
+            # must always do the transfer on all procs even if subsys not local
+            # for subsys2 in subsystem_list:
+            # if self._rel_systems is not None and subsys2.pathname not in self._rel_systems:
+            #     return
+            # must always do the transfer on all procs even if subsys not local
+            # system._transfer("linear", mode, subsys2.name)
+            # system._transfer("linear", mode, subsys2.name)
+
+            # if not subsys2._is_local:
+            #     return
 
             ########################
             #### schur_jacobian ####
@@ -220,6 +216,11 @@ class LinearSchur(BlockLinearSolver):
             # backup the vectors here
             rvec.set_val(r_data)
             ovec.set_val(o_data)
+            ivec.set_val(i_data)
+
+            system._dinputs.set_val(inpu_cache)
+            system._doutputs.set_val(outp_cache)
+            system._dresiduals.set_val(resd_cache)
 
             # set the rhs vector
             b_vec.set_val(subsys1_rhs)
@@ -238,19 +239,20 @@ class LinearSchur(BlockLinearSolver):
             subsys1._vectors["output"]["linear"] *= -1.0
 
             # set the inputs to be zero
-            subsys2._dinputs.set_val(0.0)
-            system._transfer("linear", "fwd", subsys2.name)
+            subsys2._doutputs.set_val(0.0)
+            system._dinputs.set_val(0.0)
+            system._transfer("linear", "fwd")
 
             # we do an apply linear on the subsys2 to get the negative part of the rhs
-            scope_out, scope_in = system._get_matvec_scope(subsys2)
+            scope_out, scope_in = system._get_matvec_scope()
             scope_out = self._vars_union(self._scope_out, scope_out)
             scope_in = self._vars_union(self._scope_in, scope_in)
-            subsys2._apply_linear(None, None, mode, scope_out, scope_in)
+            system._apply_linear(None, None, mode, scope_out, scope_in)
 
             # add the rhs vector with the resultant negative part
             b_vec2 += subsys2_rhs
 
-            d_subsys2 = scipy.linalg.solve(schur_jac, subsys2._vectors["residual"]["linear"].asarray())
+            d_subsys2 = scipy.linalg.solve(schur_jac, b_vec2.asarray())
 
             # loop over the variables just to be safe with the ordering
             # subsys1._doutputs.set_val(subsys1_output)
@@ -261,42 +263,11 @@ class LinearSchur(BlockLinearSolver):
             #### End solve for subsys 2 ####
             ################################
 
-            ################################
-            #### Beg solve for subsys 1 ####
-            ################################
-
-            # subsys1._doutputs.set_val(0.0)
-            if self._rel_systems is not None and subsys1.pathname not in self._rel_systems:
-                return
-            # must always do the transfer on all procs even if subsys not local
-            system._transfer("linear", mode, subsys1.name)
-
-            if not subsys1._is_local:
-                return
-
-            scope_out, scope_in = system._get_matvec_scope(subsys1)
-            scope_out = self._vars_union(self._scope_out, scope_out)
-            scope_in = self._vars_union(self._scope_in, scope_in)
-
-            if subsys1._iter_call_apply_linear():
-                subsys1._apply_linear(None, None, mode, scope_out, scope_in)
-                b_vec *= -1.0
-                b_vec += self._rhs_vec[off : off + len(b_vec)]
-            else:
-                b_vec.set_val(self._rhs_vec[off : off + len(b_vec)])
-
-            subsys1._solve_linear(mode, ContainsAll(), scope_out, scope_in)
-
-            ################################
-            #### End solve for subsys 1 ####
-            ################################
-
         else:  # rev
             parent_offset = system._doutputs._root_offset
 
             # update the output of subsys2
             # system._transfer("linear", mode, subsys2.name)
-            dinputs_cahce = system._dinputs.asarray(copy=True)
             b_vec = subsys1._doutputs
             # b_vec_cache1 = subsys1._doutputs.asarray(copy=True)
             off = b_vec._root_offset - parent_offset
@@ -315,10 +286,6 @@ class LinearSchur(BlockLinearSolver):
             ## Schur_Jac = D - C A^-1 B ##
 
             rvec.set_val(np.zeros(len(rvec)))
-
-            # inpu_cache=system._doutputs.asarray(copy=True)
-            outp_cache = system._doutputs.asarray(copy=True)
-            resd_cache = system._dresiduals.asarray(copy=True)
 
             for ii, var in enumerate(resd_to_solve):
                 # set the linear seed of the variable we want to solve for in subsys 2
@@ -370,17 +337,18 @@ class LinearSchur(BlockLinearSolver):
             ################################
             #### Beg solve for subsys 2 ####
             ################################
-            system._dinputs.set_val(dinputs_cahce)
+            system._dinputs.set_val(inpu_cache)
             system._doutputs.set_val(outp_cache)
             system._dresiduals.set_val(resd_cache)
+
             b_vec2.set_val(0.0)
 
             system._transfer("linear", mode, subsys2.name)
 
-            b_vec2 *= -1.0
-            # b_vec += subsys1_rhs
+            # b_vec2 *= -1.0
+            # # b_vec += subsys1_rhs
 
-            b_vec2 += subsys2_rhs
+            # b_vec2 += subsys2_rhs
 
             # b_vec.set_val(b_vec_cache1)
             # if self._rel_systems is None and subsys2.pathname in self._rel_systems:
@@ -453,66 +421,5 @@ class LinearSchur(BlockLinearSolver):
             #### End solve for subsys 1 ####
             ################################
 
-        if use_aitken:
-            if self._mode_linear == "fwd":
-                d_resid_vec = system._dresiduals
-                d_out_vec = system._doutputs
-            else:
-                d_resid_vec = system._doutputs
-                d_out_vec = system._dresiduals
-
-            theta_n = self.options["aitken_initial_factor"]
-
-            # compute the change in the outputs after the NLBGS iteration
-            delta_d_n -= d_out_vec.asarray()
-            delta_d_n *= -1
-
-            if self._iter_count >= 2:
-                # Compute relaxation factor. This method is used by Kenway et al. in
-                # "Scalable Parallel Approach for High-Fidelity Steady-State Aero-
-                # elastic Analysis and Adjoint Derivative Computations" (ln 22 of Algo 1)
-
-                temp = delta_d_n.copy()
-                temp -= delta_d_n_1
-
-                # If MPI, piggyback on the residual vector to perform a distributed norm.
-                if system.comm.size > 1:
-                    backup_r = d_resid_vec.asarray(copy=True)
-                    d_resid_vec.set_val(temp)
-                    temp_norm = d_resid_vec.get_norm()
-                else:
-                    temp_norm = np.linalg.norm(temp)
-
-                if temp_norm == 0.0:
-                    temp_norm = 1e-12  # prevent division by 0 below
-
-                # If MPI, piggyback on the output and residual vectors to perform a distributed
-                # dot product.
-                if system.comm.size > 1:
-                    backup_o = d_out_vec.asarray(copy=True)
-                    d_out_vec.set_val(delta_d_n)
-                    tddo = d_resid_vec.dot(d_out_vec)
-                    d_resid_vec.set_val(backup_r)
-                    d_out_vec.set_val(backup_o)
-                else:
-                    tddo = temp.dot(delta_d_n)
-
-                theta_n = theta_n_1 * (1 - tddo / temp_norm**2)
-
-            else:
-                # keep the initial the relaxation factor
-                pass
-
-            # limit relaxation factor to the specified range
-            theta_n = max(aitken_min_factor, min(aitken_max_factor, theta_n))
-
-            # save relaxation factor for the next iteration
-            self._theta_n_1 = theta_n
-
-            d_out_vec.set_val(d_n)
-
-            # compute relaxed outputs
-            d_out_vec += theta_n * delta_d_n
-
-            # save update to use in next iteration
-            delta_d_n_1[:] = delta_d_n
+        # reset after solve is done
+        self._scope_in = self._scope_out = _UNDEFINED
