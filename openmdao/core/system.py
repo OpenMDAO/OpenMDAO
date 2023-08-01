457fded561c93c638158d5b84b741c20d7b15946
Bret Naylor <naylor.b@gmail.com>
2023-08-01 09:00:19 -0400

    removing another promotion warning (when promoted name is the same as the previous promoted name)

--- openmdao/core/system.py
+++ openmdao/core/system.py
@@ -2369,7 +2369,7 @@ class System(object):
                                            f"'{tup[0]}' because '{name}' has already been promoted "
                                            f"as '{old_key}'.")
 
-                if old_using != "'*'" and new_using != "'*'":
+                if old_using != "'*'" and new_using != "'*'" and old_using != new_using:
                     msg = f"{io} variable '{name}', promoted using {new_using}, " \
                           f"was already promoted using {old_using}."
                     issue_warning(msg, prefix=self.msginfo, category=PromotionWarning)
