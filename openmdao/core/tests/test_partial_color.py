9ddd8ad9ba6f0e469e80bdab1603eb002f4817b7
Bret Naylor <naylor.b@gmail.com>
2024-08-29 10:29:21 -0400

    attempt to fix hang in TestStaticColoringParallelCS test

--- openmdao/core/tests/test_partial_color.py
+++ openmdao/core/tests/test_partial_color.py
@@ -1537,21 +1537,6 @@ class TestStaticColoringParallelCS(unittest.TestCase):
 
     def setUp(self):
         np.random.seed(11)
-        self.startdir = os.getcwd()
-        if MPI.COMM_WORLD.rank == 0:
-            self.tempdir = tempfile.mkdtemp(prefix=self.__class__.__name__ + '_')
-            MPI.COMM_WORLD.bcast(self.tempdir, root=0)
-        else:
-            self.tempdir = MPI.COMM_WORLD.bcast(None, root=0)
-        os.chdir(self.tempdir)
-
-    def tearDown(self):
-        os.chdir(self.startdir)
-        if MPI.COMM_WORLD.rank == 0:
-            try:
-                shutil.rmtree(self.tempdir)
-            except OSError:
-                pass
 
     # semi-total coloring feature disabled.
 
