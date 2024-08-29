bf538883b3150d749b3ef56306bf199dde7291f3
Bret Naylor <naylor.b@gmail.com>
2024-08-29 08:51:48 -0400

    workdir stuff was unnecessary, so removed it

--- openmdao/utils/file_utils.py
+++ openmdao/utils/file_utils.py
@@ -433,22 +433,6 @@ def image2html(imagefile, title='', alt=''):
 """
 
 
-def get_work_dir():
-    """
-    Return either os.getcwd() or the value of the OPENMDAO_WORKDIR environment variable.
-
-    Returns
-    -------
-    str
-        The working directory.
-    """
-    workdir = os.environ.get('OPENMDAO_WORKDIR', '')
-    if workdir:
-        return workdir
-
-    return os.getcwd()
-
-
 def _get_outputs_dir(obj=None, *subdirs, mkdir=True):
     """
     Return a pathlib.Path for the outputs directory related to the given problem or system.
@@ -465,10 +449,11 @@ def _get_outputs_dir(obj=None, *subdirs, mkdir=True):
     ----------
     obj : Problem or System or Solver or None
         The problem or system or Solver from which we are opening a file.
+    subdirs : str
+        Additional subdirectories under the top level directory for the relevant problem. Each
+        subdir is passed as a separate positional argument.
     mkdir : bool
         If True, force the creation of this directory.
-    subdirs : str
-        Additional subdirectories under the top level directory for the relevant problem.
     """
     from openmdao.core.problem import Problem
     from openmdao.core.system import System
@@ -495,15 +480,14 @@ def _get_outputs_dir(obj=None, *subdirs, mkdir=True):
 
     prob_pathname = prob_meta['pathname']
 
-    outspath = pathlib.Path(get_work_dir()) / pathlib.Path(*[f'{p}_out'
-                                                             for p in prob_pathname.split('/')])
-    dirpath = outspath / pathlib.Path(*subdirs)
+    outs_dir = pathlib.Path(*[f'{p}_out' for p in prob_pathname.split('/')])
+    dirpath = outs_dir / pathlib.Path(*subdirs)
 
     if not dirpath.is_dir() and comm.rank == 0 and mkdir:
         dirpath.mkdir(parents=True, exist_ok=True)
         # Touch the .openmdao_out file for the output directory to ease identification.
-        if not (outspath / '.openmdao_out').exists():
-            open(outspath / '.openmdao_out', 'w').close()
+        if not (outs_dir / '.openmdao_out').exists():
+            open(outs_dir / '.openmdao_out', 'w').close()
 
     return dirpath
 
