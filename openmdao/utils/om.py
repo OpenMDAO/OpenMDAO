ec84e6b3f469c131f97bac98319d46a7f17ab8b5
Herb Schilling <hschilling@nasa.gov>
2025-02-20 09:47:49 -0500

    Added commands for running the realtime plot independently and also as part of a script run

--- openmdao/utils/om.py
+++ openmdao/utils/om.py
@@ -8,6 +8,8 @@ import argparse
 import importlib.metadata as ilmd
 
 import re
+import subprocess
+
 from openmdao import __version__ as version
 
 try:
@@ -480,6 +482,55 @@ def _list_pre_post_cmd(options, user_args):
 
     _load_and_exec(options.file[0], user_args)
 
+def _rtplot_setup_parser(parser):
+    """
+    Set up the openmdao subparser for the 'openmdao rtplot' command.
+
+    Parameters
+    ----------
+    parser : argparse subparser
+        The parser we're adding options to.
+    """
+    parser.add_argument('file', nargs=1, help='Python file containing the model.')
+
+
+def _rtplot_cmd(options, user_args):
+    """
+    Return the post_setup hook function for 'openmdao list_pre_post'.
+
+    Parameters
+    ----------
+    options : argparse Namespace
+        Command line options.
+    user_args : list of str
+        Args to be passed to the user script.
+    """
+
+    def _view_realtime_opt_plot(problem):
+        print(f"{problem=}")
+        print(f"{problem.driver=}")
+        print(f"{problem.driver._rec_mgr._recorders=}")
+        print(f"{problem.driver._rec_mgr._recorders[0]=}")
+        print(f"{str(problem.driver._rec_mgr._recorders[0]._filepath)=}")
+
+        driver = problem.driver
+        if not driver:
+            raise RuntimeError("Unable to run realtime optimization progress plot because no Driver a")
+        recorder_filepath = str(problem.driver._rec_mgr._recorders[0]._filepath)
+        
+        print("in _view_realtime_opt_plot  *****************************")
+        cmd = ['openmdao', 'realtime_opt_plot', recorder_filepath]
+        cp = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)  # nosec: trusted input
+        if (cp.returncode != 0):
+            raise RuntimeError(f"Failed to create HTML file with generic model ({cp.stderr}).")
+
+        # This code runs immediately
+        print("Main program continues!")
+
+    # register the hook
+    hooks._register_hook('_setup_recording', 'Problem', post=_view_realtime_opt_plot, ncalls=1)
+    # run the script
+    _load_and_exec(options.file[0], user_args)
 
 def _get_deps(dep_dict: dict, package_name: str) -> None:
     """
@@ -651,6 +702,11 @@ _command_map = {
         _partial_coloring_setup_parser,
         _partial_coloring_cmd,
         "Compute coloring(s) for specified partial jacobians.",
+    ),
+    'rtplot': (
+        _rtplot_setup_parser, 
+        _rtplot_cmd,
+        "Run the realtime optimization progress plot tool once the driver recorder file is started"
     ),
     'realtime_opt_plot': (
         _realtime_opt_plot_setup_parser, 
