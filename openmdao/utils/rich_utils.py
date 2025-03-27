440a6b544cdd6a750d7d49a295e0c5fd8da4a7a9
Rob Falck <rfalck@nasa.gov>
2025-03-27 14:32:55 -0400

    Moved rich_wrap to its own utility file.

---
+++ openmdao/utils/rich_utils.py
@@ -0,0 +1,43 @@
+import re
+
+try:
+    import rich
+except ImportError:
+    rich = None
+
+
+def rich_wrap(s, tags=None):
+    """
+    If rich is available, escape square brackets and wrap the given string in the provided tags.
+    If rich is not available, just return the string.
+
+    Parameters
+    ----------
+    s : str
+        The string to be wrapped in rich tags.
+    tags : Sequence[str] or set[str], optional
+        The rich tags to be wrapped around s. These can either be
+        strings.
+
+    Returns
+    -------
+    str
+        The given string wrapped in the provided rich tags.
+    """
+    if rich is None:
+        return s
+
+    # Escape any open square brackets in the string.
+    s = re.sub(r'(\[)(?=[^]]*\])', '\\[', s)
+
+    if tags is None:
+        return s
+
+    if isinstance(tags, str):
+        tags = {tags}
+
+    # cmds = sorted(flatten([t if isinstance(t, str) else t.value for t in tags]))
+    cmds = sorted(tags)
+    on = ' '.join(cmds)
+    off = '/' + ' '.join(reversed(cmds))
+    return f'[{on}]{s}[{off}]'
