f4cb89f69f703f4f6f1247a97ebe5a3cd015c851
Bret Naylor <naylor.b@gmail.com>
2025-05-27 10:37:18 -0400

    dbg

--- openmdao/core/explicitcomponent.py
+++ openmdao/core/explicitcomponent.py
@@ -175,7 +175,7 @@ class ExplicitComponent(Component):
             if self._jacobian is None:
                 om_dump_indent(self, f"New Jacobian for {self.msginfo}")
                 self._jacobian = self._choose_jac_type()
-                om_dump_indent(self, f"New Jacobian for {self.msginfo} is: {self._jacobian}")
+                om_dump_indent(self, f"New Jacobian for {self.msginfo} is: {type(self._jacobian).__name__}")
                 if self._has_approx:
                     self._get_static_wrt_matches()
                     self._add_approximations(use_relevance=use_relevance)
