f622c0766fc02e36895c5711b7c1acc37b5a85f6
Daniel Ingraham <d.j.ingraham@gmail.com>
2026-01-20 10:00:23 -0500

    Fix some `ruff` complaints in the new Julia stuff

--- openmdao/docs/openmdao_book/features/experimental/paraboloid.py
+++ openmdao/docs/openmdao_book/features/experimental/paraboloid.py
@@ -1,7 +1,8 @@
 import os
 
 # Create a new Julia module that will hold all the Julia code imported into this Python module.
-import juliacall; jl = juliacall.newmodule("ParaboloidComponentsStub")
+import juliacall
+jl = juliacall.newmodule("ParaboloidComponentsStub")
 
 # Get the directory this file is in, then include the `paraboloid.jl` Julia source code.
 d = os.path.dirname(os.path.abspath(__file__))
