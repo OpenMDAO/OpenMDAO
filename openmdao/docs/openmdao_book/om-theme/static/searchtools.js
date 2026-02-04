73aec11f98dca825de38cb8f21b72d9747650f8d
Herbert Schilling <hschilling@nasa.gov>
2026-02-04 16:00:47 -0500

    removed unneeded lines and whitespace

--- openmdao/docs/openmdao_book/om-theme/static/searchtools.js
+++ openmdao/docs/openmdao_book/om-theme/static/searchtools.js
@@ -236,7 +236,6 @@ const Search = {
 
     if (params.has("search_source")) {
       this._search_source = true
-      // $('input[name="search_source"]')[0].checked = true;
       document.querySelector('input[name="search_source"]').checked = true;
     }
 
@@ -480,25 +479,21 @@ const Search = {
       objectTerms,
     );
 
+    if (!this._search_source) {
+      // filter out all results from the source docs
+      results = results.filter(function(value, index, arr) {
+          return !value[0].startsWith("_srcdocs");
+      });
+    }
 
-
-            if (!this._search_source) {
-            // filter out all results from the source docs
-            results = results.filter(function(value, index, arr) {
-                return !value[0].startsWith("_srcdocs");
-            });
-        }
-
-        // lookup as object (only if search source option is true)
-        if (this._search_source) {
-            for (let i = 0; i < objectTerms.length; i++) {
-                var others = [].concat(objectTerms.slice(0, i),
-                    objectTerms.slice(i + 1, objectTerms.length));
-                results = results.concat(this.performObjectSearch(objectTerms[i], others));
-            }
-          }
-
-
+    // lookup as object (only if search source option is true)
+    if (this._search_source) {
+      for (let i = 0; i < objectTerms.length; i++) {
+          var others = [].concat(objectTerms.slice(0, i),
+              objectTerms.slice(i + 1, objectTerms.length));
+          results = results.concat(this.performObjectSearch(objectTerms[i], others));
+    }
+    }
 
     // for debugging
     //Search.lastresults = results.slice();  // a copy
