1765b75074a708a01e9c3d19731750c7d78217f6
Kenneth-T-Moore <kenneth.t.moore-1@nasa.gov>
2026-03-25 08:11:24 -0400

    First pass fix for broken rel_element

--- openmdao/core/group.py
+++ openmdao/core/group.py
@@ -3005,8 +3005,6 @@ class Group(System):
                 raise RuntimeError("When computing total derivatives for the model, the "
                                    f"wrt variable '{name}' is not an independent variable "
                                    "or does not have an independant variable as a source.")
-            wrt.append(src)
-
 
         of = [m['source'] for m in self._owns_approx_of.values()]
 
